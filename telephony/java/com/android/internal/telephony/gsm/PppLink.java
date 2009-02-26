93ae8c57f5f80c67819a350e10ef0c43bccae347
Sean McNeil <sean.mcneil@windriver.com>
2009-02-26 12:07:27 +0700

    Do as the comment says and allow "up" or "unknown" to bring up device.

--- telephony/java/com/android/internal/telephony/gsm/PppLink.java
+++ telephony/java/com/android/internal/telephony/gsm/PppLink.java
@@ -150,10 +150,10 @@ final class PppLink extends DataLink implements DataLinkInterface {
 
             // Unfortunately, we're currently seeing operstate
             // "unknown" where one might otherwise expect "up"
-            if (ArrayUtils.equals(mCheckPPPBuffer, UP_ASCII_STRING, UP_ASCII_STRING.length)
+            if ((ArrayUtils.equals(mCheckPPPBuffer, UP_ASCII_STRING, UP_ASCII_STRING.length)
                     || ArrayUtils.equals(mCheckPPPBuffer, UNKNOWN_ASCII_STRING,
-                            UNKNOWN_ASCII_STRING.length) 
-                            && dataConnection.state == State.CONNECTING) {
+                            UNKNOWN_ASCII_STRING.length)) 
+                            && (dataConnection.state == State.INITING || connecting)) {
 
                 Log.i(LOG_TAG, 
                 "found ppp interface. Notifying GPRS connected");
