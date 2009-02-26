e85f0c32029e7ce83719a26b172f2cb21f9da121
Sean McNeil <sean.mcneil@windriver.com>
2009-02-26 12:05:55 +0700

    Object not initialized yet.

--- telephony/java/com/android/internal/telephony/gsm/PdpConnection.java
+++ telephony/java/com/android/internal/telephony/gsm/PdpConnection.java
@@ -155,11 +155,6 @@ public class PdpConnection extends Handler {
         this.dataLink = null;
         receivedDisconnectReq = false;
         this.dnsServers = new String[2];
-
-        if (SystemProperties.get("ro.radio.use-ppp","no").equals("yes")) {
-            dataLink = new PppLink(phone.mDataConnection);
-            dataLink.setOnLinkChange(this, EVENT_LINK_STATE_CHANGED, null);
-        }
     }
 
     /**
@@ -199,6 +194,7 @@ public class PdpConnection extends Handler {
         if (state == PdpState.ACTIVE) {
             if (dataLink != null) {
                 dataLink.disconnect();
+                dataLink = null;
             }
 
             if (phone.mCM.getRadioState().isOn()) {
@@ -406,9 +402,9 @@ public class PdpConnection extends Handler {
                     } else {
                         String[] response = ((String[]) ar.result);
                         cid = Integer.parseInt(response[0]);
+                        interfaceName = response[1];
                         
                         if (response.length > 2) {
-                            interfaceName = response[1];
                             ipAddress = response[2];
                             String prefix = "net." + interfaceName + ".";
                             gatewayAddress = SystemProperties.get(prefix + "gw");
@@ -435,10 +431,17 @@ public class PdpConnection extends Handler {
                                 }
                             }
                         }
-                        
+
+                        if (SystemProperties.get("ro.radio.use-ppp","no").equals("yes")) {
+                            dataLink = new PppLink(phone.mDataConnection);
+                            dataLink.setOnLinkChange(this, EVENT_LINK_STATE_CHANGED, null);
+                        }
+
                         if (dataLink != null) {
+                            if (DBG) log("PDP connecting to dataLink");
                             dataLink.connect();
                         } else {
+                            if (DBG) log("PDP there is no dataLink");
                             onLinkStateChanged(DataLink.LinkState.LINK_UP);
                         }
                         
