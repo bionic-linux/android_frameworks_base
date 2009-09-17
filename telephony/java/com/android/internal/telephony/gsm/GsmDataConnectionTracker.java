53b3b6d7b0e2202f621b4b534f3c3579a868b316
Kenny <gongwuchun@gmail.com>
2009-09-17 22:26:31 +0800

    Change the checking status

--- telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java
@@ -1233,7 +1233,7 @@ public final class GsmDataConnectionTracker extends DataConnectionTracker {
     protected void onEnableNewApn() {
         // TODO:  To support simultaneous PDP contexts, this should really only call
         // cleanUpConnection if it needs to free up a PdpConnection.
-        if (state == State.FAILED) {
+        if (state == State.DISCONNECTING) {
             cleanUpConnection(true, Phone.REASON_APN_SWITCHED);
         }
     }
