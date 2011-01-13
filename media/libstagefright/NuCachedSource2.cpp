8e4aff8d4089c08b747187dca7b952ab8219505a
Apurupa Pattapu <apurupa@codeaurora.org>
2011-01-12 17:08:28 -0800

    libstagefright: Check size before doing a copy
    This will avoid memory violation when a error occurs
    during fetching
    
    Change-Id: Icf1587b552e3fff8f961f2a8402d48d8edbd4a2b

--- media/libstagefright/NuCachedSource2.cpp
+++ media/libstagefright/NuCachedSource2.cpp
@@ -438,10 +438,11 @@ ssize_t NuCachedSource2::readInternal(off_t offset, void *data, size_t size) {
             return mFinalStatus;
         }
 
-        size_t avail = mCache->totalSize() - delta;
-        mCache->copy(delta, data, avail);
+        size_t sizeToCopy = mCache->totalSize() - delta;
+        sizeToCopy = (sizeToCopy > size) ? size : sizeToCopy;
+        mCache->copy(delta, data, sizeToCopy);
 
-        return avail;
+        return sizeToCopy;
     }
 
     if (offset + size <= mCacheOffset + mCache->totalSize()) {
