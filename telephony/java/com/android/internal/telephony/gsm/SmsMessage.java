aea59b0551c53460b27944f6f01c220db631fc29
Björn Ekberg <bjorn.ekberg.x@sonyericsson.com>
2011-02-02 12:34:55 +0100

    Improved handling of Korean specific DCS in SMS
    The earlier fix assumed KSC5601 for all reserved values of
    DCS, this is however wrong. The field should be interpreted
    as follows:
    
    10xx0100  ASCII 7bit (normally reserved)
    10xx0101  KSC5601 (normally reserved)
    00001100  KSC5601 (normally reserved)
    00xx01xx  KSC5601 (normally 8bit)
    
    Change-Id: I1c898b84cadcb2cf9310a056687174535b1bd702

--- telephony/java/com/android/internal/telephony/gsm/SmsMessage.java
+++ telephony/java/com/android/internal/telephony/gsm/SmsMessage.java
@@ -54,6 +54,7 @@ public class SmsMessage extends SmsMessageBase{
      * Not a part of the public API, therefore not in order with those constants.
      */
     private static final int ENCODING_KSC5601 = 4000;
+    private static final int ENCODING_ASCII_7BIT = 4001;
 
     private MessageClass messageClass;
 
@@ -789,17 +790,17 @@ public class SmsMessage extends SmsMessageBase{
         }
 
         /**
-         * Interprets the user data payload as KSC5601 characters, and
+         * Interprets the user data payload with the provided charset and
          * decodes them into a String
          *
          * @param byteCount the number of bytes in the user data payload
          * @return a String with the decoded characters
          */
-        String getUserDataKSC5601(int byteCount) {
+        String getUserDataWithCharset(int byteCount, String charset) {
             String ret;
 
             try {
-                ret = new String(pdu, cur, byteCount, "KSC5601");
+                ret = new String(pdu, cur, byteCount, charset);
             } catch (UnsupportedEncodingException ex) {
                 // Should return same as ENCODING_UNKNOWN on error.
                 ret = null;
@@ -1088,9 +1089,13 @@ public class SmsMessage extends SmsMessageBase{
 
                 case 1: // 8 bit data
                 case 3: // reserved
-                    Log.w(LOG_TAG, "1 - Unsupported SMS data coding scheme "
-                            + (dataCodingScheme & 0xff));
-                    encodingType = ENCODING_8BIT;
+                    if (SimRegionCache.getRegion() == SimRegionCache.MCC_KOREAN) {
+                        encodingType = ENCODING_KSC5601;
+                    } else {
+                        Log.w(LOG_TAG, "1 - Unsupported SMS data coding scheme "
+                                + (dataCodingScheme & 0xff));
+                        encodingType = ENCODING_8BIT;
+                    }
                     break;
                 }
             }
@@ -1136,13 +1141,36 @@ public class SmsMessage extends SmsMessageBase{
                 Log.w(LOG_TAG, "MWI for fax, email, or other "
                         + (dataCodingScheme & 0xff));
             }
+        } else if ((dataCodingScheme & 0xC0) == 0x80 &&
+                SimRegionCache.getRegion() == SimRegionCache.MCC_KOREAN) {
+            // Bits 7..4 == 10xx && MCC Korea
+            // Enter Korea specific decoding.
+            automaticDeletion = false;
+            userDataCompressed = (0 != (dataCodingScheme & 0x20));
+            hasMessageClass = (0 != (dataCodingScheme & 0x10));
+
+            if (userDataCompressed) {
+                Log.w(LOG_TAG, "4 - Unsupported SMS data coding scheme "
+                        + "(compression) " + (dataCodingScheme & 0xff));
+            } else {
+                switch ((dataCodingScheme >> 2) & 0x3) {
+                case 0: // ASCII 7bit
+                    encodingType = ENCODING_ASCII_7BIT;
+                    break;
+
+                case 1: // KSC5601
+                    encodingType = ENCODING_KSC5601;
+                    break;
+
+                default:
+                    Log.w(LOG_TAG, "1 - Unsupported SMS data coding scheme "
+                            + (dataCodingScheme & 0xff));
+                    break;
+                }
+            }
         } else {
             Log.w(LOG_TAG, "3 - Unsupported SMS data coding scheme "
                     + (dataCodingScheme & 0xff));
-            if (SimRegionCache.getRegion() == SimRegionCache.MCC_KOREAN) {
-                Log.w(LOG_TAG, "Korean SIM, using KSC5601 for decoding.");
-                encodingType = ENCODING_KSC5601;
-            }
         }
 
         // set both the user data and the user data header.
@@ -1166,8 +1194,13 @@ public class SmsMessage extends SmsMessageBase{
             break;
 
         case ENCODING_KSC5601:
-            messageBody = p.getUserDataKSC5601(count);
+            messageBody = p.getUserDataWithCharset(count, "KSC5601");
             break;
+
+        case ENCODING_ASCII_7BIT:
+            messageBody = p.getUserDataWithCharset(count, "US-ASCII");
+            break;
+
         }
 
         if (Config.LOGV) Log.v(LOG_TAG, "SMS message body (raw): '" + messageBody + "'");
