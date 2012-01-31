MIPS/frameworks-base

[MIPS] add the validation and benchmark program
Change-Id: I14a87affa88dd02b50c25e8c08a80ec5b1fd10b3

opengl/libagl/test-bench/Android.mk
@@ -1,0 +1,24 @@
+ifeq ($(TARGET_ARCH),mips)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= libagl_bench_test.c
+
+ifneq ($(ARCH_MIPS_HAS_FPU),true)
+	LOCAL_SRC_FILES += ../arch-mips/fixed_asm.S
+endif
+
+LOCAL_MODULE:= libagl_bench_test
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)
+
+LOCAL_STATIC_LIBRARIES := libcutils libc libm
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+endif

bionic-linux/android_frameworks_base

opengl/libagl/test-bench/Android.mk
@@ -1,0 +1,24 @@
+ifeq ($(TARGET_ARCH),mips)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= libagl_bench_test.c
+
+ifneq ($(ARCH_MIPS_HAVE_FPU),true)
+    LOCAL_SRC_FILES += ../arch-mips/fixed_asm.S
+endif
+
+LOCAL_MODULE:= libagl_bench_test
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)
+
+LOCAL_STATIC_LIBRARIES := libcutils libc libm
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+endif