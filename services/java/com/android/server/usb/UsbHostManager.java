a9fe7f1948e7345d6cac60510b88fd499e1fe653
Robin Cutshaw <robin.cutshaw@gmail.com>
2012-05-01 19:45:25 -0400

    Added missing USB device descriptor fields needed for intent filters
    The UsbDevice object is missing the ManufacturerName, ProductName, and
    SerialNumber fields.  These are needed by intent filters to further
    qualify a USB device that is plugged in while in host mode.  These
    fields have been added in the jni UsbHostManager implementation and
    propagated through UsbHostManager and UsbDevice implementations.
    The UsbSettingsManager implementation has been modified to allow
    manufacturer-name, product-name, and serial-number tags in intents.
    
    File changes:
           modified:   core/java/android/hardware/usb/UsbDevice.java
           modified:   services/java/com/android/server/usb/UsbHostManager.java
           modified:   services/java/com/android/server/usb/UsbSettingsManager.java
           modified:   services/jni/com_android_server_UsbHostManager.cpp
    
    Change-Id: I386884715d1b732b06a63feb77790be6b59b6fe6
    Signed-off-by: Robin Cutshaw <robin.cutshaw@gmail.com>

--- services/java/com/android/server/usb/UsbHostManager.java
+++ services/java/com/android/server/usb/UsbHostManager.java
@@ -97,6 +97,7 @@ public class UsbHostManager {
     /* Called from JNI in monitorUsbHostBus() to report new USB devices */
     private void usbDeviceAdded(String deviceName, int vendorID, int productID,
             int deviceClass, int deviceSubclass, int deviceProtocol,
+            String manufacturerName, String productName, String serialNumber,
             /* array of quintuples containing id, class, subclass, protocol
                and number of endpoints for each interface */
             int[] interfaceValues,
@@ -152,7 +153,8 @@ public class UsbHostManager {
             }
 
             UsbDevice device = new UsbDevice(deviceName, vendorID, productID,
-                    deviceClass, deviceSubclass, deviceProtocol, interfaces);
+                    deviceClass, deviceSubclass, deviceProtocol,
+                    manufacturerName, productName, serialNumber, interfaces);
             mDevices.put(deviceName, device);
             mSettingsManager.deviceAttached(device);
         }
