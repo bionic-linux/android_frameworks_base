3a03d248378413fe3d54bff290de48c5dbad3dd9
Mårten Kongstad <marten.kongstad@sonymobile.com>
2012-11-22 15:12:00 +0100

    New command line tool 'idmap'
    Introduce a new tool 'idmap' to handle generation and verification of
    idmap files. The tool is modelled on 'dexopt', and is intended to be
    used similarly, notably by 'installd'.
    
    See cmds/idmap/idmap.cpp for further documentation on 'idmap'.
    
    Note: this commit is interdependent on a commit in project build/ to add
    'idmap' to PRODUCT_PACKAGES.
    
    Change-Id: I7131b74ece1e46c8a9c0a31d103e686aa07da2bb

--- include/androidfw/AssetManager.h
+++ include/androidfw/AssetManager.h
@@ -218,6 +218,13 @@ public:
      */
     void getLocales(Vector<String8>* locales) const;
 
+    /**
+     * Generate idmap data to translate resources IDs between a package and a
+     * corresponding overlay package.
+     */
+    bool createIdmap(const char* targetApkPath, const char* overlayApkPath,
+        uint32_t targetCrc, uint32_t overlayCrc, uint32_t** outData, uint32_t* outSize);
+
 private:
     struct asset_path
     {
