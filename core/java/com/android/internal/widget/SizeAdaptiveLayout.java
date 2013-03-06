9342479685789e8c390f7a19aebebe3fcef4f86d
Conley Owens <cco3@android.com>
2013-03-06 15:16:57 -0800

    Comment out stubbed attr javadoc
    Two attrs in the SizeAdaptiveLayout.checkLayoutParams are simply stubs
    and do not document actual attrs.  This causes a warning to be spit out
    on every build.
    
    Change-Id: I5420dc34ea6434f6ff538471c0061b42e3929a93

--- core/java/com/android/internal/widget/SizeAdaptiveLayout.java
+++ core/java/com/android/internal/widget/SizeAdaptiveLayout.java
@@ -306,8 +306,8 @@ public class SizeAdaptiveLayout extends ViewGroup {
      *
      * TODO extend to width and height
      *
-     * @attr ref android.R.styleable#SizeAdaptiveLayout_Layout_layout_minHeight
-     * @attr ref android.R.styleable#SizeAdaptiveLayout_Layout_layout_maxHeight
+     * // @attr ref android.R.styleable#SizeAdaptiveLayout_Layout_layout_minHeight
+     * // @attr ref android.R.styleable#SizeAdaptiveLayout_Layout_layout_maxHeight
      */
     public static class LayoutParams extends ViewGroup.LayoutParams {
 
