dde217991218a29faf0bf384ec6da3c419d29f26
Gary Steimle <w16438@motorola.com>
2013-05-28 14:40:54 -0500

    Extend synchronization lock in VibratorService
    All references to mCurrentVibration are guarded by the mVibrations
    lock EXCEPT in the vibrate() method. This has been seen to occasionally
    cause null-pointer exceptions. Extend the use of the lock in this
    method to ensure that ALL references to mCurrentVibration are
    synchronized.
    
    Change-Id: I3a3e599c0c78d908fdd437723a5ed97985907afa

--- services/java/com/android/server/VibratorService.java
+++ services/java/com/android/server/VibratorService.java
@@ -170,18 +170,18 @@ public class VibratorService extends IVibratorService.Stub
             throw new SecurityException("Requires VIBRATE permission");
         }
         int uid = Binder.getCallingUid();
-        // We're running in the system server so we cannot crash. Check for a
-        // timeout of 0 or negative. This will ensure that a vibration has
-        // either a timeout of > 0 or a non-null pattern.
-        if (milliseconds <= 0 || (mCurrentVibration != null
-                && mCurrentVibration.hasLongerTimeout(milliseconds))) {
-            // Ignore this vibration since the current vibration will play for
-            // longer than milliseconds.
-            return;
-        }
-
-        Vibration vib = new Vibration(token, milliseconds, uid);
         synchronized (mVibrations) {
+            // We're running in the system server so we cannot crash. Check for a
+            // timeout of 0 or negative. This will ensure that a vibration has
+            // either a timeout of > 0 or a non-null pattern.
+            if (milliseconds <= 0 || (mCurrentVibration != null
+                    && mCurrentVibration.hasLongerTimeout(milliseconds))) {
+                // Ignore this vibration since the current vibration will play for
+                // longer than milliseconds.
+                return;
+            }
+
+            Vibration vib = new Vibration(token, milliseconds, uid);
             removeVibrationLocked(token);
             doCancelVibrateLocked();
             mCurrentVibration = vib;
