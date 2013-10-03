fdf38f7ec59457f1b0a6e4964081443d3f2b5878
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2013-10-03 19:35:39 +0530

    Telephony(MSIM): Add support for MultiSim.
    Make changes to add support for MultiSim.
      -Add TelephonyManager and TelePhonyRegistry for MultiSim
      -Add ITelephony and IPhoneSubinfo aidl files for MultiSim
      -Extend ShutdownThread to close all radio's for MultiSim
      -Add Constants and Utils used for MultiSim.
    
    Add prompt support for SMS
     - Add prompt option in SMS multi sim settings.
     - New iterface API to get SMS prompt is enabled or not.
     - Disply prompt while sending SMS, if user selects
       prompt mode in multi sim settings.
    
    Change Default subscription property name.
     - Change default subscription property name from
       persist.default.subscription to persist.radio.default.sub so
       that it can be set by radio group access processes.
    
    Subscription specific for Mobile data, roaming and usage
     - Provide database flags as per subscription for both Mobile
       data and Data roaming options.
     - Add function to provide DDS susbcriber id.
    
    Set priority when send sms
     - Add the priority support in 3gpp2 text message
    
    Add support for emergency calls
     -Populate ecclist by reading ril.ecclist corresponds to
      each subscription.
     -If dialed number is present in consolidated ecclist then
      mark as emergency number.
    
    Define invalid subscription constant
     -Define a new constant for invalid subscription id.
    
    Third Party Intent Handling
     -Remove firing of below intents from TelephonyRegistry when
      Multisim is enabled, as they will be fired from MSimTelephonyReistry
      ACTION_SERVICE_STATE_CHANGED
      ACTION_SIGNAL_STRENGTH_CHANGED
      ACTION_ANY_DATA_CONNECTION_STATE_CHANGED
      ACTION_DATA_CONNECTION_FAILED
     -Remove below intent from both TelephonyRegistry & MSimTelephonyReistry
      as it will be fired in MSimDefaultNotifier.
       ACTION_PHONE_STATE_CHANGED
    
    Change-Id: I6ee5d2a22e44fe7d0d68a41af0481ddbbbe21c3f

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -1798,7 +1798,7 @@ public abstract class Context {
      * @hide like {@link #stopService(Intent)} but for a specific user.
      */
     public abstract boolean stopServiceAsUser(Intent service, UserHandle user);
-    
+
     /**
      * Connect to an application service, creating it if needed.  This defines
      * a dependency between your application and the service.  The given
@@ -2260,6 +2260,17 @@ public abstract class Context {
      * @see android.telephony.TelephonyManager
      */
     public static final String TELEPHONY_SERVICE = "phone";
+
+    /**
+     * Use with {@link #getSystemService} to retrieve a
+     * {android.telephony.MSimTelephonyManager} for handling the management
+     * of the telephony features of the multi sim device.
+     *
+     * @see #getSystemService
+     * @see android.telephony.MSimTelephonyManager
+     * @hide
+     */
+    public static final String MSIM_TELEPHONY_SERVICE = "phone_msim";
 
     /**
      * Use with {@link #getSystemService} to retrieve a
