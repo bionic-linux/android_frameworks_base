5d1a3ce276edb55a3c36547e239ee6ae20da5014
Justin Breitfeller <justin.breitfeller@gmail.com>
2014-02-14 13:09:29 -0500

    Fixes potential errors from .cancel() entering user space
    If the LoaderInfo.cancel() method enters user space due to a callback, we can't
    predict all LoaderManager changes that may occur. So, if LoaderInfo.cancel() 
    alters the active loader, simply call restartLoader again to correctly handle
    any state changes that cancel() caused.

--- core/java/android/app/LoaderManager.java
+++ core/java/android/app/LoaderManager.java
@@ -682,9 +682,13 @@ class LoaderManagerImpl extends LoaderManager {
                         if (DEBUG) Log.v(TAG, "  Current loader is running; attempting to cancel");
                         info.cancel();
 
-                        // info.cancel() may have changed the currently active loader so update our reference.
-                        info = mLoaders.get(id);
-
+                        // info.cancel() may have changed the currently active 
+                        // loader. If it has, we must start over.
+                        if(info != mLoaders.get(id)) {
+                            if (DEBUG) Log.v(TAG, "  Active loader change; restartLoader again");
+                            return restartLoader(id, args, callback);
+                        }
+                        
                         if (info.mPendingLoader != null) {
                             if (DEBUG) Log.v(TAG, "  Removing pending loader: " + info.mPendingLoader);
                             info.mPendingLoader.destroy();
