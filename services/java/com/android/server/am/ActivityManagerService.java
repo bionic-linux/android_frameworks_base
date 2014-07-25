809f907712d388f16f0e6b9e774390297ce32daa
Eunae Kim <eunae.kim@lge.com>
2014-07-25 09:16:13 +0900

    Fix a bug that the RecentTask list of other’s may show up to non-primary users.
    When switching to a newly created user, the user may face this bug when he opens the RecentTask screen.
    A possible bug scenario is described as follows:
    A user id of a removed user may be recycled when created a new user.
    However, mRecentTasks is not correctly controlled so that old information may still remain and be possibly mapped to wrong user.
    This patch prevents this bug by explicitly removing old information in mRecentTasks when removing existing user.
    
    Change-Id: I1874dbd604598a5d740ae1e034981e21214c15c6
    Signed-off-by: Eunae Kim <eunae.kim@lge.com>

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -3355,6 +3355,26 @@ public final class ActivityManagerService extends ActivityManagerNative
         return ret;
     }
 
+    //explicitly remove thd old information in mRecentTasks when removing existing user.
+    private void removeRecentTaskLocked(int userId) {
+        if(userId <= 0) {
+            Slog.i(TAG, "Can't remove recent task on user " + userId);
+            return;
+        }
+
+        int N = mRecentTasks.size();
+        for (int i = 0; i < N; i++) {
+            TaskRecord tr = mRecentTasks.get(i);
+            if (tr.userId == userId) {
+                tr.disposeThumbnail();
+                mRecentTasks.remove(i);
+                Slog.i(TAG, "remove RecentTask " + userId);
+                i--;
+                N--;
+            }
+        }
+    }
+
     final void addRecentTaskLocked(TaskRecord task) {
         int N = mRecentTasks.size();
         // Quick case: check if the top-most recent task is the same.
@@ -16497,6 +16517,8 @@ public final class ActivityManagerService extends ActivityManagerNative
                 // Kill all the processes for the user.
                 forceStopUserLocked(userId, "finish user");
             }
+            //explicitly remove the old information in mRecentTasks when removing existing user.
+            removeRecentTaskLocked(userId);
         }
 
         for (int i=0; i<callbacks.size(); i++) {
