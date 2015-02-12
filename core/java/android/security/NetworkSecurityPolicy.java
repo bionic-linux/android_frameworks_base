345559774d7e5a5ff995a3030bc991d5de6da764
Alex Klyubin <klyubin@google.com>
2015-02-12 11:43:09 -0800

    Add android.security.NetworkSecurityPolicy.
    The initial purpose of this class is to provide a way for HTTP stacks
    (including unbundled ones) to check whether cleartext HTTP traffic
    should be blocked. Thus, obtaining a reference to the policy and
    querying whether insecure traffic is permited will become public API.
    
    The larger scale plan is for this policy to be expressed declaratively
    in the app manifest. The platform will then translate the policy into
    configuring the NetworkSecurityPolicy appropriately early on in the
    app's initialization process, to avoid race conditions.
    
    Alternatives:
    * Keep this API hidden (and thus potentially move it from framework to
      libcore), thus precluding unbundled HTTP stacks from using the API.
      This might be acceptable if whoever initializes these unbundled
      stacks has access to android.content.Context from which the app
      manifest configuration / ApplicationInfo can be obtained.
    * Introduce a new java.lang.System property instead of this API.
      However, such properties are a mess and not as powerful/extensible
      as a public class.
    
    Bug: 19215516
    Change-Id: If22056a74d257bf1d805ebb4fc284240b3d338f1

---
+++ core/java/android/security/NetworkSecurityPolicy.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2015, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.security;
+
+/**
+ * Network security policy.
+ *
+ * @hide
+ */
+public class NetworkSecurityPolicy {
+
+  private static final NetworkSecurityPolicy INSTANCE = new NetworkSecurityPolicy();
+
+  /**
+   * {@code true} if insecure network traffic is permitted for this application, {@code false}
+   * otherwise.
+   */
+  private boolean mInsecureTrafficPermitted = true;
+
+  private NetworkSecurityPolicy() {}
+
+  /**
+   * Gets the current policy.
+   */
+  public static NetworkSecurityPolicy get() {
+    return INSTANCE;
+  }
+
+  /**
+   * Checks whether insecure network traffic (e.g., cleartext HTTP) is permitted for this
+   * application.
+   *
+   * <p>When insecure network traffic is not permitted, the platform and third-party network
+   * stacks (e.g,. HTTP stacks) are encouraged to block attempts from this application to use
+   * insecure network traffic, for example, by throwing a
+   * {@link java.lang.SecurityException SecurityException}.
+   *
+   * @return {@code true} if insecure network traffic is permitted, {@code false} otherwise.
+   */
+  public boolean isInsecureTrafficPermitted() {
+    synchronized (this) {
+      return mInsecureTrafficPermitted;
+    }
+  }
+
+  /**
+   * Sets whether insecure network traffic (e.g., cleartext HTTP) is permitted for this
+   * application.
+   *
+   * <p>This method is method is meant to be used by the platform early on in the application's
+   * initialization to set the policy.
+   *
+   * @hide
+   */
+  public void setInsecureTrafficPermitted(boolean permitted) {
+    synchronized (this) {
+      mInsecureTrafficPermitted = permitted;
+    }
+  }
+}
