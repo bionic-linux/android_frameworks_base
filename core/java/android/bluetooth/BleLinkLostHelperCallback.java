a2b6ea36ca67e0a9e5ea073d937859db8644d2b5
zhanjboo <bo.c.zhang@tieto.com>
2015-06-05 11:26:25 +0800

    Implementation for BAS and LLS profile
    user can call BAS API to read battery level from remote devices;
    call LLS API to wirte/read remote alertlevel.
    
    Change-Id: I305048e2877891fc95bd34e25100be997a44ca2b

---
+++ core/java/android/bluetooth/BleLinkLostHelperCallback.java
@@ -0,0 +1,12 @@
+package android.bluetooth;
+
+public abstract class BleLinkLostHelperCallback {
+    public void onConnectionStateChanged(int status, int newState) {
+
+    }
+
+    public void onAlertLevelChanged(int alertLevel) {
+
+    }
+
+}
