0e2f610222c386bddfe4fca8b99f15dd675a0282
Athimoolam Ealumalai <athimoolam.ealumalai@sonymobile.com>
2015-10-06 11:17:39 +0900

    SystemUI: Display RAT icon when data is off
    Introduce customization and implementation to display RAT icon in status
    bar even when data is off. Since some carriers require, this feature is
    necessary.
    
    Bug: 28805511
    Change-Id: I412a407b8bcfeb9b3caf265bc7e0331a88433725

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -825,6 +825,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         boolean alwaysShowCdmaRssi = false;
         boolean show4gForLte = false;
         boolean hspaDataDistinguishable;
+        boolean alwaysShowRATIcon = false;
 
         static Config readConfig(Context context) {
             Config config = new Config();
@@ -836,6 +837,8 @@ public class NetworkControllerImpl extends BroadcastReceiver
             config.show4gForLte = res.getBoolean(R.bool.config_show4GForLTE);
             config.hspaDataDistinguishable =
                     res.getBoolean(R.bool.config_hspa_data_distinguishable);
+            config.alwaysShowRATIcon = res.getBoolean(R.bool.config_alwaysShowRATIcon);
+
             return config;
         }
     }
