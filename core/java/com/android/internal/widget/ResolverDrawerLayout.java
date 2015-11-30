59a475635360ecac502165377c091487dca9ec08
Yuichi Biwa <yuichi.xa.biwa@sonymobile.com>
2015-11-30 14:43:34 +0900

    ChooserActivity: Cannot start app that the icon overflows the layout
    Symptom:
    When sharing an image from Album, ChooserActivity can be shown.
    But then the app to be located to the bottom part of the list may not
    be started even if user tap it.
    
    Root cause:
    ChooserActivity uses ResolverDrawerLayout. And ResolverDrawerLayout
    can display only some items on the list (known as "Collapse mode").
    When the item clipping along the bottom edge is tapped by the user,
    ResolverDrawerLayout tries to expand the list and scroll it to a
    better position, instead of starting an application.
    In this problem case, ResolverDrawerLayout continues to try to expand
    the list whenever tapping, so an application will never start.
    
    Solution:
    Change a condition so that mOpenOnClick becomes true only when the list
    has been collapsed (mCollapseOffset > 0).
    
    Change-Id: I576fb6c8b6a91d79c1e0d46d069146779f4dbd17

--- core/java/com/android/internal/widget/ResolverDrawerLayout.java
+++ core/java/com/android/internal/widget/ResolverDrawerLayout.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2015 Sony Mobile Communications Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +13,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * NOTE: This file has been modified by Sony Mobile Communications Inc.
+ * Modifications are licensed under the License.
  */
 
 
@@ -237,7 +241,7 @@ public class ResolverDrawerLayout extends ViewGroup {
                 final float y = ev.getY();
                 mInitialTouchX = x;
                 mInitialTouchY = mLastTouchY = y;
-                mOpenOnClick = isListChildUnderClipped(x, y) && mCollapsibleHeight > 0;
+                mOpenOnClick = isListChildUnderClipped(x, y) && mCollapseOffset > 0;
             }
             break;
 
