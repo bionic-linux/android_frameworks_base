04138582a111523b4fa9b0875d0601365f829b73
Michael Peck <mpeck@mitre.org>
2016-01-06 21:44:10 -0500

    Pass sdkpost23 specifier for SELinux labeling
    Motivation:
    Provide the ability to phase in new security policies by
    applying them to apps with targetSdkVersion > 23
    
    See other submissions with same Change-Id to
    external/sepolicy and external/libselinux.
    
    Change-Id: Ib9f6ded9bd2f426861a6d843861b4074084253b0

--- services/core/java/com/android/server/pm/SELinuxMMAC.java
+++ services/core/java/com/android/server/pm/SELinuxMMAC.java
@@ -18,6 +18,7 @@ package com.android.server.pm;
 
 import android.content.pm.PackageParser;
 import android.content.pm.Signature;
+import android.os.Build;
 import android.os.Environment;
 import android.util.Slog;
 import android.util.Xml;
@@ -102,6 +103,9 @@ public final class SELinuxMMAC {
 
     // Append privapp to existing seinfo label
     private static final String PRIVILEGED_APP_STR = ":privapp";
+
+    // Append sdkpost23 to existing seinfo label
+    private static final String TARGET_SDK_POST23_STR = ":sdkpost23";
 
     /**
      * Load the mac_permissions.xml file containing all seinfo assignments used to
@@ -318,6 +322,11 @@ public final class SELinuxMMAC {
 
         if (pkg.applicationInfo.isPrivilegedApp())
             pkg.applicationInfo.seinfo += PRIVILEGED_APP_STR;
+
+        if (pkg.applicationInfo.targetSdkVersion >
+                Build.VERSION_CODES.M) {
+            pkg.applicationInfo.seinfo += TARGET_SDK_POST23_STR;
+        }
 
         if (DEBUG_POLICY_INSTALL) {
             Slog.i(TAG, "package (" + pkg.packageName + ") labeled with " +
