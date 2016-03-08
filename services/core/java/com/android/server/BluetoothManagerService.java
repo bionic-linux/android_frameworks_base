f4be01e16a34f72d505251ada746d10d65c6dff1
Ajay Panicker <apanicke@google.com>
2016-03-07 16:20:28 -0800

    getAddress will load address and name if not loaded yet
    Bug: 25836009
    Change-Id: I647d3b36ce53bc5607fe1171b3ce48ed73e6ad7e

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -1016,6 +1016,10 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                 }
             }
         }
+
+        if (!isNameAndAddressSet())
+            loadStoredNameAndAddress();
+
         // mAddress is accessed from outside.
         // It is alright without a lock. Here, bluetooth is off, no other thread is
         // changing mAddress
@@ -1161,7 +1165,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                                 Log.e(TAG,"",re);
                             }
 
-                            if (name != null && address != null) {
+                            if (name != null || address != null) {
                                 storeNameAndAddress(name,address);
                                 if (mConnection.isGetNameAddressOnly()) {
                                     unbind = true;
