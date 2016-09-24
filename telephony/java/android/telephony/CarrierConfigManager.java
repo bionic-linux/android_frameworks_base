735516b14e1afe471cf145c55bc4ecc71443161f
Liu Lei <lei.xe.liu@sonymobile.com>
2016-09-24 14:19:20 +0800

    Support wifi calling icon in status bar
    - Add new wifi calling icon in status bar, and it is shown
      when meet below conditions:
      (1) IMS is registered
      (2) IMS feature supports voice-over-wifi or video-over-wifi
    
    - Add new CarrierConfig to show wifi calling icon in status bar.
      The default value is true.
    
    Test: Manual - Wifi calling icon is shown in status bar
    Test: Auto tests pass.
    Bug: 37493651
    Change-Id: I4c891fec37d12b85663c96d171b665e5b26035cd

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -1712,6 +1712,13 @@ public class CarrierConfigManager {
     public static final String KEY_SPN_DISPLAY_RULE_USE_ROAMING_FROM_SERVICE_STATE_BOOL =
             "spn_display_rule_use_roaming_from_service_state_bool";
 
+    /**
+     * Controls whether to show wifi calling icon in statusbar when wifi calling is available.
+     * @hide
+     */
+    public static final String KEY_SHOW_WIFI_CALLING_ICON_IN_STATUS_BAR_BOOL =
+            "show_wifi_calling_icon_in_status_bar_bool";
+
     /** The default value for every variable. */
     private final static PersistableBundle sDefaults;
 
@@ -1995,6 +2002,7 @@ public class CarrierConfigManager {
         sDefaults.putBoolean(KEY_IDENTIFY_HIGH_DEFINITION_CALLS_IN_CALL_LOG_BOOL, false);
         sDefaults.putBoolean(KEY_SPN_DISPLAY_RULE_USE_ROAMING_FROM_SERVICE_STATE_BOOL, false);
         sDefaults.putBoolean(KEY_ALWAYS_SHOW_DATA_RAT_ICON_BOOL, false);
+        sDefaults.putBoolean(KEY_SHOW_WIFI_CALLING_ICON_IN_STATUS_BAR_BOOL, true);
     }
 
     /**
