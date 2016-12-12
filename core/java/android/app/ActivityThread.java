51e40727524547967f7b940c65f2bfcdc6627ace
Jesse Hall <jessehall@google.com>
2016-12-12 12:53:02 -0800

    Create GraphicsEnvironment for communicating with driver loaders
    The GraphicsEnvironment class is given information during application
    start, and makes it available to EGL/GLES/Vulkan loaders that don't
    have easy access to the VM or to the application Context. Currently
    only the driver path is handled, but the existing support for setting
    library paths (for Vulkan extensions) and cache directory information
    should move here.
    
    Bug: 33531483
    Change-Id: I4e4e7fb21f1bcc67122e9173514af5f18c063991

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -62,6 +62,7 @@ import android.os.Bundle;
 import android.os.Debug;
 import android.os.DropBoxManager;
 import android.os.Environment;
+import android.os.GraphicsEnvironment;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.LocaleList;
@@ -4968,7 +4969,7 @@ public final class ActivityThread {
         WindowManagerGlobal.getInstance().trimMemory(level);
     }
 
-    private void setupGraphicsSupport(LoadedApk info, File cacheDir) {
+    private void setupGraphicsSupport(Context context, File cacheDir) {
         if (Process.isIsolated()) {
             // Isolated processes aren't going to do UI.
             return;
@@ -4983,6 +4984,7 @@ public final class ActivityThread {
             if (packages != null && packages.length == 1) {
                 ThreadedRenderer.setupDiskCache(cacheDir);
                 RenderScriptCacheDir.setupDiskCache(cacheDir);
+                GraphicsEnvironment.setupGraphicsEnvironment(context);
             }
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
@@ -5270,7 +5272,7 @@ public final class ActivityThread {
             final Context deviceContext = appContext.createDeviceProtectedStorageContext();
             final File codeCacheDir = deviceContext.getCodeCacheDir();
             if (codeCacheDir != null) {
-                setupGraphicsSupport(data.info, codeCacheDir);
+                setupGraphicsSupport(appContext, codeCacheDir);
             } else {
                 Log.e(TAG, "Unable to setupGraphicsSupport due to missing code-cache directory");
             }
