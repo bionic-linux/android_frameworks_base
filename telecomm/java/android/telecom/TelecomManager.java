4cde7f92ae535e9ae7993d3a49b81f2afeeb14d6
Hall Liu <hallliu@google.com>
2017-01-25 17:12:49 -0800

    Introduce APIs for RTT calls (part 1)
    Add signaling methods and data pipes for handling real-time text during
    a call.
    
    Change-Id: I876827c448252c5f786d7a4919c47891acb03877
    Test: manual, through telecom testapps

--- telecomm/java/android/telecom/TelecomManager.java
+++ telecomm/java/android/telecom/TelecomManager.java
@@ -317,6 +317,14 @@ public class TelecomManager {
     public static final String EXTRA_CALL_BACK_NUMBER = "android.telecom.extra.CALL_BACK_NUMBER";
 
     /**
+     * A boolean extra, which when set on the {@link Intent#ACTION_CALL} intent or on the bundle
+     * passed into {@link #placeCall(Uri, Bundle)}, indicates that the call should be initiated with
+     * an RTT session open. See {@link android.telecom.Call.RttCall} for more information on RTT.
+     */
+    public static final String EXTRA_START_CALL_WITH_RTT =
+            "android.telecom.extra.START_CALL_WITH_RTT";
+
+    /**
      * A boolean meta-data value indicating whether an {@link InCallService} implements an
      * in-call user interface. Dialer implementations (see {@link #getDefaultDialerPackage()}) which
      * would also like to replace the in-call interface should set this meta-data to {@code true} in
