71f143c0b75ea935050fbcb3545fdcbef3f0550e
Minoru Sakuma <minoru.x.sakuma@sonymobile.com>
2017-10-25 15:52:31 +0900

    Add a new carrier config to show the WFC location privacy policy UI
    Controls whether to show the privacy policy prompt message before
    opening Wi-Fi call settings.
    
    Test: manual - Passed make build
    Bug: 67872298
    
    Change-Id: I58df76ae97b54009b55a7a78fcaec95cd9b27ca6

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -2246,6 +2246,12 @@ public class CarrierConfigManager {
     public static final String KEY_SUPPORT_EMERGENCY_DIALER_SHORTCUT_BOOL =
             "support_emergency_dialer_shortcut_bool";
 
+    /**
+     * Determines whether wifi calling location privacy policy is shown.
+     */
+    public static final String KEY_SHOW_WFC_LOCATION_PRIVACY_POLICY_BOOL =
+            "show_wfc_location_privacy_policy_bool";
+
     /** The default value for every variable. */
     private final static PersistableBundle sDefaults;
 
@@ -2596,6 +2602,7 @@ public class CarrierConfigManager {
         sDefaults.putBoolean(KEY_CALL_WAITING_OVER_UT_WARNING_BOOL, false);
         sDefaults.putBoolean(KEY_SUPPORT_CLIR_NETWORK_DEFAULT_BOOL, true);
         sDefaults.putBoolean(KEY_SUPPORT_EMERGENCY_DIALER_SHORTCUT_BOOL, true);
+        sDefaults.putBoolean(KEY_SHOW_WFC_LOCATION_PRIVACY_POLICY_BOOL, true);
     }
 
     /**
