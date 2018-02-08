bd3eb094f6e1164ab6217e9b38c44ebeeeb4b292
Hunter Stich <hunterstich@gmail.com>
2018-02-08 10:16:28 -0800

    Added missing parethesis in Activity documentation
    Fixed a missing parethesis in Activity.java class documentation under
    the 'Activity Lifecycle' section's table description of onDestroy().
    
    Test: Proofread related section.
    
    Change-Id: I1e91bd76fa89e9c7fe10364fcc196b960e4b5cd5
    Signed-off-by: Hunter Stich <hunterstich@gmail.com>

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -386,7 +386,7 @@ import java.util.List;
  *         <td>The final call you receive before your
  *             activity is destroyed.  This can happen either because the
  *             activity is finishing (someone called {@link Activity#finish} on
- *             it, or because the system is temporarily destroying this
+ *             it), or because the system is temporarily destroying this
  *             instance of the activity to save space.  You can distinguish
  *             between these two scenarios with the {@link
  *             Activity#isFinishing} method.</td>
