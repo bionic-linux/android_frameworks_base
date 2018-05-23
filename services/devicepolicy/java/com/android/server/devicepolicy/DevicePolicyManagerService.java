6638ef3cb3d6b1b2d6e29608e12e953565863991
kwangju.chae <kwangju.chae@lge.com>
2018-05-23 12:49:36 +0900

    Google FRP : save password quality in resetPassword
    Since O MR1,
    when Google FRP is challenged,
    pattern/PIN/password which set before erase can replace account verification.
    But in case of remote lock by resetPassword, verification step is skipped.
    
    Reproducible path:
    1. Create Google account.
    2. Remote lock & Erase in Google Find My Device.
    
    The reason is because saving password quality is missed in resetPassword.
    This patch is to fix that.
    
    Change-Id: I54376f60ac53451ace22965d331b47cd8c2e614e

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -4440,7 +4440,9 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
         try {
             if (token == null) {
                 if (!TextUtils.isEmpty(password)) {
-                    mLockPatternUtils.saveLockPassword(password, null, quality, userHandle);
+                    mLockPatternUtils.saveLockPassword(
+                            password, null,
+                            Math.max(quality, getPasswordQuality(password)), userHandle);
                 } else {
                     mLockPatternUtils.clearLock(null, userHandle);
                 }
@@ -4449,7 +4451,8 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
                 result = mLockPatternUtils.setLockCredentialWithToken(password,
                         TextUtils.isEmpty(password) ? LockPatternUtils.CREDENTIAL_TYPE_NONE
                                 : LockPatternUtils.CREDENTIAL_TYPE_PASSWORD,
-                                quality, tokenHandle, token, userHandle);
+                                Math.max(quality, getPasswordQuality(password)),
+                                tokenHandle, token, userHandle);
             }
             boolean requireEntry = (flags & DevicePolicyManager.RESET_PASSWORD_REQUIRE_ENTRY) != 0;
             if (requireEntry) {
@@ -4467,6 +4470,17 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
             mInjector.binderRestoreCallingIdentity(ident);
         }
         return result;
+    }
+
+    private int getPasswordQuality(@NonNull String password) {
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (!('0' <= c && c <= '9')) {
+                return DevicePolicyManager.PASSWORD_QUALITY_ALPHABETIC;
+            }
+        }
+
+        return DevicePolicyManager.PASSWORD_QUALITY_NUMERIC;
     }
 
     private boolean isLockScreenSecureUnchecked(int userId) {
