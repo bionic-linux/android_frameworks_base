ba7b9f4980a708cac33a34b99e5b30f8132b9d67
taiki tsutsumi <taiki.tsutsumi@sony.com>
2018-05-30 13:50:16 +0900

    Add CarrierConfig to hide "Network default" option in Caller ID setting
    Add a key to hide "Network default". When value is false,
    "Network default" shouldn't be displayed for options in Caller ID
    setting.
    
    Test: manual - check options in Caller ID setting
    Bug:
    Change-Id: Id07ab2d2281a1ebb07d631045ada705cda53daf2

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -2105,6 +2105,13 @@ public class CarrierConfigManager {
     public static final String KEY_CALL_REDIRECTION_SERVICE_COMPONENT_NAME_STRING =
             "call_redirection_service_component_name_string";
 
+    /**
+     * Flag indicating whether to support "Network default" option in Caller ID settings for Calling
+     * Line Identification Restriction (CLIR).
+     */
+    public static final String KEY_SUPPORT_CLIR_NETWORK_DEFAULT_BOOL =
+            "support_clir_network_default_bool";
+
     /** The default value for every variable. */
     private final static PersistableBundle sDefaults;
 
@@ -2442,6 +2449,7 @@ public class CarrierConfigManager {
                 });
         sDefaults.putString(KEY_WCDMA_DEFAULT_SIGNAL_STRENGTH_MEASUREMENT_STRING, "");
         sDefaults.putBoolean(KEY_CONFIG_SHOW_ORIG_DIAL_STRING_FOR_CDMA_BOOL, false);
+        sDefaults.putBoolean(KEY_SUPPORT_CLIR_NETWORK_DEFAULT_BOOL, true);
     }
 
     /**
