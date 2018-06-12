606eeecd7cc0c64f18b0b617c5392203abb25531
Neil Fuller <nfuller@google.com>
2018-06-12 21:06:20 +0100

    Add more behavior to TimeDetectorService
    The client code now "suggests" time updates
    to the time detection service. The current implementation
    of the time detection service will validate and set the
    device system clock as needed. In future it will ignore
    these suggestions if better information is available.
    
    Responsibility for sending the
    TelephonyIntents.ACTION_NETWORK_SET_TIME intent has
    been moved to the time detection service until it can be
    removed or replaced.
    
    The telephony code is still responsible for basic rate
    limiting but the majority of the decision logic has been
    moved to the service.
    
    There is an associated change in telephony code to switch to
    using the server.
    
    Bug: 78217059
    Test: atest FrameworksServicesTests:com.android.server.timedetector
    Test: atest FrameworksCoreTests:android.util.TimestampedValueTest
    Change-Id: I4f7a10ac06b2d32da22689e1ddf309e0a2795f30

--- services/tests/servicestests/src/com/android/server/timedetector/TimeDetectorServiceTest.java
+++ services/tests/servicestests/src/com/android/server/timedetector/TimeDetectorServiceTest.java
@@ -16,6 +16,10 @@
 
 package com.android.server.timedetector;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
@@ -23,36 +27,44 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import android.app.timedetector.TimeSignal;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.HandlerThread;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.TimestampedValue;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.android.server.timedetector.TimeDetectorStrategy.Callback;
+
+import java.io.PrintWriter;
+
 @RunWith(AndroidJUnit4.class)
 public class TimeDetectorServiceTest {
 
-    private TimeDetectorService mTimeDetectorService;
-
     private Context mMockContext;
-    private TimeDetectorStrategy mMockTimeDetectorStrategy;
+    private StubbedTimeDetectorStrategy mStubbedTimeDetectorStrategy;
+    private HandlerThread mHandlerThread;
+    private Callback mMockCallback;
+
+    private TimeDetectorService mTimeDetectorService;
 
     @Before
     public void setUp() {
         mMockContext = mock(Context.class);
-        mMockTimeDetectorStrategy = mock(TimeDetectorStrategy.class);
-        mTimeDetectorService = new TimeDetectorService(mMockContext, mMockTimeDetectorStrategy);
-    }
+        mMockCallback = mock(Callback.class);
+        mStubbedTimeDetectorStrategy = new StubbedTimeDetectorStrategy();
+        mHandlerThread = new HandlerThread("TimeDetectorServiceTest");
+        mHandlerThread.start();
 
-    @After
-    public void tearDown() {
-        verifyNoMoreInteractions(mMockContext, mMockTimeDetectorStrategy);
+        mTimeDetectorService = new TimeDetectorService(
+                mMockContext, mHandlerThread.getThreadHandler(), mMockCallback,
+                mStubbedTimeDetectorStrategy);
     }
 
     @Test(expected=SecurityException.class)
@@ -63,6 +75,7 @@ public class TimeDetectorServiceTest {
 
         try {
             mTimeDetectorService.suggestTime(timeSignal);
+            waitForHandler(mHandlerThread);
         } finally {
             verify(mMockContext).enforceCallingPermission(
                     eq(android.Manifest.permission.SET_TIME), anyString());
@@ -75,14 +88,109 @@ public class TimeDetectorServiceTest {
 
         TimeSignal timeSignal = createNitzTimeSignal();
         mTimeDetectorService.suggestTime(timeSignal);
+        waitForHandler(mHandlerThread);
 
         verify(mMockContext)
                 .enforceCallingPermission(eq(android.Manifest.permission.SET_TIME), anyString());
-        verify(mMockTimeDetectorStrategy).suggestTime(timeSignal);
+        mStubbedTimeDetectorStrategy.verifySuggestTimeCalled(mHandlerThread, timeSignal);
+    }
+
+    @Test
+    public void testDump() {
+        when(mMockContext.checkCallingOrSelfPermission(android.Manifest.permission.DUMP))
+                .thenReturn(PackageManager.PERMISSION_GRANTED);
+
+        mTimeDetectorService.dump(null, null, null);
+        waitForHandler(mHandlerThread);
+
+        verify(mMockContext).checkCallingOrSelfPermission(eq(android.Manifest.permission.DUMP));
+        mStubbedTimeDetectorStrategy.verifyDumpCalled(mHandlerThread);
+    }
+
+    @Test
+    public void testAutoTimeDetectionToggle() {
+        when(mMockCallback.isTimeDetectionEnabled()).thenReturn(true);
+
+        mTimeDetectorService.handleAutoTimeDetectionToggle();
+        waitForHandler(mHandlerThread);
+
+        mStubbedTimeDetectorStrategy.verifyHandleAutoTimeDetectionToggleCalled(
+                mHandlerThread, true);
+
+        when(mMockCallback.isTimeDetectionEnabled()).thenReturn(false);
+
+        mTimeDetectorService.handleAutoTimeDetectionToggle();
+        waitForHandler(mHandlerThread);
+
+        mStubbedTimeDetectorStrategy.verifyHandleAutoTimeDetectionToggleCalled(
+                mHandlerThread, false);
+    }
+
+    private static void waitForHandler(HandlerThread handlerThread) {
+        // Add an event to the queue. When it completes we know we are done.
+        handlerThread.getThreadHandler().runWithScissors(() -> {}, 0);
     }
 
     private static TimeSignal createNitzTimeSignal() {
         TimestampedValue<Long> timeValue = new TimestampedValue<>(100L, 1_000_000L);
         return new TimeSignal(TimeSignal.SOURCE_ID_NITZ, timeValue);
+    }
+
+    private static class StubbedTimeDetectorStrategy implements TimeDetectorStrategy {
+
+        // Call tracking.
+        private Thread mLastThread;
+        private TimeSignal mLastSuggestedTime;
+        private Boolean mLastAutoTimeDetectionToggle;
+        private boolean mDumpCalled;
+
+        @Override
+        public void initialize(Callback ignored) {
+        }
+
+        @Override
+        public void suggestTime(TimeSignal timeSignal) {
+            resetCallTracking();
+            mLastThread = Thread.currentThread();
+            mLastSuggestedTime = timeSignal;
+        }
+
+        @Override
+        public void handleAutoTimeDetectionToggle(boolean enabled) {
+            resetCallTracking();
+            mLastThread = Thread.currentThread();
+            mLastAutoTimeDetectionToggle = enabled;
+        }
+
+        @Override
+        public void dump(PrintWriter pw, String[] args) {
+            resetCallTracking();
+            mLastThread = Thread.currentThread();
+            mDumpCalled = true;
+        }
+
+        void resetCallTracking() {
+            mLastThread = null;
+            mLastSuggestedTime = null;
+            mLastAutoTimeDetectionToggle = null;
+            mDumpCalled = false;
+        }
+
+        void verifySuggestTimeCalled(Thread expectedThread, TimeSignal expectedSignal) {
+            assertSame(expectedThread, mLastThread);
+            assertEquals(expectedSignal, mLastSuggestedTime);
+        }
+
+        void verifyHandleAutoTimeDetectionToggleCalled(
+                Thread expectedThread, boolean expectedEnable) {
+            assertSame(expectedThread, mLastThread);
+            assertNotNull(mLastAutoTimeDetectionToggle);
+            assertEquals(expectedEnable, mLastAutoTimeDetectionToggle);
+        }
+
+        void verifyDumpCalled(Thread expectedThread) {
+            assertSame(expectedThread, mLastThread);
+            assertTrue(mDumpCalled);
+        }
     }
 }
