dd3db2905d35acb7d1e2c355c30aec8519743a38
Remi NGUYEN VAN <reminv@google.com>
2018-07-04 11:15:56 +0900

    Add util to add an ARP table entry
    This is to be used by the new DhcpServer to add ARP entries with new
    addresses before sending unicast responses.
    
    Test: manual: cat /proc/net/arp with implementation based on this
    Bug: b/109584964
    Change-Id: I3559893583aa3c49b188ad689a41ee2f3e9d9bf3

--- core/java/android/net/NetworkUtils.java
+++ core/java/android/net/NetworkUtils.java
@@ -21,6 +21,7 @@ import android.util.Log;
 import android.util.Pair;
 
 import java.io.FileDescriptor;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
@@ -124,6 +125,17 @@ public class NetworkUtils {
      * @return {@code true} if {@code uid} can access network, {@code false} otherwise.
      */
     public native static boolean queryUserAccess(int uid, int netId);
+
+    /**
+     * Add an entry into the ARP cache.
+     */
+    public static void addArpEntry(Inet4Address ipv4Addr, MacAddress ethAddr, String ifname,
+            FileDescriptor fd) throws IOException {
+        addArpEntry(ethAddr.toByteArray(), ipv4Addr.getAddress(), ifname, fd);
+    }
+
+    private static native void addArpEntry(byte[] ethAddr, byte[] netAddr, String ifname,
+            FileDescriptor fd) throws IOException;
 
     /**
      * Convert a IPv4 address from an integer to an InetAddress.
