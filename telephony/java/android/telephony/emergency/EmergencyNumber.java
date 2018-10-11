5c161a2eb6a76ec2a71c0c1cabd6b5368fa5c43c
sqian <shuoq@google.com>
2018-10-10 19:18:12 -0700

    Compare Emergency number display priority
    Define the rules for emergency numbers that may have higher display
    priority, and make Emergency number comparable for these values. This
    is also essential for sorting the list so that an emergency number with
    higher display priority should be listed in a lower index.
    
    Test: Treehugger, unit test
    Bug: 112657134
    Change-Id: I46fb009ce862ec3c593a05b4b04e180faf198fc4

--- telephony/java/android/telephony/emergency/EmergencyNumber.java
+++ telephony/java/android/telephony/emergency/EmergencyNumber.java
@@ -33,7 +33,7 @@ import java.util.Set;
  * A parcelable class that wraps and retrieves the information of number, service category(s) and
  * country code for a specific emergency number.
  */
-public final class EmergencyNumber implements Parcelable {
+public final class EmergencyNumber implements Parcelable, Comparable<EmergencyNumber> {
 
     private static final String LOG_TAG = "EmergencyNumber";
 
@@ -357,6 +357,63 @@ public final class EmergencyNumber implements Parcelable {
             return false;
         }
         return (o == this || toString().equals(o.toString()));
+    }
+
+    /**
+     * Calculate the rank for display priority.
+     *
+     * A higher display priority score means the emergency number has a higher display priority.
+     * An emergency number has a higher ranking if its source is defined for a higher display
+     * priority.
+     *
+     * The priority of sources are defined as follows:
+     *     EMERGENCY_NUMBER_SOURCE_NETWORK_SIGNALING >
+     *     EMERGENCY_NUMBER_SOURCE_SIM >
+     *     EMERGENCY_NUMBER_SOURCE_DEFAULT >
+     *     EMERGENCY_NUMBER_SOURCE_MODEM_CONFIG
+     *
+     */
+    private int getDisplayPriorityScore() {
+        int score = 0;
+        if (this.isFromSources(EMERGENCY_NUMBER_SOURCE_NETWORK_SIGNALING)) {
+            score += 1 << 4;
+        }
+        if (this.isFromSources(EMERGENCY_NUMBER_SOURCE_SIM)) {
+            score += 1 << 3;
+        }
+        // TODO add a score if the number comes from Google's emergency number database
+        if (this.isFromSources(EMERGENCY_NUMBER_SOURCE_DEFAULT)) {
+            score += 1 << 1;
+        }
+        if (this.isFromSources(EMERGENCY_NUMBER_SOURCE_MODEM_CONFIG)) {
+            score += 1 << 0;
+        }
+        return score;
+    }
+
+    /**
+     * Compare the display priority for this emergency number and the supplied emergency number.
+     *
+     * @param emergencyNumber the supplied emergency number
+     * @return a negative value if the supplied emergency number has a lower display priority;
+     *         a positive value if the supplied emergency number has a higher display priority;
+     *         0 if both have equal display priority.
+     */
+    @Override
+    public int compareTo(EmergencyNumber emergencyNumber) {
+        if (this.getDisplayPriorityScore()
+                > emergencyNumber.getDisplayPriorityScore()) {
+            return -1;
+        } else if (this.getDisplayPriorityScore()
+                < emergencyNumber.getDisplayPriorityScore()) {
+            return 1;
+        } else {
+            /**
+             * TODO if both numbers have the same display priority score, the number matches the
+             * Google's emergency number database has a higher display priority.
+             */
+            return 0;
+        }
     }
 
     public static final Parcelable.Creator<EmergencyNumber> CREATOR =
