c9585f34b89300740ee89dd72cc2a681932701ff
Paul Duffin <paulduffin@google.com>
2018-10-11 15:21:46 +0100

    Add InetAddresses class to API
    Adds methods needed by app developers to avoid them having to call
    non-SDK APIs.
    
    Test: tests added to cts
    Bug: 78686891
    Change-Id: Ic0eadfdab2b111d5ca4becb753f4fe3557806433

---
+++ core/java/android/net/InetAddresses.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net;
+
+import android.annotation.Nullable;
+
+import java.net.Inet6Address;
+import java.net.InetAddress;
+
+/**
+ * Utility methods for {@link InetAddress} implementations.
+ */
+public class InetAddresses {
+
+    private InetAddresses() {}
+
+    /**
+     * Returns an InetAddress corresponding to the given numeric address (such
+     * as {@code "192.168.0.1"} or {@code "2001:4860:800d::68"}).
+     *
+     * <p>This method will never do a DNS lookup. A null or empty string will return an instance
+     * of {@link Inet6Address} representing the loopback address. Other non-numeric addresses will
+     * be treated as errors.
+     *
+     * @param numericAddress the address to parse, must be numeric, null or an empty string.
+     * @throws IllegalArgumentException if {@code numericAddress} is not null, empty or a numeric
+     * address
+     */
+    public static InetAddress parseNumericAddress(@Nullable String numericAddress) {
+        return InetAddress.parseNumericAddress(numericAddress);
+    }
+}
