1b5cc902dbe48407bf7c6abec95f2d226a2bf764
Benedict Wong <benedictwong@google.com>
2018-11-14 17:53:19 -0800

    Implement TestNetworkService
    This change adds the relevant JNI implementations, as well as the
    implementations and NetworkAgent tracking code in the TestNetworkService.
    
    Bug: 72950854
    Test: CTS tests passing
    Change-Id: Ie66ba631a548b5f9c6b5ed0797582f86688debe5

--- services/core/java/com/android/server/TestNetworkService.java
+++ services/core/java/com/android/server/TestNetworkService.java
@@ -18,19 +18,41 @@ package com.android.server;
 
 import static com.android.internal.util.Preconditions.checkNotNull;
 
+import android.app.AppOpsManager;
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.net.INetd;
 import android.net.ITestNetworkManager;
+import android.net.IpPrefix;
 import android.net.LinkAddress;
+import android.net.LinkProperties;
+import android.net.NetworkAgent;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.net.NetworkInfo.DetailedState;
+import android.net.NetworkMisc;
+import android.net.RouteInfo;
+import android.net.StringNetworkSpecifier;
 import android.net.util.NetdService;
+import android.os.Binder;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.INetworkManagementService;
+import android.os.Looper;
 import android.os.ParcelFileDescriptor;
+import android.os.RemoteException;
 import android.util.Log;
+import android.util.SparseArray;
 
 import com.android.internal.annotations.VisibleForTesting;
+
+import java.io.UncheckedIOException;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 
 /** @hide */
 class TestNetworkService extends ITestNetworkManager.Stub {
@@ -43,6 +65,10 @@ class TestNetworkService extends ITestNetworkManager.Stub {
 
     private final HandlerThread mHandlerThread;
     private final Handler mHandler;
+
+    private static native int jniCreateTun(String intfName);
+
+    private static native void jniTeardownTun(String intfName);
 
     @VisibleForTesting
     protected TestNetworkService(Context context, INetworkManagementService netManager) {
@@ -66,7 +92,148 @@ class TestNetworkService extends ITestNetworkManager.Stub {
     @Override
     public synchronized ParcelFileDescriptor buildTun(
             String intfName, LinkAddress[] linkAddrs, String callingPackage) {
-        return null;
+        enforceTestNetworkPermissions(mContext, callingPackage);
+
+        int callingUid = Binder.getCallingUid();
+        long token = Binder.clearCallingIdentity();
+        try {
+            ParcelFileDescriptor tunIntf = ParcelFileDescriptor.adoptFd(jniCreateTun(intfName));
+            for (LinkAddress addr : linkAddrs) {
+                mNetd.interfaceAddAddress(
+                        intfName, addr.getAddress().getHostAddress(), addr.getPrefixLength());
+            }
+
+            return tunIntf;
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        } finally {
+            Binder.restoreCallingIdentity(token);
+        }
+    }
+
+    // Tracker for TestNetworkAgents
+    private static SparseArray<TestNetworkAgent> sTestNetworkTracker = new SparseArray<>();
+
+    public static class TestNetworkAgent extends NetworkAgent implements IBinder.DeathRecipient {
+        private static final int NETWORK_SCORE = 1; // Use a low, non-zero score.
+
+        private final int mUid;
+        private final NetworkInfo mNi;
+        private final NetworkCapabilities mNc;
+        private final LinkProperties mLp;
+
+        private IBinder mBinder;
+
+        private TestNetworkAgent(
+                Looper looper,
+                Context context,
+                NetworkInfo ni,
+                NetworkCapabilities nc,
+                LinkProperties lp,
+                NetworkMisc networkMisc,
+                int uid,
+                IBinder binder) {
+            super(looper, context, TEST_NETWORK_TYPE, ni, nc, lp, NETWORK_SCORE, networkMisc);
+
+            mUid = uid;
+            mNi = ni;
+            mNc = nc;
+            mLp = lp;
+            mBinder = binder; // Binder null-checks in create()
+
+            try {
+                mBinder.linkToDeath(this, 0);
+            } catch (RemoteException e) {
+                binderDied();
+            }
+
+            sTestNetworkTracker.put(this.netId, this);
+        }
+
+        /**
+         * If the Binder object dies, this function is called to free the resources of this
+         * TestNetworkAgent
+         */
+        @Override
+        public void binderDied() {
+            teardown();
+        }
+
+        @Override
+        protected void unwanted() {
+            teardown();
+        }
+
+        private synchronized void teardown() {
+            if (mBinder != null) {
+                mNi.setDetailedState(DetailedState.DISCONNECTED, null, null);
+                mNi.setIsAvailable(false);
+                sendNetworkInfo(mNi);
+
+                mBinder.unlinkToDeath(this, 0);
+                mBinder = null;
+
+                sTestNetworkTracker.remove(this.netId);
+            }
+        }
+    }
+
+    private static TestNetworkAgent registerTestNetworkAgent(
+            Looper looper, Context context, String intfName, int callingUid, IBinder binder)
+            throws SocketException {
+        if (binder == null) {
+            throw new IllegalArgumentException(
+                    "Must pass a binder to ensure cleanup of TestNetworkAgent");
+        }
+
+        // Build network info with special testing type, and a network score of 0
+        NetworkInfo ni = new NetworkInfo(ConnectivityManager.TYPE_TEST, 0, TEST_NETWORK_TYPE, "");
+        ni.setDetailedState(DetailedState.CONNECTED, null, null);
+        ni.setIsAvailable(true);
+
+        // Build narrow set of NetworkCapabilities, useful only for testing
+        NetworkCapabilities nc = new NetworkCapabilities();
+        nc.clearAll(); // Remove default capabilities.
+        nc.addTransportType(NetworkCapabilities.TRANSPORT_TEST);
+        nc.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_SUSPENDED);
+        nc.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_RESTRICTED);
+        nc.setSingleUid(callingUid);
+        nc.setLinkUpstreamBandwidthKbps(1);
+        nc.setLinkDownstreamBandwidthKbps(1);
+        nc.setNetworkSpecifier(new StringNetworkSpecifier(intfName));
+
+        // Build LinkProperties
+        LinkProperties lp = new LinkProperties();
+        lp.setInterfaceName(intfName);
+
+        // Find the currently assigned addresses, and add them to LinkProperties
+        boolean allowIPv4 = false, allowIPv6 = false;
+        NetworkInterface netIntf = NetworkInterface.getByName(intfName);
+        for (InterfaceAddress intfAddr : netIntf.getInterfaceAddresses()) {
+            lp.addLinkAddress(
+                    new LinkAddress(intfAddr.getAddress(), intfAddr.getNetworkPrefixLength()));
+
+            if (intfAddr.getAddress() instanceof Inet6Address) {
+                allowIPv6 = true;
+            } else {
+                allowIPv4 = true;
+            }
+        }
+
+        // Add global routes (but as non-default, non-internet providing, single-UID network)
+        if (allowIPv4) {
+            lp.addRoute(new RouteInfo(new IpPrefix(Inet4Address.ANY, 0), null, intfName));
+        }
+        if (allowIPv6) {
+            lp.addRoute(new RouteInfo(new IpPrefix(Inet6Address.ANY, 0), null, intfName));
+        }
+
+        // Allow bypass so that traffic is routed correctly when multiple test networks are stacked.
+        // This is useful for VPN and IPsec testing.
+        NetworkMisc networkMisc = new NetworkMisc();
+        networkMisc.allowBypass = true;
+
+        return new TestNetworkAgent(looper, context, ni, nc, lp, networkMisc, callingUid, binder);
     }
 
     /**
@@ -76,9 +243,50 @@ class TestNetworkService extends ITestNetworkManager.Stub {
      */
     @Override
     public synchronized void buildTestNetwork(
-            String intfName, IBinder binder, String callingPackage) {}
+            String intfName, IBinder binder, String callingPackage) {
+        enforceTestNetworkPermissions(mContext, callingPackage);
+        int callingUid = Binder.getCallingUid();
+
+        // Most of this needs to be done with NETWORK_STACK privileges.
+        long token = Binder.clearCallingIdentity();
+        try {
+            mNMS.setInterfaceUp(intfName);
+            registerTestNetworkAgent(mHandler.getLooper(), mContext, intfName, callingUid, binder);
+        } catch (SocketException e) {
+            throw new UncheckedIOException(e);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        } finally {
+            Binder.restoreCallingIdentity(token);
+        }
+    }
 
     /** Teardown a test network */
     @Override
-    public synchronized void teardownTestNetwork(int netId, String callingPackage) {}
+    public synchronized void teardownTestNetwork(int netId, String callingPackage) {
+        enforceTestNetworkPermissions(mContext, callingPackage);
+
+        TestNetworkAgent agent = sTestNetworkTracker.get(netId);
+        if (agent == null) {
+            return;
+        } else if (agent.mUid != Binder.getCallingUid()) {
+            throw new SecurityException("Attempted to modify other user's test networks");
+        }
+
+        agent.teardown();
+    }
+
+    /** Enforce that the caller has AppOp OP_MANAGE_TEST_NETWORKS. */
+    public static void enforceTestNetworkPermissions(Context context, String callingPackage) {
+        checkNotNull(callingPackage, "Null calling package cannot create Test Networks");
+
+        AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        switch (appOps.noteOp(
+                AppOpsManager.OPSTR_MANAGE_TEST_NETWORKS, Binder.getCallingUid(), callingPackage)) {
+            case AppOpsManager.MODE_ALLOWED:
+                return;
+            default:
+                throw new SecurityException("AppOp required for test network creation");
+        }
+    }
 }
---
+++ services/core/jni/com_android_server_TestNetworkService.cpp
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_NDEBUG 0
+
+#define LOG_TAG "TestNetworkServiceJni"
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <linux/ipv6_route.h>
+#include <linux/route.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <log/log.h>
+
+#include "netutils/ifc.h"
+
+#include "jni.h"
+#include <nativehelper/JNIHelp.h>
+
+namespace android {
+
+static int inet4 = -1;
+
+//------------------------------------------------------------------------------
+
+#define SYSTEM_ERROR (-1)
+#define BAD_ARGUMENT (-2)
+
+static int create_tun_interface(const char* name) {
+    int tun = open("/dev/tun", O_RDWR | O_NONBLOCK);
+
+    ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+
+    // Allocate interface.
+    ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
+    strncpy(ifr.ifr_name, name, IFNAMSIZ);
+    if (ioctl(tun, TUNSETIFF, &ifr)) {
+        ALOGE("Cannot allocate TUN: %s", strerror(errno));
+        goto error;
+    }
+
+    // Activate interface.
+    ifr.ifr_flags = IFF_UP;
+    if (ioctl(inet4, SIOCSIFFLAGS, &ifr)) {
+        ALOGE("Cannot activate %s: %s", ifr.ifr_name, strerror(errno));
+        goto error;
+    }
+
+    return tun;
+
+error:
+    close(tun);
+    return SYSTEM_ERROR;
+}
+
+static int reset_tun_interface(const char* name) {
+    ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+
+    ifr.ifr_flags = 0;
+    strncpy(ifr.ifr_name, name, IFNAMSIZ);
+    if (ioctl(inet4, SIOCSIFFLAGS, &ifr) && errno != ENODEV) {
+        ALOGE("Cannot reset %s: %s", name, strerror(errno));
+        return SYSTEM_ERROR;
+    }
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+
+static void throwException(JNIEnv* env, int error, const char* message) {
+    if (error == SYSTEM_ERROR) {
+        jniThrowException(env, "java/lang/IllegalStateException", message);
+    } else {
+        jniThrowException(env, "java/lang/IllegalArgumentException", message);
+    }
+}
+
+static jint create(JNIEnv* env, jobject /* thiz */, jstring jName) {
+    const char* name = jName ? env->GetStringUTFChars(jName, NULL) : NULL;
+    if (!name) {
+        jniThrowNullPointerException(env, "name");
+        return -1;
+    }
+
+    int tun = create_tun_interface(name);
+    if (tun < 0) {
+        throwException(env, tun, "Cannot create interface");
+        return -1;
+    }
+    return tun;
+}
+
+static void reset(JNIEnv* env, jobject /* thiz */, jstring jName) {
+    const char* name = jName ? env->GetStringUTFChars(jName, NULL) : NULL;
+    if (!name) {
+        jniThrowNullPointerException(env, "name");
+        return;
+    }
+    if (reset_tun_interface(name) < 0) {
+        throwException(env, SYSTEM_ERROR, "Cannot reset interface");
+    }
+    env->ReleaseStringUTFChars(jName, name);
+}
+
+//------------------------------------------------------------------------------
+
+static const JNINativeMethod gMethods[] = {
+    {"jniCreateTun", "(Ljava/lang/String;)I", (void*)create},
+    {"jniTeardownTun", "(Ljava/lang/String;)V", (void*)reset},
+};
+
+int register_android_server_TestNetworkService(JNIEnv* env) {
+    if (inet4 == -1) {
+        inet4 = socket(AF_INET, SOCK_DGRAM, 0);
+    }
+
+    return jniRegisterNativeMethods(env, "com/android/server/TestNetworkService", gMethods,
+                                    NELEM(gMethods));
+}
+
+}; // namespace android
