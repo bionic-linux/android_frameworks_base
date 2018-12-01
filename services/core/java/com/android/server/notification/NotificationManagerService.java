d695bc91c8a4566c0f8a911509e1632456fbc976
Terigele <terigele@huawei.com>
2018-12-01 12:01:49 +0800

    NMS:change save policy in other HandlerThread Test: change notification policy
    Change-Id: Ic1ca52ca3373533c8b95587db13e59e2dfd06321

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -574,8 +574,7 @@ public class NotificationManagerService extends SystemService {
     }
 
     public void savePolicyFile() {
-        mHandler.removeMessages(MESSAGE_SAVE_POLICY_FILE);
-        mHandler.sendEmptyMessage(MESSAGE_SAVE_POLICY_FILE);
+        mRankingHandler.requestSavePolicyFile();
     }
 
     private void handleSavePolicyFile() {
@@ -5333,9 +5332,6 @@ public class NotificationManagerService extends SystemService {
                 case MESSAGE_FINISH_TOKEN_TIMEOUT:
                     handleKillTokenTimeout((IBinder)msg.obj);
                     break;
-                case MESSAGE_SAVE_POLICY_FILE:
-                    handleSavePolicyFile();
-                    break;
                 case MESSAGE_SEND_RANKING_UPDATE:
                     handleSendRankingUpdate();
                     break;
@@ -5372,6 +5368,9 @@ public class NotificationManagerService extends SystemService {
                 case MESSAGE_RANKING_SORT:
                     handleRankingSort();
                     break;
+                case MESSAGE_SAVE_POLICY_FILE:
+                    handleSavePolicyFile();
+                    break;
             }
         }
 
@@ -5379,6 +5378,13 @@ public class NotificationManagerService extends SystemService {
             removeMessages(MESSAGE_RANKING_SORT);
             Message msg = Message.obtain();
             msg.what = MESSAGE_RANKING_SORT;
+            sendMessage(msg);
+        }
+
+        public void requestSavePolicyFile() {
+            removeMessages(MESSAGE_SAVE_POLICY_FILE);
+            Message msg = Message.obtain();
+            msg.what = MESSAGE_SAVE_POLICY_FILE;
             sendMessage(msg);
         }
 
