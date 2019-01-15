56b007e71e525121f40af6c7eaeebaa01947e3bc
Hector Dearman <hjd@google.com>
2019-01-15 14:12:28 +0000

    Remove DropBoxManager::getNextEntry
    - Remove DropBoxManager::getNextEntry as the method was unused.
    - Stop DropBoxManager::Entry inheriting from RefBase.
      This was causing bugs since DropBoxManager constructs Entry
      on the stack (in a non-ref counted context).
    
    Bug: 119655216
    Test: make
    Merged-In: If0852ab6bcc1a92560132628897977eac16fddea

--- libs/services/src/os/DropBoxManager.cpp
+++ libs/services/src/os/DropBoxManager.cpp
@@ -228,15 +228,4 @@ DropBoxManager::add(const Entry& entry)
     return service->add(entry);
 }
 
-Status
-DropBoxManager::getNextEntry(const String16& tag, long msec, Entry* entry)
-{
-    sp<IDropBoxManagerService> service = interface_cast<IDropBoxManagerService>(
-        defaultServiceManager()->getService(android::String16("dropbox")));
-    if (service == NULL) {
-        return Status::fromExceptionCode(Status::EX_NULL_POINTER, "can't find dropbox service");
-    }
-    return service->getNextEntry(tag, msec, entry);
-}
-
 }} // namespace android::os
