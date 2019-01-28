b86463c94dddae5bb5fdb00acc5b6b2437ab64d2
Ye Jiao <ye.jiao@mediatek.com>
2019-01-28 12:54:22 +0800

    Fix getWifiManager exception if phone is encrypted
    [Issue Details]
    NetworkPolicyManagerService may invoke
    dispatchRestrictBackgroundChanged after reboot if settings changed, and
    invokes untetherAll in turn, which triggers ServiceNotFoundException
    indirectly if the device is under encryption. The cause is that
    WifiService is not running if the device is still locked.
    
    [Solution]
    In order to avoid ServiceNotFoundException in setWifiTethering, we add
    check on return value of getWifiManager. Moreover, check of UsbManager
    is also added to prevent potential similar issues of USB tethering.
    
    Bug: 123504004
    Test: FrameworksNetTests
      adb shell am instrument -w com.android.frameworks.tests.net
    Test: CTS - CtsNetTestCases
      ANDROID_BUILD_droid-cts/tools/cts-tradefed run cts -m CtsNetTestCases
    Test: CTS - CtsNetTestCasesLegacyApi22
      ANDROID_BUILD_droid-cts/tools/cts-tradefed run cts -m
      CtsNetTestCasesLegacyApi22
    Test: CTS - CtsNetTestCasesLegacyPermission22
      ANDROID_BUILD_droid-cts/tools/cts-tradefed run cts -m
      CtsNetTestCasesLegacyPermission22
    
    Change-Id: I2643188b51900f97ba58c8614c66cb55ebfaa2f6

--- services/core/java/com/android/server/connectivity/Tethering.java
+++ services/core/java/com/android/server/connectivity/Tethering.java
@@ -427,16 +427,22 @@ public class Tethering extends BaseNetworkObserver {
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mPublicSync) {
-                mWifiTetherRequested = enable;
                 final WifiManager mgr = getWifiManager();
+                if (mgr == null) {
+                    mLog.e("setWifiTethering: failed to get WifiManager!");
+                    return TETHER_ERROR_SERVICE_UNAVAIL;
+                }
                 if ((enable && mgr.startSoftAp(null /* use existing wifi config */)) ||
                     (!enable && mgr.stopSoftAp())) {
-                    rval = TETHER_ERROR_NO_ERROR;
+                    mWifiTetherRequested = enable;
+                    return TETHER_ERROR_NO_ERROR;
                 }
+                return TETHER_ERROR_MASTER_ERRROR;
             }
         } finally {
             Binder.restoreCallingIdentity(ident);
         }
+
         return rval;
     }
 
@@ -989,6 +995,11 @@ public class Tethering extends BaseNetworkObserver {
     public int setUsbTethering(boolean enable) {
         if (VDBG) Log.d(TAG, "setUsbTethering(" + enable + ")");
         UsbManager usbManager = (UsbManager) mContext.getSystemService(Context.USB_SERVICE);
+        if (usbManager == null) {
+            mLog.e("setUsbTethering: failed to get UsbManager!");
+            return TETHER_ERROR_SERVICE_UNAVAIL;
+        }
+
         synchronized (mPublicSync) {
             usbManager.setCurrentFunctions(enable ? UsbManager.FUNCTION_RNDIS
                     : UsbManager.FUNCTION_NONE);
