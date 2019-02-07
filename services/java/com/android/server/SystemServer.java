8cebd2e8914a6f992378668c71f92d304f2df902
David Anderson <dvander@google.com>
2019-02-07 14:07:23 -0800

    Don't overwrite the host FRP when running a GSI.
    Bug: 123777418
    Test: manual test
    Change-Id: Ia251dd939af71126a8236bc5c5cfed6622b2c946

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -244,6 +244,8 @@ public final class SystemServer {
     private static final String UNCRYPT_PACKAGE_FILE = "/cache/recovery/uncrypt_file";
     private static final String BLOCK_MAP_FILE = "/cache/recovery/block.map";
 
+    private static final String IS_GSI_RUNNING_FILE = "/metadata/gsi/booted";
+
     // maximum number of binder threads used for system_server
     // will be higher than the system default
     private static final int sMaxBinderThreads = 31;
@@ -1076,7 +1078,8 @@ public final class SystemServer {
             traceEnd();
 
             final boolean hasPdb = !SystemProperties.get(PERSISTENT_DATA_BLOCK_PROP).equals("");
-            if (hasPdb) {
+            final boolean isRunningGsi = new File(IS_GSI_RUNNING_FILE).exists();
+            if (hasPdb && !isRunningGsi) {
                 traceBeginAndSlog("StartPersistentDataBlock");
                 mSystemServiceManager.startService(PersistentDataBlockService.class);
                 traceEnd();
