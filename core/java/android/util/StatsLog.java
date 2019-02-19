23e17fd169eb39472193aa6483e94ef684363cb5
Chiachang Wang <chiachangwang@google.com>
2019-02-19 19:36:27 +0800

    Support empty StatsLog.write
    Mainline module will not able to access @hide classes or
    functions. Support an epmty API to resolve the dependency
    readiness before formal AP ready
    
    Bug: 120452078
    Test: Build pass
    
    Change-Id: I46f91851482ec6b18db755c2659e18c868f7bd9f
    Merged-In: I5a68e7e093e63cc87eae753068183215a97e022d

--- core/java/android/util/StatsLog.java
+++ core/java/android/util/StatsLog.java
@@ -16,6 +16,7 @@
 
 package android.util;
 
+import android.annotation.NonNull;
 import android.os.IStatsManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -112,5 +113,15 @@ public final class StatsLog extends StatsLogInternal {
         }
         sService = IStatsManager.Stub.asInterface(ServiceManager.getService("stats"));
         return sService;
+    }
+
+    /**
+     * Add a log to the stats log.
+     *
+     * @param id     The id of the atom
+     * @param params The parameters of the atom's message.
+     */
+    public static void write(int id, @NonNull Object... params) {
+       // Temporary API. Will be removed later. See b/124613085.
     }
 }
