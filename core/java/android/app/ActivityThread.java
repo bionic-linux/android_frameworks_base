0cee02531f6b2636358b4db86bcbbbb3232dd942
katao <katao@bytedance.com>
2019-03-12 18:57:20 +0800

    Fixes crash (race cond) when destroyActivity.
    some UI stress (or sleep on UI thread) causes a crash
    from DESTROY_TIMEOUT_MSG.
    The steps that crash the app are:
    1. build two activities ,such as A and B;
    2. A start B;
    3. B(oncreate) start A use FLAG_ACTIVITY_CLEAR_TOP;
    4. B(oncreate) stall UI thread (Thread.sleep, heavy task)
    5. crash tring to handleLaunchActivity or handleResumeActivity
    
    Test: manual - write an app to jump between two activities, then
    Thread.sleep();
    Signed-off-by: katao <katao@bytedance.com>
    
    Change-Id: I4027dfc0cf3878c12964cabee54ad82bc108d79c

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -3156,7 +3156,7 @@ public final class ActivityThread extends ClientTransactionHandler {
 
         final Activity a = performLaunchActivity(r, customIntent);
 
-        if (a != null) {
+        if (a != null && !r.finishing) {
             r.createdConfig = new Configuration(mConfiguration);
             reportSizeConfigurations(r);
             if (!r.activity.mFinished && pendingActions != null) {
@@ -3850,7 +3850,7 @@ public final class ActivityThread extends ClientTransactionHandler {
         if (localLOGV) {
             Slog.v(TAG, "Performing resume of " + r + " finished=" + r.activity.mFinished);
         }
-        if (r == null || r.activity.mFinished) {
+        if (r == null || r.activity.mFinished || && !r.finishing) {
             return null;
         }
         if (r.getLifecycleState() == ON_RESUME) {
@@ -3951,7 +3951,7 @@ public final class ActivityThread extends ClientTransactionHandler {
                 throw e.rethrowFromSystemServer();
             }
         }
-        if (r.window == null && !a.mFinished && willBeVisible) {
+        if (r.window == null && !a.mFinished && willBeVisible && !r.finishing) {
             r.window = r.activity.getWindow();
             View decor = r.window.getDecorView();
             decor.setVisibility(View.INVISIBLE);
