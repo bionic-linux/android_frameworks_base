0813e85deb4bf3d7c92b3ba6f40a59e610b7aa84
Howard Chen <howardsoc@google.com>
2019-06-06 11:36:48 +0800

    Stub the oemlock operations when the PersistentDataBlock is not supported
    The PersistentDataBlockService is not a compulsory component of framework thus we need to
    handle cases when the PersistentDataBlockService is unavailable.
    
    Bug: 132943408
    Bug: 123777418
    Test: toggle the oemlock in setting menu.
    
    Change-Id: I59eec0a4905b588bf261b8447ad4b4e26ba5278d
    Merged-In: I59eec0a4905b588bf261b8447ad4b4e26ba5278d

--- services/core/java/com/android/server/oemlock/PersistentDataBlockLock.java
+++ services/core/java/com/android/server/oemlock/PersistentDataBlockLock.java
@@ -67,6 +67,10 @@ class PersistentDataBlockLock extends OemLock {
         // unlocked but doesn't actually do any unlocking.
         final PersistentDataBlockManager pdbm = (PersistentDataBlockManager)
                 mContext.getSystemService(Context.PERSISTENT_DATA_BLOCK_SERVICE);
+        if (pdbm == null) {
+            Slog.w(TAG, "PersistentDataBlock is not supported on this device");
+            return;
+        }
         pdbm.setOemUnlockEnabled(allowedByDevice);
     }
 
@@ -74,6 +78,10 @@ class PersistentDataBlockLock extends OemLock {
     boolean isOemUnlockAllowedByDevice() {
         final PersistentDataBlockManager pdbm = (PersistentDataBlockManager)
             mContext.getSystemService(Context.PERSISTENT_DATA_BLOCK_SERVICE);
+        if (pdbm == null) {
+            Slog.w(TAG, "PersistentDataBlock is not supported on this device");
+            return false;
+        }
         return pdbm.getOemUnlockEnabled();
     }
 
@@ -85,6 +93,10 @@ class PersistentDataBlockLock extends OemLock {
     private void disallowUnlockIfNotUnlocked() {
         final PersistentDataBlockManager pdbm = (PersistentDataBlockManager)
             mContext.getSystemService(Context.PERSISTENT_DATA_BLOCK_SERVICE);
+        if (pdbm == null) {
+            Slog.w(TAG, "PersistentDataBlock is not supported on this device");
+            return;
+        }
         if (pdbm.getFlashLockState() != PersistentDataBlockManager.FLASH_LOCK_UNLOCKED) {
             pdbm.setOemUnlockEnabled(false);
         }
