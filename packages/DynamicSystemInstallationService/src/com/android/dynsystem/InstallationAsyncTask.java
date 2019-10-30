4e593d7bf2850a5eee7cbaf4aaaae1596ae9fd29
Po-Chien Hsueh <pchsueh@google.com>
2019-10-30 16:48:15 +0800

    DSU to support zip files
    DSU previously only support gz'ed system images. This CL enables DSU
    to read zip files. If there are multiple images in the zip file. DSU
    will install them all.
    
    Bug: 134353973
    Test: adb shell am start-activity \
        -n com.android.dynsystem/com.android.dynsystem.VerificationActivity \
        -a android.os.image.action.START_INSTALL \
        -d https://dl.google.com/developers/android/qt/images/gsi/aosp_arm64-QP1A.190771.020-5800535.zip \
        --el KEY_USERDATA_SIZE 8589934592
    
    Change-Id: I9c2137d4d81398a9c6153df63a29c16eb8339795

--- packages/DynamicSystemInstallationService/src/com/android/dynsystem/InstallationAsyncTask.java
+++ packages/DynamicSystemInstallationService/src/com/android/dynsystem/InstallationAsyncTask.java
@@ -17,7 +17,6 @@
 package com.android.dynsystem;
 
 import android.content.Context;
-import android.gsi.GsiProgress;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.MemoryFile;
@@ -30,32 +29,63 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
-class InstallationAsyncTask extends AsyncTask<String, Long, Throwable> {
+class InstallationAsyncTask extends AsyncTask<String, InstallationAsyncTask.Progress, Throwable> {
 
     private static final String TAG = "InstallationAsyncTask";
 
     private static final int READ_BUFFER_SIZE = 1 << 13;
+    private static final long MIN_PROGRESS_TO_PUBLISH = 1 << 23;
 
-    private class InvalidImageUrlException extends RuntimeException {
-        private InvalidImageUrlException(String message) {
+    private static final List<String> UNSUPPORTED_PARTITIONS = Arrays.asList("vbmeta", "boot");
+
+    private class UnsupportedUrlException extends RuntimeException {
+        private UnsupportedUrlException(String message) {
             super(message);
         }
     }
 
-    /** Not completed, including being cancelled */
-    static final int NO_RESULT = 0;
+    private class UnsupportedFormatException extends RuntimeException {
+        private UnsupportedFormatException(String message) {
+            super(message);
+        }
+    }
+
+    /** UNSET means the installation is not completed */
+    static final int RESULT_UNSET = 0;
     static final int RESULT_OK = 1;
-    static final int RESULT_ERROR_IO = 2;
-    static final int RESULT_ERROR_INVALID_URL = 3;
+    static final int RESULT_CANCELLED = 2;
+    static final int RESULT_ERROR_IO = 3;
+    static final int RESULT_ERROR_UNSUPPORTED_URL = 4;
+    static final int RESULT_ERROR_UNSUPPORTED_FORMAT = 5;
     static final int RESULT_ERROR_EXCEPTION = 6;
 
-    interface InstallStatusListener {
-        void onProgressUpdate(long installedSize);
+    class Progress {
+        String mPartitionName;
+        long mPartitionSize;
+        long mInstalledSize;
+
+        int mNumInstalledPartitions;
+
+        Progress(String partitionName, long partitionSize, long installedSize,
+                int numInstalled) {
+            mPartitionName = partitionName;
+            mPartitionSize = partitionSize;
+            mInstalledSize = installedSize;
+
+            mNumInstalledPartitions = numInstalled;
+        }
+    }
+
+    interface ProgressListener {
+        void onProgressUpdate(Progress progress);
         void onResult(int resultCode, Throwable detail);
-        void onCancelled();
     }
 
     private final String mUrl;
@@ -63,16 +93,14 @@ class InstallationAsyncTask extends AsyncTask<String, Long, Throwable> {
     private final long mUserdataSize;
     private final Context mContext;
     private final DynamicSystemManager mDynSystem;
-    private final InstallStatusListener mListener;
+    private final ProgressListener mListener;
     private DynamicSystemManager.Session mInstallationSession;
 
-    private int mResult = NO_RESULT;
-
-    private InputStream mStream;
+    private boolean mIsCompleted;
 
 
     InstallationAsyncTask(String url, long systemSize, long userdataSize, Context context,
-            DynamicSystemManager dynSystem, InstallStatusListener listener) {
+            DynamicSystemManager dynSystem, ProgressListener listener) {
         mUrl = url;
         mSystemSize = systemSize;
         mUserdataSize = userdataSize;
@@ -82,88 +110,162 @@ class InstallationAsyncTask extends AsyncTask<String, Long, Throwable> {
     }
 
     @Override
-    protected void onPreExecute() {
-        mListener.onProgressUpdate(0);
-    }
-
-    @Override
     protected Throwable doInBackground(String... voids) {
         Log.d(TAG, "Start doInBackground(), URL: " + mUrl);
 
+        InputStream is = null;
+
         try {
-            long installedSize = 0;
-            long reportedInstalledSize = 0;
+            // init input stream before creating userdata, which takes 90 seconds.
+            is = initInputStream();
 
-            long minStepToReport = (mSystemSize + mUserdataSize) / 100;
+            installUserdata();
 
-            // init input stream before calling startInstallation(), which takes 90 seconds.
-            initInputStream();
-
-            Thread thread =
-                    new Thread(
-                            () -> {
-                                mDynSystem.startInstallation("userdata", mUserdataSize, false);
-                                mInstallationSession =
-                                        mDynSystem.startInstallation("system", mSystemSize, true);
-                            });
-
-            thread.start();
-
-            while (thread.isAlive()) {
-                if (isCancelled()) {
-                    boolean aborted = mDynSystem.abort();
-                    Log.d(TAG, "Called DynamicSystemManager.abort(), result = " + aborted);
-                    return null;
-                }
-
-                GsiProgress progress = mDynSystem.getInstallationProgress();
-                installedSize = progress.bytes_processed;
-
-                if (installedSize > reportedInstalledSize + minStepToReport) {
-                    publishProgress(installedSize);
-                    reportedInstalledSize = installedSize;
-                }
-
-                Thread.sleep(10);
+            if (isCancelled()) {
+                return null;
             }
 
-            if (mInstallationSession == null) {
-                throw new IOException(
-                        "Failed to start installation with requested size: "
-                                + (mSystemSize + mUserdataSize));
+            String extension = mUrl.substring(mUrl.lastIndexOf('.') + 1);
+
+            if ("gz".equals(extension)) {
+                installImage("system", mSystemSize, new GZIPInputStream(is), 1);
+            } else if ("zip".equals(extension)) {
+                ZipInputStream zis = new ZipInputStream(is);
+                ZipEntry zipEntry = null;
+
+                int numInstalledPartitions = 1;
+
+                while ((zipEntry = zis.getNextEntry()) != null) {
+                    String name = zipEntry.getName();
+
+                    if (!name.endsWith(".img")) {
+                        continue;
+                    }
+
+                    String partitionName = name.substring(0, name.length() - 4);
+
+                    if (UNSUPPORTED_PARTITIONS.contains(partitionName)) {
+                        continue;
+                    }
+
+                    long partitionSize = zipEntry.getSize();
+
+                    if (partitionSize == -1) {
+                        throw new IOException("Cannot get uncompressed file size");
+                    }
+
+                    installImage(partitionName, partitionSize, zis, numInstalledPartitions);
+
+                    if (isCancelled()) {
+                        break;
+                    }
+
+                    numInstalledPartitions++;
+                }
+            } else {
+                throw new UnsupportedFormatException(
+                    String.format(Locale.US, "Unsupported file format: %s", mUrl));
             }
 
-            installedSize = mUserdataSize;
-
-            MemoryFile memoryFile = new MemoryFile("dsu", READ_BUFFER_SIZE);
-            byte[] bytes = new byte[READ_BUFFER_SIZE];
-            mInstallationSession.setAshmem(
-                    new ParcelFileDescriptor(memoryFile.getFileDescriptor()), READ_BUFFER_SIZE);
-            int numBytesRead;
-            Log.d(TAG, "Start installation loop");
-            while ((numBytesRead = mStream.read(bytes, 0, READ_BUFFER_SIZE)) != -1) {
-                memoryFile.writeBytes(bytes, 0, 0, numBytesRead);
-                if (isCancelled()) {
-                    break;
-                }
-                if (!mInstallationSession.submitFromAshmem(numBytesRead)) {
-                    throw new IOException("Failed write() to DynamicSystem");
-                }
-
-                installedSize += numBytesRead;
-
-                if (installedSize > reportedInstalledSize + minStepToReport) {
-                    publishProgress(installedSize);
-                    reportedInstalledSize = installedSize;
-                }
-            }
             return null;
 
         } catch (Exception e) {
             e.printStackTrace();
             return e;
         } finally {
-            close();
+            close(is);
+        }
+    }
+
+    private void installUserdata() throws Exception {
+        Thread thread = new Thread(() -> {
+            mInstallationSession = mDynSystem.startInstallation("userdata", mUserdataSize, false);
+        });
+
+        Log.d(TAG, "Creating partition: userdata");
+        thread.start();
+
+        long installedSize = 0;
+        long publishedProgress = 0;
+
+        while (thread.isAlive()) {
+            if (isCancelled()) {
+                return;
+            }
+
+            installedSize = mDynSystem.getInstallationProgress().bytes_processed;
+
+            if (installedSize > publishedProgress + MIN_PROGRESS_TO_PUBLISH) {
+                publishProgress(new Progress("userdata", mUserdataSize, installedSize, 0));
+                publishedProgress = installedSize;
+            }
+
+            Thread.sleep(10);
+        }
+
+        if (mInstallationSession == null) {
+            throw new IOException(
+                    "Failed to start installation with requested size: " + mUserdataSize);
+        }
+    }
+
+    private void installImage(String partitionName, long partitionSize, InputStream is,
+            int numInstalledPartitions) throws Exception {
+        Thread thread = new Thread(() -> {
+            mInstallationSession =
+                    mDynSystem.startInstallation(partitionName, partitionSize, true);
+        });
+
+        Log.d(TAG, "Creating partition: " + partitionName);
+        thread.start();
+
+        while (thread.isAlive()) {
+            if (isCancelled()) {
+                return;
+            }
+
+            Thread.sleep(10);
+        }
+
+        if (mInstallationSession == null) {
+            throw new IOException(
+                    "Failed to start installation with requested size: " + partitionSize);
+        }
+
+        Log.d(TAG, "Start installation: " + partitionName);
+
+        MemoryFile memoryFile = new MemoryFile("dsu_" + partitionName , READ_BUFFER_SIZE);
+        ParcelFileDescriptor pfd = new ParcelFileDescriptor(memoryFile.getFileDescriptor());
+
+        mInstallationSession.setAshmem(pfd, READ_BUFFER_SIZE);
+
+        long installedSize = 0;
+        long publishedProgress = 0;
+
+        byte[] bytes = new byte[READ_BUFFER_SIZE];
+        int numBytesRead;
+
+        BufferedInputStream bis = new BufferedInputStream(is);
+
+        while ((numBytesRead = bis.read(bytes, 0, READ_BUFFER_SIZE)) != -1) {
+            if (isCancelled()) {
+                return;
+            }
+
+            memoryFile.writeBytes(bytes, 0, 0, numBytesRead);
+
+            if (!mInstallationSession.submitFromAshmem(numBytesRead)) {
+                throw new IOException("Failed write() to DynamicSystem");
+            }
+
+            installedSize += numBytesRead;
+
+            if (installedSize > publishedProgress + MIN_PROGRESS_TO_PUBLISH) {
+                publishProgress(new Progress(
+                        partitionName, partitionSize, installedSize, numInstalledPartitions));
+
+                publishedProgress = installedSize;
+            }
         }
     }
 
@@ -171,65 +273,69 @@ class InstallationAsyncTask extends AsyncTask<String, Long, Throwable> {
     protected void onCancelled() {
         Log.d(TAG, "onCancelled(), URL: " + mUrl);
 
-        mListener.onCancelled();
+        if (mDynSystem.abort()) {
+            Log.d(TAG, "Installation aborted");
+        } else {
+            Log.w(TAG, "DynamicSystemManager.abort() returned false");
+        }
+
+        mListener.onResult(RESULT_CANCELLED, null);
     }
 
     @Override
     protected void onPostExecute(Throwable detail) {
+        int result = RESULT_UNSET;
+
         if (detail == null) {
-            mResult = RESULT_OK;
+            result = RESULT_OK;
+            mIsCompleted = true;
         } else if (detail instanceof IOException) {
-            mResult = RESULT_ERROR_IO;
-        } else if (detail instanceof InvalidImageUrlException) {
-            mResult = RESULT_ERROR_INVALID_URL;
+            result = RESULT_ERROR_IO;
+        } else if (detail instanceof UnsupportedUrlException) {
+            result = RESULT_ERROR_UNSUPPORTED_URL;
+        } else if (detail instanceof UnsupportedFormatException) {
+            result = RESULT_ERROR_UNSUPPORTED_FORMAT;
         } else {
-            mResult = RESULT_ERROR_EXCEPTION;
+            result = RESULT_ERROR_EXCEPTION;
         }
 
-        Log.d(TAG, "onPostExecute(), URL: " + mUrl + ", result: " + mResult);
+        Log.d(TAG, "onPostExecute(), URL: " + mUrl + ", result: " + result);
 
-        mListener.onResult(mResult, detail);
+        mListener.onResult(result, detail);
     }
 
     @Override
-    protected void onProgressUpdate(Long... values) {
-        long progress = values[0];
+    protected void onProgressUpdate(Progress... values) {
+        Progress progress = values[0];
         mListener.onProgressUpdate(progress);
     }
 
-    private void initInputStream() throws IOException, InvalidImageUrlException {
+    private InputStream initInputStream() throws IOException, UnsupportedUrlException {
         if (URLUtil.isNetworkUrl(mUrl) || URLUtil.isFileUrl(mUrl)) {
-            mStream = new BufferedInputStream(new GZIPInputStream(new URL(mUrl).openStream()));
+            return new URL(mUrl).openStream();
         } else if (URLUtil.isContentUrl(mUrl)) {
-            Uri uri = Uri.parse(mUrl);
-            mStream = new BufferedInputStream(new GZIPInputStream(
-                    mContext.getContentResolver().openInputStream(uri)));
+            return mContext.getContentResolver().openInputStream(Uri.parse(mUrl));
         } else {
-            throw new InvalidImageUrlException(
+            throw new UnsupportedUrlException(
                     String.format(Locale.US, "Unsupported file source: %s", mUrl));
         }
     }
 
-    private void close() {
+    private void close(InputStream is) {
         try {
-            if (mStream != null) {
-                mStream.close();
-                mStream = null;
+            if (is != null) {
+                is.close();
             }
         } catch (IOException e) {
             // ignore
         }
     }
 
-    int getResult() {
-        return mResult;
+    boolean isCompleted() {
+        return mIsCompleted;
     }
 
     boolean commit() {
-        if (mInstallationSession == null) {
-            return false;
-        }
-
-        return mInstallationSession.commit();
+        return mDynSystem.setEnable(true, true);
     }
 }
