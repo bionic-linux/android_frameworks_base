47ed50a4a90d28a4e1057cc7da592ecf8c0e802b
markchien <markchien@google.com>
2019-11-02 15:21:15 +0800

    [Tether11] Passing network stack binder to tethering module
    Since NetworkStackClient is not updatable and use some @hide API
    which are not suitable to be exposed. Passing network stack binder
    to tethering service directly.
    
    Bug: 144320246
    Test: -build, flash, boot
    Change-Id: Ie93372d77beece29fc55f7439a8ebab013561fd0

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -42,7 +42,6 @@ import android.hardware.display.DisplayManagerInternal;
 import android.net.ConnectivityModuleConnector;
 import android.net.Network;
 import android.net.NetworkStackClient;
-import android.net.TetheringManager;
 import android.os.BaseBundle;
 import android.os.Binder;
 import android.os.Build;
@@ -2193,15 +2192,6 @@ public final class SystemServer {
                 NetworkStackClient.getInstance().start();
             } catch (Throwable e) {
                 reportWtf("starting Network Stack", e);
-            }
-            traceEnd();
-
-            traceBeginAndSlog("StartTethering");
-            try {
-                // Tethering must start after ConnectivityService and NetworkStack.
-                TetheringManager.getInstance().start();
-            } catch (Throwable e) {
-                reportWtf("starting Tethering", e);
             }
             traceEnd();
 
