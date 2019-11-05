204a8a5cadfad8a152bc2213eaa2f5b7434d2dd9
Chalard Jean <jchalard@google.com>
2019-11-05 15:10:49 +0900

    [NS A03] Remove mNetworkForRequestId
    Bug: 113554781
    Test: ConnectivityServiceTest
    Change-Id: Ie4ae6a97053559e6cbac8230f2db3d35000b35a9

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -3165,12 +3165,13 @@ public class ConnectivityService extends IConnectivityManager.Stub
             final NetworkRequestInfo nri = mNetworkRequests.get(request);
             final NetworkAgentInfo currentNetwork = getNetworkForRequest(nri);
             if (currentNetwork != null && currentNetwork.network.netId == nai.network.netId) {
-                setNetworkForRequest(nri, null);
+                nri.mSatisfier = null;
                 sendUpdatedScoreToFactories(request, null);
             }
         }
         nai.clearLingerState();
         if (nai.isSatisfyingRequest(mDefaultRequest.requestId)) {
+            mDefaultNetworkNai = null;
             updateDataActivityTracking(null /* newNetwork */, nai);
             notifyLockdownVpn(nai);
             ensureNetworkTransitionWakelock(nai.name());
@@ -3416,7 +3417,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
                 } else {
                     wasKept = true;
                 }
-                setNetworkForRequest(nri, null);
+                nri.mSatisfier = null;
                 if (!wasBackgroundNetwork && nai.isBackgroundNetwork()) {
                     // Went from foreground to background.
                     updateCapabilities(nai.getCurrentScore(), nai, nai.networkCapabilities);
@@ -5490,16 +5491,6 @@ public class ConnectivityService extends IConnectivityManager.Stub
         if (DBG) log("unregisterNetworkFactory for " + nfi.name);
     }
 
-    /**
-     * NetworkAgentInfo supporting a request by requestId.
-     * These have already been vetted (their Capabilities satisfy the request)
-     * and the are the highest scored network available.
-     * the are keyed off the Requests requestId.
-     */
-    // NOTE: Accessed on multiple threads, must be synchronized on itself.
-    @GuardedBy("mNetworkForRequestId")
-    private final SparseArray<NetworkAgentInfo> mNetworkForRequestId = new SparseArray<>();
-
     // NOTE: Accessed on multiple threads, must be synchronized on itself.
     @GuardedBy("mNetworkForNetId")
     private final SparseArray<NetworkAgentInfo> mNetworkForNetId = new SparseArray<>();
@@ -5534,18 +5525,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
     @Nullable
     private NetworkAgentInfo getNetworkForRequest(@NonNull final NetworkRequestInfo requestInfo) {
         ensureRunningOnConnectivityServiceThread();
-        synchronized (mNetworkForRequestId) {
-            return requestInfo.mSatisfier;
-        }
-    }
-
-    private void setNetworkForRequest(@NonNull final NetworkRequestInfo requestInfo,
-            @Nullable NetworkAgentInfo nai) {
-        ensureRunningOnConnectivityServiceThread();
-        synchronized (mNetworkForRequestId) {
-            requestInfo.mSatisfier = nai;
-            if (isDefaultRequest(requestInfo)) mDefaultNetworkNai = nai;
-        }
+        return requestInfo.mSatisfier;
     }
 
     private NetworkAgentInfo getDefaultNetwork() {
@@ -6265,7 +6245,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
         }
     }
 
-    private void makeDefault(NetworkAgentInfo newNetwork) {
+    private void makeDefault(@NonNull final NetworkAgentInfo newNetwork) {
         if (DBG) log("Switching to new default network: " + newNetwork);
 
         try {
@@ -6274,6 +6254,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
             loge("Exception setting default network :" + e);
         }
 
+        mDefaultNetworkNai = newNetwork;
         notifyLockdownVpn(newNetwork);
         handleApplyDefaultProxy(newNetwork.linkProperties.getHttpProxy());
         updateTcpBufferSizes(newNetwork.linkProperties.getTcpBufferSizes());
@@ -6395,7 +6376,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
                         if (VDBG || DDBG) log("   accepting network in place of null");
                     }
                     newNetwork.unlingerRequest(nri.request);
-                    setNetworkForRequest(nri, newNetwork);
+                    nri.mSatisfier = newNetwork;
                     if (!newNetwork.addRequest(nri.request)) {
                         Slog.wtf(TAG, "BUG: " + newNetwork.name() + " already has " + nri.request);
                     }
@@ -6429,12 +6410,13 @@ public class ConnectivityService extends IConnectivityManager.Stub
                 }
                 newNetwork.removeRequest(nri.request.requestId);
                 if (currentNetwork == newNetwork) {
-                    setNetworkForRequest(nri, null);
+                    nri.mSatisfier = null;
+                    if (isDefaultRequest(nri)) mDefaultNetworkNai = null;
                     sendUpdatedScoreToFactories(nri.request, null);
                 } else {
                     Slog.wtf(TAG, "BUG: Removing request " + nri.request.requestId + " from " +
                             newNetwork.name() +
-                            " without updating mNetworkForRequestId or factories!");
+                            " without updating mSatisfier or factories!");
                 }
                 // TODO: Technically, sending CALLBACK_LOST here is
                 // incorrect if there is a replacement network currently
