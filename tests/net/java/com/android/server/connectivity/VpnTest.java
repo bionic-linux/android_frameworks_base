0abdbd2089b757e946b91f6649900a69a7f109cd
Benedict Wong <benedictwong@google.com>
2019-11-06 00:20:15 -0800

    Add separate user consent for Platform VPNs
    This change adds a new VPN user consent flow (using the same text) for
    granting the lesser OP_ACTIVATE_PLATFORM_VPN. A new
    PlatformVpnConfirmDialog is created as a subclass to preserve all logic,
    but ensure the right appop is granted for the relevant dialog.
    
    Intent extras were considered, but are inherently unsafe, since the
    caller may add any extras that they would want.
    
    Bug: 144246835
    Test: FrameworksNetTests passing
    Change-Id: Ia6f36207d43c3748f938430c2780dcf29e5623f3

--- tests/net/java/com/android/server/connectivity/VpnTest.java
+++ tests/net/java/com/android/server/connectivity/VpnTest.java
@@ -62,6 +62,7 @@ import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo.DetailedState;
 import android.net.UidRange;
+import android.net.VpnManager;
 import android.net.VpnService;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
@@ -814,6 +815,60 @@ public class VpnTest {
                         eq(AppOpsManager.OP_ACTIVATE_PLATFORM_VPN),
                         eq(Process.myUid()),
                         eq(TEST_VPN_PKG));
+    }
+
+    @Test
+    public void testSetPackageAuthorizationVpnService() throws Exception {
+        final Vpn vpn = createVpnAndSetupUidChecks();
+
+        assertTrue(
+                vpn.setPackageAuthorization(
+                        TEST_VPN_PKG, true /* authorized */, VpnManager.TYPE_VPN_SERVICE));
+        verify(mAppOps)
+                .setMode(
+                        eq(AppOpsManager.OP_ACTIVATE_VPN),
+                        eq(Process.myUid()),
+                        eq(TEST_VPN_PKG),
+                        eq(AppOpsManager.MODE_ALLOWED));
+    }
+
+    @Test
+    public void testSetPackageAuthorizationPlatformVpn() throws Exception {
+        final Vpn vpn = createVpnAndSetupUidChecks();
+
+        assertTrue(vpn.setPackageAuthorization(
+                TEST_VPN_PKG, true /* authorized */, VpnManager.TYPE_PLATFORM_VPN));
+        verify(mAppOps)
+                .setMode(
+                        eq(AppOpsManager.OP_ACTIVATE_PLATFORM_VPN),
+                        eq(Process.myUid()),
+                        eq(TEST_VPN_PKG),
+                        eq(AppOpsManager.MODE_ALLOWED));
+    }
+
+    @Test
+    public void testSetPackageAuthorizationRevokeAuthorization() throws Exception {
+        final Vpn vpn = createVpnAndSetupUidChecks();
+
+        // Verify that for any given type, both appops are cleared.
+        final int[] allTypes = new int[] {
+                VpnManager.TYPE_IGNORED, VpnManager.TYPE_VPN_SERVICE, VpnManager.TYPE_PLATFORM_VPN};
+        for (int i = 0; i < allTypes.length; i++) {
+            assertTrue(
+                    vpn.setPackageAuthorization(TEST_VPN_PKG, false /* authorized */, allTypes[i]));
+            verify(mAppOps, times(i + 1))
+                    .setMode(
+                            eq(AppOpsManager.OP_ACTIVATE_VPN),
+                            eq(Process.myUid()),
+                            eq(TEST_VPN_PKG),
+                            eq(AppOpsManager.MODE_IGNORED));
+            verify(mAppOps, times(i + 1))
+                    .setMode(
+                            eq(AppOpsManager.OP_ACTIVATE_PLATFORM_VPN),
+                            eq(Process.myUid()),
+                            eq(TEST_VPN_PKG),
+                            eq(AppOpsManager.MODE_IGNORED));
+        }
     }
 
     /**
