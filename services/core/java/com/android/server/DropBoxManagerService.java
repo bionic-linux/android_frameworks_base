c5ce3fd5c6624daa5d67cf54834b1f0b022cb0b9
juhyun.han <juhyun.han@lge.com>
2019-11-06 14:44:47 +0900

    fix integer overflow in DropboxManagerService
    Integer overflows if a device has a large storage.
    (mStatFs.getBlockCount() * reservePercent > Integer.MAX_VALUE)
    
    Test: CtsIncidentHostTestCases
    com.android.server.cts.ErrorsTest#testNativeCrash
    com.android.server.cts.ErrorsTest#testThrowException
    
    Change-Id: I4f50efd189a4107fd786c935bbccce2c95d6b72c

--- services/core/java/com/android/server/DropBoxManagerService.java
+++ services/core/java/com/android/server/DropBoxManagerService.java
@@ -1036,13 +1036,22 @@ public final class DropBoxManagerService extends SystemService {
             } catch (IllegalArgumentException e) {  // restat throws this on error
                 throw new IOException("Can't restat: " + mDropBoxDir);
             }
+<<<<<<< HEAD   (d5ef2d Merge "API Lint: remove obsolete python linter")
             long available = mStatFs.getAvailableBlocksLong();
             long nonreserved = available - mStatFs.getBlockCountLong() * reservePercent / 100;
             long maxAvailableLong = nonreserved * quotaPercent / 100;
             int maxAvailable = Math.toIntExact(Math.max(0,
                     Math.min(maxAvailableLong, Integer.MAX_VALUE)));
+=======
+            int available = mStatFs.getAvailableBlocks();
+            int nonreserved = available - (int)((long)mStatFs.getBlockCount() * reservePercent / 100);
+>>>>>>> CHANGE (241ffd fix integer overflow in DropboxManagerService)
             int maximum = quotaKb * 1024 / mBlockSize;
+<<<<<<< HEAD   (d5ef2d Merge "API Lint: remove obsolete python linter")
             mCachedQuotaBlocks = Math.min(maximum, maxAvailable);
+=======
+            mCachedQuotaBlocks = Math.min(maximum, Math.max(0, (int)((long)nonreserved * quotaPercent / 100)));
+>>>>>>> CHANGE (241ffd fix integer overflow in DropboxManagerService)
             mCachedQuotaUptimeMillis = uptimeMillis;
         }
 
