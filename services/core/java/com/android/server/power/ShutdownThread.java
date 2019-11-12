db3778d8d9c2700d7465cbb0bc0a8e465a3ecacf
daxing.cao <daxing.cao@unisoc.corp-partner.google.com>
2019-11-12 14:05:01 +0800

    Shutdown optimization
    Test: manual
    Bug:144321983
    Change-Id: I6c3ff8b7d1f522a20dc04ef6ba878b04e1ab0dab

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -478,7 +478,24 @@ public final class ShutdownThread extends Thread {
         }
         shutdownTimingLog.traceEnd(); // SendShutdownBroadcast
         metricEnded(METRIC_SEND_BROADCAST);
-
+        //ShutDown Radio in a new thread
+        Thread t = new Thread() {
+            public void run() {
+                boolean radioOff;
+                final ITelephony phone =
+                       ITelephony.Stub.asInterface(ServiceManager.checkService("phone"));
+                try {
+                    radioOff = phone == null || !phone.needMobileRadioShutdown();
+                    if (!radioOff) {
+                        Log.w(TAG, "Turning off cellular radios...");
+                        phone.shutdownMobileRadios();
+                    }
+                } catch (RemoteException ex) {
+                    Log.e(TAG, "RemoteException during radio shutdown", ex);
+                }
+            }
+        };
+        t.start();
         Log.i(TAG, "Shutting down activity manager...");
         shutdownTimingLog.traceBegin("ShutdownActivityManager");
         metricStarted(METRIC_AM);
@@ -592,7 +609,6 @@ public final class ShutdownThread extends Thread {
                     if (!radioOff) {
                         Log.w(TAG, "Turning off cellular radios...");
                         metricStarted(METRIC_RADIO);
-                        phone.shutdownMobileRadios();
                     }
                 } catch (RemoteException ex) {
                     Log.e(TAG, "RemoteException during radio shutdown", ex);
