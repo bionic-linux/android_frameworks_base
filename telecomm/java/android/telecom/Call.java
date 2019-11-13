759b1c3e26e98b897726993a62676a2d137d60cc
Grace Jia <xiaotonj@google.com>
2019-11-13 15:59:14 -0800

    Add missed information relative parameter and fields.
    Test: android.provider.cts.CallLogTest
    Bug: 144452099
    Change-Id: Idb5fb877a5e947b00ae1d7d4046f0bef26af8550

--- telecomm/java/android/telecom/Call.java
+++ telecomm/java/android/telecom/Call.java
@@ -25,7 +25,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
 import android.os.ParcelFileDescriptor;
 
 import com.android.internal.telecom.IVideoProvider;
