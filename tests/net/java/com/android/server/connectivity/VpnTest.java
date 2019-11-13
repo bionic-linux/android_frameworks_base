b32832472c97eff355d5ac0bd70ccdd1a30d1f01
Benedict Wong <benedictwong@google.com>
2019-11-12 22:31:51 -0800

    Implement Ikev2VpnRunner
    This change adds the implementation for IKEv2/IPsec VPNs.
    
    Bug: 144246767
    Test: Manually tested
    Change-Id: I5ccec756cec49ccf57ccc4d5ad800eeb5d595a76

--- tests/net/java/com/android/server/connectivity/VpnTest.java
+++ tests/net/java/com/android/server/connectivity/VpnTest.java
@@ -147,6 +147,7 @@ public class VpnTest {
     @Mock private AppOpsManager mAppOps;
     @Mock private NotificationManager mNotificationManager;
     @Mock private Vpn.SystemServices mSystemServices;
+    @Mock private Vpn.Ikev2SessionCreator mIkev2SessionCreator;
     @Mock private ConnectivityManager mConnectivityManager;
     @Mock private KeyStore mKeyStore;
     private final VpnProfile mVpnProfile = new VpnProfile("key");
@@ -700,6 +701,10 @@ public class VpnTest {
 
     @Test
     public void testStartVpnProfile() throws Exception {
+        startVpnAndVerify();
+    }
+
+    private void startVpnAndVerify() throws Exception {
         final Vpn vpn = createVpn(primaryUser.id);
         setMockedUsers(primaryUser);
 
@@ -835,7 +840,8 @@ public class VpnTest {
      * Mock some methods of vpn object.
      */
     private Vpn createVpn(@UserIdInt int userId) {
-        return new Vpn(Looper.myLooper(), mContext, mNetService, userId, mSystemServices);
+        return new Vpn(Looper.myLooper(), mContext, mNetService,
+                userId, mSystemServices, mIkev2SessionCreator);
     }
 
     private static void assertBlocked(Vpn vpn, int... uids) {
