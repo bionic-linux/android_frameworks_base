b43380436b77a071a6dff6f96746f31f4c916080
Chalard Jean <jchalard@google.com>
2019-11-19 22:20:55 +0900

    [NS A21] More cleanup
    Test: FrameworksNetTests NetworkStackTests
    Change-Id: I4771f2e9151ff16a7045d9c3025ac686f244b22d

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -6476,8 +6476,9 @@ public class ConnectivityService extends IConnectivityManager.Stub
         // before LegacyTypeTracker sends legacy broadcasts
         for (NetworkRequestInfo nri : addedRequests) notifyNetworkAvailable(newNetwork, nri);
 
-        // Second pass: process all listens.
-        if (wasBackgroundNetwork != newNetwork.isBackgroundNetwork()) {
+        // Maybe the network changed background states. Update its capabilities.
+        final boolean backgroundChanged = wasBackgroundNetwork != newNetwork.isBackgroundNetwork();
+        if (backgroundChanged) {
             final NetworkCapabilities newNc = mixInCapabilities(newNetwork,
                     newNetwork.networkCapabilities);
 
@@ -6494,13 +6495,10 @@ public class ConnectivityService extends IConnectivityManager.Stub
             synchronized (newNetwork) {
                 newNetwork.setNetworkCapabilities(newNc);
             }
-
-            // The requestable capabilities and the score can't have changed, therefore the change
-            // in background can't affect any requests. Only processing the listens is fine.
-            processListenRequests(newNetwork, true);
-        } else {
-            processListenRequests(newNetwork, false);
         }
+
+        // Finally, process listen requests.
+        processListenRequests(newNetwork, backgroundChanged);
     }
 
     /**
