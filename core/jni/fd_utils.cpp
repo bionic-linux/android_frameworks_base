e706618e5a4dc3b2a60b713a5feae265dd10b342
Benedict Wong <benedictwong@google.com>
2019-11-19 18:45:17 -0800

    [DNS] Add IKE to allowable zygote paths
    This change adds the IKE apex to the whitelist of open paths. Without
    this change, adding IKE to the boot classpath will fail.
    
    Bug: 143905344
    Test: Compiles, doesn't fail.
    Change-Id: I9d3c2b18e8115a6e0f871e15f288ae38768d02e8

--- core/jni/fd_utils.cpp
+++ core/jni/fd_utils.cpp
@@ -35,6 +35,7 @@
 static const char* kPathWhitelist[] = {
   "/apex/com.android.conscrypt/javalib/conscrypt.jar",
   "/apex/com.android.media/javalib/updatable-media.jar",
+  "/apex/com.android.ipsec/javalib/ike.jar",
   "/dev/null",
   "/dev/socket/zygote",
   "/dev/socket/zygote_secondary",
