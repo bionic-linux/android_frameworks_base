c45962cb6761eab79ef2a1cf1c83a465c37a5903
wonyoung <won_0.byun@samsung.com>
2019-12-06 14:26:54 +0900

    notification is not cleard after cllick the clear button If you raise touch directly instead of automatically going up,
    Test: look at the statusbar notification area
    Change-Id: I74a912fd5c7913fcf94e6e37ef2dc6b6de5df014

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -229,7 +229,6 @@ import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.statusbar.policy.UserInfoControllerImpl;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.statusbar.policy.ZenModeController;
-import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.InjectionInflationController;
 import com.android.systemui.volume.VolumeComponent;
 
@@ -1937,6 +1936,11 @@ public class StatusBar extends SystemUI implements DemoMode,
             mStatusBarView.collapsePanel(true /* animate */, delayed, speedUpFactor);
         } else {
             mBubbleController.collapseStack();
+
+            if (mNotificationPanel != null && mNotificationPanel.isFullyCollapsed()
+                    && mPostCollapseRunnables.size() > 0) {
+                runPostCollapseRunnables();
+            }
         }
     }
 
