7dc5eab334e69e7764f10b96c52c699a2be6dd1b
Artur Satayev <satayev@google.com>
2019-12-10 17:47:53 +0000

    Use new UnsupportedAppUsage annotation.
    Existing annotations in libcore/ and frameworks/ will deleted after the migration. This also means that any java library that compiles @UnsupportedAppUsage requires a direct dependency on "unsupportedappusage" java_library.
    
    Bug: 145132366
    Test: m && diff unsupportedappusage_index.csv
    Change-Id: I288969b0c22fa3a63bc2e71bb5009fe4a927e154

--- core/java/android/telephony/PhoneStateListener.java
+++ core/java/android/telephony/PhoneStateListener.java
@@ -21,7 +21,7 @@ import android.annotation.NonNull;
 import android.annotation.RequiresPermission;
 import android.annotation.SystemApi;
 import android.annotation.TestApi;
-import android.annotation.UnsupportedAppUsage;
+import android.compat.annotation.UnsupportedAppUsage;
 import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
@@ -35,8 +35,8 @@ import android.telephony.Annotation.SrvccState;
 import android.telephony.emergency.EmergencyNumber;
 import android.telephony.ims.ImsReasonInfo;
 
-import com.android.internal.telephony.IPhoneStateListener;
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.telephony.IPhoneStateListener;
 
 import dalvik.system.VMRuntime;
 
