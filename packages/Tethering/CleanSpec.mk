4fb92310b086b97c78fd211ea24020ea37274bca
markchien <markchien@google.com>
2019-12-16 20:15:20 +0800

    [Tether13] Move TetheringManager into framework
    Move tethering out of ConnectivityService. All client would
    use TetheringManager to talk with TetheringService directly.
    
    Bug: 144320246
    Test: -build, flash, boot
          -atest TetheringTests
    
    Change-Id: Ib051bea724a256f9c4572b566e46ae7b9c4abe6e
    Merged-In: Ib051bea724a256f9c4572b566e46ae7b9c4abe6e

--- packages/Tethering/CleanSpec.mk
+++ packages/Tethering/CleanSpec.mk
@@ -45,7 +45,13 @@
 #$(call add-clean-step, rm -rf $(PRODUCT_OUT)/data/*)
 
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/Tethering)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/apex/com.android.tethering.apex.apex)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/ETC/com.android.tethering.apex*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/apex/com.android.tethering.apex)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/InProcessTethering*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/InProcessTethering*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/InProcessTethering)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system_other/system/priv-app/InProcessTethering)
 
 # ******************************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST ABOVE THIS BANNER
