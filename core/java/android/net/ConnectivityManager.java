d3b3f50423a19ce96a1a2ee536eecd6a7850b952
Remi NGUYEN VAN <reminv@google.com>
2019-12-17 16:45:42 +0900

    Add API for NetworkMetadata
    NetworkMetadata will be used to hold metadata about networks that is
    useful to system applications like Settings, but should not be available
    to other apps as it holds internal information about network
    configuration that can contain user information (like location-related
    information).
    
    The first user of this data is the captive portal API with
    CaptivePortalData.
    
    Test: atest FrameworksNetTests
    Bug: 142837299
    Change-Id: I341175b5fece8ee00e19898af5e8eabe66cefbf3

--- core/java/android/net/ConnectivityManager.java
+++ core/java/android/net/ConnectivityManager.java
@@ -3266,12 +3266,16 @@ public class ConnectivityManager {
          * @param network The {@link Network} of the satisfying network.
          * @param networkCapabilities The {@link NetworkCapabilities} of the satisfying network.
          * @param linkProperties The {@link LinkProperties} of the satisfying network.
+         * @param metadata The {@link NetworkMetadata} of the satisfying network; null if the
+         *                 application is not allowed to receive it.
          * @param blocked Whether access to the {@link Network} is blocked due to system policy.
          * @hide
          */
         public void onAvailable(@NonNull Network network,
                 @NonNull NetworkCapabilities networkCapabilities,
-                @NonNull LinkProperties linkProperties, boolean blocked) {
+                @NonNull LinkProperties linkProperties,
+                @Nullable NetworkMetadata metadata,
+                boolean blocked) {
             // Internally only this method is called when a new network is available, and
             // it calls the callback in the same way and order that older versions used
             // to call so as not to change the behavior.
@@ -3283,6 +3287,9 @@ public class ConnectivityManager {
             onCapabilitiesChanged(network, networkCapabilities);
             onLinkPropertiesChanged(network, linkProperties);
             onBlockedStatusChanged(network, blocked);
+            if (metadata != null) {
+                onMetadataChanged(network, metadata);
+            }
         }
 
         /**
@@ -3446,6 +3453,11 @@ public class ConnectivityManager {
          */
         public void onBlockedStatusChanged(@NonNull Network network, boolean blocked) {}
 
+        /** @hide */
+        @SystemApi
+        public void onMetadataChanged(@NonNull Network network,
+                @NonNull NetworkMetadata metadata) {}
+
         private NetworkRequest networkRequest;
     }
 
@@ -3494,21 +3506,24 @@ public class ConnectivityManager {
     public static final int CALLBACK_RESUMED             = BASE + 10;
     /** @hide */
     public static final int CALLBACK_BLK_CHANGED         = BASE + 11;
+    /** @hide */
+    public static final int CALLBACK_METADATA_CHANGED    = BASE + 12;
 
     /** @hide */
     public static String getCallbackName(int whichCallback) {
         switch (whichCallback) {
-            case CALLBACK_PRECHECK:     return "CALLBACK_PRECHECK";
-            case CALLBACK_AVAILABLE:    return "CALLBACK_AVAILABLE";
-            case CALLBACK_LOSING:       return "CALLBACK_LOSING";
-            case CALLBACK_LOST:         return "CALLBACK_LOST";
-            case CALLBACK_UNAVAIL:      return "CALLBACK_UNAVAIL";
-            case CALLBACK_CAP_CHANGED:  return "CALLBACK_CAP_CHANGED";
-            case CALLBACK_IP_CHANGED:   return "CALLBACK_IP_CHANGED";
-            case EXPIRE_LEGACY_REQUEST: return "EXPIRE_LEGACY_REQUEST";
-            case CALLBACK_SUSPENDED:    return "CALLBACK_SUSPENDED";
-            case CALLBACK_RESUMED:      return "CALLBACK_RESUMED";
-            case CALLBACK_BLK_CHANGED:  return "CALLBACK_BLK_CHANGED";
+            case CALLBACK_PRECHECK:         return "CALLBACK_PRECHECK";
+            case CALLBACK_AVAILABLE:        return "CALLBACK_AVAILABLE";
+            case CALLBACK_LOSING:           return "CALLBACK_LOSING";
+            case CALLBACK_LOST:             return "CALLBACK_LOST";
+            case CALLBACK_UNAVAIL:          return "CALLBACK_UNAVAIL";
+            case CALLBACK_CAP_CHANGED:      return "CALLBACK_CAP_CHANGED";
+            case CALLBACK_IP_CHANGED:       return "CALLBACK_IP_CHANGED";
+            case EXPIRE_LEGACY_REQUEST:     return "EXPIRE_LEGACY_REQUEST";
+            case CALLBACK_SUSPENDED:        return "CALLBACK_SUSPENDED";
+            case CALLBACK_RESUMED:          return "CALLBACK_RESUMED";
+            case CALLBACK_BLK_CHANGED:      return "CALLBACK_BLK_CHANGED";
+            case CALLBACK_METADATA_CHANGED: return "CALLBACK_METADATA_CHANGED";
             default:
                 return Integer.toString(whichCallback);
         }
@@ -3560,7 +3575,9 @@ public class ConnectivityManager {
                 case CALLBACK_AVAILABLE: {
                     NetworkCapabilities cap = getObject(message, NetworkCapabilities.class);
                     LinkProperties lp = getObject(message, LinkProperties.class);
-                    callback.onAvailable(network, cap, lp, message.arg1 != 0);
+                    // TODO: send metadata from ConnectivityService
+                    NetworkMetadata metadata = null;
+                    callback.onAvailable(network, cap, lp, metadata, message.arg1 != 0);
                     break;
                 }
                 case CALLBACK_LOSING: {
@@ -3596,6 +3613,12 @@ public class ConnectivityManager {
                 case CALLBACK_BLK_CHANGED: {
                     boolean blocked = message.arg1 != 0;
                     callback.onBlockedStatusChanged(network, blocked);
+                    break;
+                }
+                case CALLBACK_METADATA_CHANGED: {
+                    NetworkMetadata metadata = getObject(message, NetworkMetadata.class);
+                    callback.onMetadataChanged(network, metadata);
+                    break;
                 }
             }
         }
---
+++ core/java/android/net/NetworkMetadata.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net;
+
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.annotation.SystemApi;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Objects;
+
+/**
+ * Metadata about a network to be used by the system or privileged applications.
+ * @hide
+ */
+@SystemApi
+public final class NetworkMetadata implements Parcelable {
+
+    // Metadata obtained from NetworkAgents
+    @Nullable
+    private final Uri mCaptivePortalApiUrl;
+
+    // Metadata obtained from NetworkMonitor
+    @Nullable
+    private final CaptivePortalData mCaptivePortalData;
+
+    /**
+     * A builder to create new {@link NetworkMetadata}.
+     */
+    public static class Builder {
+        private Uri mCaptivePortalApiUrl;
+        private CaptivePortalData mCaptivePortalData;
+
+        public Builder() {}
+        public Builder(@NonNull NetworkMetadata metadata) {
+            mCaptivePortalApiUrl = metadata.mCaptivePortalApiUrl;
+            mCaptivePortalData = metadata.mCaptivePortalData;
+        }
+
+        /**
+         * Set the URL where the network {@link CaptivePortalData} can be fetched.
+         */
+        @NonNull
+        public Builder setCaptivePortalApiUrl(@Nullable Uri captivePortalApiUrl) {
+            mCaptivePortalApiUrl = captivePortalApiUrl;
+            return this;
+        }
+
+        /**
+         * Set the {@link CaptivePortalData} that was obtained from the network.
+         */
+        @NonNull
+        public Builder setCaptivePortalData(@Nullable CaptivePortalData captivePortalData) {
+            mCaptivePortalData = captivePortalData;
+            return this;
+        }
+
+        /**
+         * Create a new {@link NetworkMetadata}.
+         */
+        @NonNull
+        public NetworkMetadata build() {
+            return new NetworkMetadata(mCaptivePortalApiUrl, mCaptivePortalData);
+        }
+    }
+
+    private NetworkMetadata(Uri captivePortalApiUrl, CaptivePortalData captivePortalData) {
+        mCaptivePortalApiUrl = captivePortalApiUrl;
+        mCaptivePortalData = captivePortalData;
+    }
+
+    private NetworkMetadata(Parcel source) {
+        this(source.readParcelable(Uri.class.getClassLoader()),
+                source.readParcelable(CaptivePortalData.class.getClassLoader()));
+    }
+
+    /**
+     * Return a new {@link NetworkMetadata} where fields sourced from {@link NetworkAgent} data are
+     * updated with values held by the argument.
+     * @hide
+     */
+    public NetworkMetadata mergeNetworkAgentMetadata(@Nullable NetworkMetadata metadata) {
+        if (metadata == null) return this;
+        return new NetworkMetadata.Builder(this)
+                .setCaptivePortalApiUrl(metadata.mCaptivePortalApiUrl)
+                .build();
+    }
+
+    /**
+     * Return a new {@link NetworkMetadata} where fields sourced from NetworkMonitor data are
+     * updated with values held by the argument.
+     * @hide
+     */
+    public NetworkMetadata mergeNetworkMonitorMetadata(@Nullable NetworkMetadata metadata) {
+        if (metadata == null) return this;
+        return new NetworkMetadata.Builder(this)
+                .setCaptivePortalData(metadata.getCaptivePortalData())
+                .build();
+    }
+
+    @Nullable
+    public Uri getCaptivePortalApiUrl() {
+        return mCaptivePortalApiUrl;
+    }
+
+    @Nullable
+    public CaptivePortalData getCaptivePortalData() {
+        return mCaptivePortalData;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeParcelable(mCaptivePortalApiUrl, 0);
+        dest.writeParcelable(mCaptivePortalData, 0);
+    }
+
+    @NonNull
+    public static final Creator<NetworkMetadata> CREATOR = new Creator<NetworkMetadata>() {
+        @Override
+        public NetworkMetadata createFromParcel(Parcel source) {
+            return new NetworkMetadata(source);
+        }
+
+        @Override
+        public NetworkMetadata[] newArray(int size) {
+            return new NetworkMetadata[size];
+        }
+    };
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof NetworkMetadata)) return false;
+        final NetworkMetadata other = (NetworkMetadata) o;
+        return Objects.equals(mCaptivePortalApiUrl, other.mCaptivePortalApiUrl)
+                && Objects.equals(mCaptivePortalData, other.mCaptivePortalData);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mCaptivePortalApiUrl, mCaptivePortalData);
+    }
+
+    @Override
+    public String toString() {
+        return "NetworkMetadata {"
+                + "portalApiUrl: " + mCaptivePortalApiUrl
+                + ", captivePortalData: " + mCaptivePortalData
+                + "}";
+    }
+}
