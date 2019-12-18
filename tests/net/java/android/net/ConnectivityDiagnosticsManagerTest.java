059984e19bd439272d098f2fa92ea952d3f7008c
Cody Kesting <ckesting@google.com>
2019-12-18 10:57:50 -0800

    Add callback registration in ConnectivityService.
    ConnectivityDiagnosticsManager will send callbacks to
    ConnectivityService for registering and unregistering them with the
    system. ConnectivityService needs to do the processing for persisting
    (and deleting) these callbacks on the ConnectivityService Thread, so
    messages are sent to the Connectivity Diagnostics  Handler, which runs
    on the ConnectivityService Thread.
    
    Bug: 146444622
    Bug: 143187964
    Test: compiles
    Test: atest FrameworksNetTests
    Change-Id: Ia5c8f90a60c050504e8676de9564a7607a9b03bc

--- tests/net/java/android/net/ConnectivityDiagnosticsManagerTest.java
+++ tests/net/java/android/net/ConnectivityDiagnosticsManagerTest.java
@@ -26,14 +26,21 @@ import static com.android.testutils.ParcelUtilsKt.assertParcelingIsLossless;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
+import android.content.Context;
 import android.os.PersistableBundle;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mock;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
@@ -48,11 +55,19 @@ public class ConnectivityDiagnosticsManagerTest {
     private static final String BUNDLE_KEY = "key";
     private static final String BUNDLE_VALUE = "value";
 
+    @Mock private Context mContext;
+    @Mock private IConnectivityManager mService;
+
     private Executor mInlineExecutor;
+    private ConnectivityDiagnosticsManager mManager;
 
     @Before
     public void setUp() {
+        mContext = mock(Context.class);
+        mService = mock(IConnectivityManager.class);
+
         mInlineExecutor = x -> x.run();
+        mManager = new ConnectivityDiagnosticsManager(mContext, mService);
     }
 
     private ConnectivityReport getSampleConnectivityReport() {
@@ -191,5 +206,25 @@ public class ConnectivityDiagnosticsManagerTest {
         // immediately check the latch without waiting.
         cb.mBinder.onNetworkConnectivityReported(n, connectivity);
         assertEquals(0, latch.getCount());
+    }
+
+    @Test
+    public void testRegisterConnectivityDiagnosticsCallback() throws Exception {
+        final NetworkRequest request = new NetworkRequest.Builder().build();
+        final ConnectivityDiagnosticsCallback cb = new ConnectivityDiagnosticsCallback() {};
+        mManager.registerConnectivityDiagnosticsCallback(request, mInlineExecutor, cb);
+
+        assertNotNull(cb.mExecutor);
+        verify(mService).registerConnectivityDiagnosticsCallback(eq(cb.mBinder), eq(request));
+    }
+
+    @Test
+    public void testUnregisterConnectivityDiagnosticsCallback() throws Exception {
+        final ConnectivityDiagnosticsCallback cb = new ConnectivityDiagnosticsCallback() {};
+        cb.setExecutor(mInlineExecutor);
+        mManager.unregisterConnectivityDiagnosticsCallback(cb);
+
+        assertNull(cb.mExecutor);
+        verify(mService).unregisterConnectivityDiagnosticsCallback(eq(cb.mBinder));
     }
 }
