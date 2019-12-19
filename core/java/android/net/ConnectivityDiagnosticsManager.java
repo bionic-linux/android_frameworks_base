ab1025e1286e937166152a0749dd1fdbd6fb9564
Cody Kesting <ckesting@google.com>
2019-12-19 12:36:18 -0800

    Update javadocs for ConnectivityDiagnosticsManager.
    ConnectivityDiagnosticsManager comments for
    registerConnectivityDiagnosticsCallback and
    unregisterConnectivityDiagnosticsCallback are updated to reflect several
    changes.
    
    For register calls, any app will be able to register callbacks, but only
    permissioned applications will have their callbacks invoked (and only
    for networks managed by the application). Additionally, only the
    registering app (uid) will be able to unregister a callback once
    registered.
    
    Bug: 143187964
    Test: docs change only. compiles.
    Change-Id: Ie7ae86a1afccb22d6c84027dbac49d7b8e431e8c

--- core/java/android/net/ConnectivityDiagnosticsManager.java
+++ core/java/android/net/ConnectivityDiagnosticsManager.java
@@ -472,8 +472,9 @@ public class ConnectivityDiagnosticsManager {
     /**
      * Registers a ConnectivityDiagnosticsCallback with the System.
      *
-     * <p>Only apps that offer network connectivity to the user are allowed to register callbacks.
-     * This includes:
+     * <p>Only apps that offer network connectivity to the user should be registering callbacks.
+     * These are the only apps whose callbacks will be invoked by the system. Categories of apps
+     * meeting this criteria are:
      *
      * <ul>
      *   <li>Carrier apps with active subscriptions
@@ -481,8 +482,7 @@ public class ConnectivityDiagnosticsManager {
      *   <li>WiFi Suggesters
      * </ul>
      *
-     * <p>Callbacks will be limited to receiving notifications for networks over which apps provide
-     * connectivity.
+     * <p>Callbacks registered by apps not meeting the above criteria will not be invoked.
      *
      * <p>If a registering app loses its relevant permissions, any callbacks it registered will
      * silently stop receiving callbacks.
@@ -498,8 +498,6 @@ public class ConnectivityDiagnosticsManager {
      *     System
      * @throws IllegalArgumentException if the same callback instance is registered with multiple
      *     NetworkRequests
-     * @throws SecurityException if the caller does not have appropriate permissions to register a
-     *     callback
      */
     public void registerConnectivityDiagnosticsCallback(
             @NonNull NetworkRequest request,
