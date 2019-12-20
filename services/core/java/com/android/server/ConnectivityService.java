a7cd065b07b9cb618b2ad964708530ecc3dbe429
Benedict Wong <benedictwong@google.com>
2019-12-19 21:15:25 -0800

    WIP: Grant minimum permission for always-on VPNs
    This change makes sure that the minimum appop (OP_ACTIVATE_VPN or
    OP_ACTIVATE_PLATFORM_VPN) is granted, instead of always granting the
    OP_ACTIVATE_VPN.
    
    Test: Tests updated
    Test: TODO: Add new tests
    Change-Id: Id4369a5aaebee9ee420cca568e2ce27ccca888b7

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -4637,11 +4637,11 @@ public class ConnectivityService extends IConnectivityManager.Stub
                 Slog.w(TAG, "User " + userId + " has no Vpn configuration");
                 return false;
             }
-            if (!vpn.setAlwaysOnPackage(packageName, lockdown, lockdownWhitelist)) {
+            if (!vpn.setAlwaysOnPackage(packageName, lockdown, lockdownWhitelist, mKeystore)) {
                 return false;
             }
             if (!startAlwaysOnVpn(userId)) {
-                vpn.setAlwaysOnPackage(null, false, null);
+                vpn.setAlwaysOnPackage(null, false, null, mKeystore);
                 return false;
             }
         }
@@ -4920,7 +4920,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
             if (TextUtils.equals(vpn.getAlwaysOnPackage(), packageName) && !isReplacing) {
                 Slog.d(TAG, "Removing always-on VPN package " + packageName + " for user "
                         + userId);
-                vpn.setAlwaysOnPackage(null, false, null);
+                vpn.setAlwaysOnPackage(null, false, null, mKeystore);
             }
         }
     }
