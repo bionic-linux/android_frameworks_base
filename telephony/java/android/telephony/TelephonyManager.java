6ae0434d3ce6696f13664ef23179412ed66bc290
evitayan <evitayan@google.com>
2019-12-30 11:19:47 -0800

    Allow mainline IPsec(IKE) to statically include @UiccAppType
    @UiccAppType was hidden API defined in TelephonyManager and
    mainline module IPsec(IKE) depended on it.
    
    To remove this hidden API dependency, this commit:
    - Moves definition of @UiccAppType from TelephonyManager.java
      to Annotiation.java
    - Adds android.telephony.Annotation to framework-ike-shared-srcs
      so that mainline module IPsec(IKE) can statically include it.
    
    Bug: 146360859
    Test: build, flash, boot
    Test: atest FrameworksIkeTests
    Change-Id: I58b733fd6c020b76313b750e18eebb7bd0c12e7c

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -73,6 +73,7 @@ import android.telephony.Annotation.DataState;
 import android.telephony.Annotation.NetworkType;
 import android.telephony.Annotation.RadioPowerState;
 import android.telephony.Annotation.SimActivationState;
+import android.telephony.Annotation.UiccAppType;
 import android.telephony.VisualVoicemailService.VisualVoicemailTask;
 import android.telephony.emergency.EmergencyNumber;
 import android.telephony.emergency.EmergencyNumber.EmergencyServiceCategories;
@@ -6672,19 +6673,6 @@ public class TelephonyManager {
         }
     }
 
-    /**
-     * UICC SIM Application Types
-     * @hide
-     */
-    @IntDef(prefix = { "APPTYPE_" }, value = {
-            APPTYPE_SIM,
-            APPTYPE_USIM,
-            APPTYPE_RUIM,
-            APPTYPE_CSIM,
-            APPTYPE_ISIM
-    })
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface UiccAppType{}
     /** UICC application type is SIM */
     public static final int APPTYPE_SIM = PhoneConstants.APPTYPE_SIM;
     /** UICC application type is USIM */
