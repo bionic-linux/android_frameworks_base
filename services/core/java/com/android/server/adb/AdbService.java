99c1d42ad96eddd7be5d871ffee950515c39fa23
Joshua Duong <joshuaduong@google.com>
2020-01-02 14:53:32 -0800

    Add Settings.Global.ADB_WIFI_ENABLED.
    ADB_ENABLED historically meant the state for USB debugging. Since
    wireless debugging can be enabled separately, define another setting
    for it.
    
    BUG: b/111434128
    
    Test: make
    Change-Id: If3abca8e77381d6832f55d55a43c52ee1a1267d1

--- services/core/java/com/android/server/adb/AdbService.java
+++ services/core/java/com/android/server/adb/AdbService.java
@@ -132,9 +132,13 @@ public class AdbService extends IAdbManager.Stub {
                 mAdbWifiEnabled = false;
 
                 // register observer to listen for settings changes
+                mObserver = new AdbSettingsObserver();
                 mContentResolver.registerContentObserver(
                         Settings.Global.getUriFor(Settings.Global.ADB_ENABLED),
-                        false, new AdbSettingsObserver());
+                        false, mObserver);
+                mContentResolver.registerContentObserver(
+                        Settings.Global.getUriFor(Settings.Global.ADB_WIFI_ENABLED),
+                        false, mObserver);
             } catch (Exception e) {
                 Slog.e(TAG, "Error initializing AdbHandler", e);
             }
@@ -171,10 +175,13 @@ public class AdbService extends IAdbManager.Stub {
                     break;
             }
         }
+
+        private ContentObserver mObserver;
     }
 
     private class AdbSettingsObserver extends ContentObserver {
         private final Uri mAdbUsbUri = Settings.Global.getUriFor(Settings.Global.ADB_ENABLED);
+        private final Uri mAdbWifiUri = Settings.Global.getUriFor(Settings.Global.ADB_WIFI_ENABLED);
 
         AdbSettingsObserver() {
             super(null);
@@ -186,8 +193,11 @@ public class AdbService extends IAdbManager.Stub {
                 boolean enable = (Settings.Global.getInt(mContentResolver,
                         Settings.Global.ADB_ENABLED, 0) > 0);
                 mHandler.sendMessage(MSG_ENABLE_ADB, enable, AdbTransportType.USB);
+            } else if (mAdbWifiUri.equals(uri)) {
+                boolean enable = (Settings.Global.getInt(mContentResolver,
+                        Settings.Global.ADB_WIFI_ENABLED, 0) > 0);
+                mHandler.sendMessage(MSG_ENABLE_ADB, enable, AdbTransportType.WIFI);
             }
-            // TODO: Add condition for WIFI transport
         }
     }
 
@@ -238,6 +248,8 @@ public class AdbService extends IAdbManager.Stub {
         try {
             Settings.Global.putInt(mContentResolver,
                     Settings.Global.ADB_ENABLED, mAdbUsbEnabled ? 1 : 0);
+            Settings.Global.putInt(mContentResolver,
+                    Settings.Global.ADB_WIFI_ENABLED, mAdbWifiEnabled ? 1 : 0);
         } catch (SecurityException e) {
             // If UserManager.DISALLOW_DEBUGGING_FEATURES is on, that this setting can't be changed.
             Slog.d(TAG, "ADB_ENABLED is restricted.");
