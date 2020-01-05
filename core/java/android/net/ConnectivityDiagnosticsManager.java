f56fae8db022a48da2c12af66395614a8c5e0997
Cody Kesting <ckesting@google.com>
2020-01-05 14:06:39 -0800

    Implement INetworkMonitorCallbacks#notifyNetworkTestedWithExtras.
    INetworkMonitorCallbacks defines notifyNetworkTestedWithExtras() for
    notifying ConnectivityService of networks being tested along with a
    PersistableBundle of extras. A new event is introduced for
    NetworkStateTrackerHandler to notify the ConnectivityDiagnosticsHandler
    before continuing with the normal processing for "network tested"
    notifications. The event is also used in the
    ConnectivityDiagnosticsHandler.
    
    Bug: 143187964
    Test: compiles.
    Test: atest CtsNetTestCases FrameworksNetTests
    Change-Id: Iab29da790c0f5faae68227770bc3a84bbc94f124

--- core/java/android/net/ConnectivityDiagnosticsManager.java
+++ core/java/android/net/ConnectivityDiagnosticsManager.java
@@ -531,8 +531,8 @@ public class ConnectivityDiagnosticsManager {
      * Registers a ConnectivityDiagnosticsCallback with the System.
      *
      * <p>Only apps that offer network connectivity to the user should be registering callbacks.
-     * These are the only apps whose callbacks will be invoked by the system. Apps considered to
-     * meet these conditions include:
+     * These are the only apps whose callbacks will be invoked by the system. Apps that are
+     * considered to offer network connectivity generally include:
      *
      * <ul>
      *   <li>Carrier apps with active subscriptions
