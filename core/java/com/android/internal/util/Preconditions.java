1b613861305c2b29e7178005928c9df5ff359706
Benedict Wong <benedictwong@google.com>
2020-01-13 19:07:35 -0800

    Re-add string-format version of Preconditions.checkNotNull
    This change re-adds the string format overload of
    Preconditions.checkNotNull as a utility method. This calls directly into
    the Objects.requireNonNull, with the string formatted as per the
    arguments.
    
    Test: compiles
    Change-Id: Ida6e60e517bcae8697061acdbb6f4ef8eb3c5893

--- core/java/com/android/internal/util/Preconditions.java
+++ core/java/com/android/internal/util/Preconditions.java
@@ -22,6 +22,7 @@ import android.annotation.UnsupportedAppUsage;
 import android.text.TextUtils;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Simple static methods to be called at the start of your own methods to verify
@@ -155,6 +156,23 @@ public class Preconditions {
             throw new NullPointerException(String.valueOf(errorMessage));
         }
         return reference;
+    }
+
+    /**
+     * Ensures that an object reference passed as a parameter to the calling
+     * method is not null.
+     *
+     * @param reference an object reference
+     * @param messageTemplate a printf-style message template to use if the check fails; will
+     *     be converted to a string using {@link String#format(String, Object...)}
+     * @param messageArgs arguments for {@code messageTemplate}
+     * @return the non-null reference that was validated
+     * @throws NullPointerException if {@code reference} is null
+     */
+    public static @NonNull <T> T checkNotNull(final T reference,
+            final String messageTemplate,
+            final Object... messageArgs) {
+        return Objects.requireNonNull(reference, String.format(messageTemplate, messageArgs));
     }
 
     /**
