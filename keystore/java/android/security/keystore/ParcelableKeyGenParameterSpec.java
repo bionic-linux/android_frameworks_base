9eac85cc55cb2ca0b619433b3ce86b86a1010fe9
Max Bires <jbires@google.com>
2020-01-14 16:10:36 -0800

    Changing op per auth keys to allow password
    Previously, auth per operation keystore keys could only be authorized
    with biometrics. There is no reason to restrict this functionality to
    biometrics. This change slightly refactors the key parameter builder
    interface to allow the caller to specify which authentication types
    should be allowed for an auth per op key.
    
    Bug: 147693375
    Bug: 140256692
    Test: atest keystore
    Change-Id: I5cbf3d4e3f0e84d577dbf6b4cb356b1010100925

--- keystore/java/android/security/keystore/ParcelableKeyGenParameterSpec.java
+++ keystore/java/android/security/keystore/ParcelableKeyGenParameterSpec.java
@@ -97,6 +97,7 @@ public final class ParcelableKeyGenParameterSpec implements Parcelable {
         out.writeBoolean(mSpec.isRandomizedEncryptionRequired());
         out.writeBoolean(mSpec.isUserAuthenticationRequired());
         out.writeInt(mSpec.getUserAuthenticationValidityDurationSeconds());
+        out.writeInt(mSpec.getUserAuthenticationType());
         out.writeBoolean(mSpec.isUserPresenceRequired());
         out.writeByteArray(mSpec.getAttestationChallenge());
         out.writeBoolean(mSpec.isUniqueIdIncluded());
@@ -152,6 +153,7 @@ public final class ParcelableKeyGenParameterSpec implements Parcelable {
         final boolean randomizedEncryptionRequired = in.readBoolean();
         final boolean userAuthenticationRequired = in.readBoolean();
         final int userAuthenticationValidityDurationSeconds = in.readInt();
+        final int userAuthenticationTypes = in.readInt();
         final boolean userPresenceRequired = in.readBoolean();
         final byte[] attestationChallenge = in.createByteArray();
         final boolean uniqueIdIncluded = in.readBoolean();
@@ -183,6 +185,7 @@ public final class ParcelableKeyGenParameterSpec implements Parcelable {
                 randomizedEncryptionRequired,
                 userAuthenticationRequired,
                 userAuthenticationValidityDurationSeconds,
+                userAuthenticationTypes,
                 userPresenceRequired,
                 attestationChallenge,
                 uniqueIdIncluded,
