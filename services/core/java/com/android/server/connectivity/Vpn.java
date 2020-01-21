f4be529c353012eb3da20d29b8b5db3d7d6339d1
Benedict Wong <benedictwong@google.com>
2020-01-20 22:14:59 -0800

    Enforce restricted user, establishingAppUid checks
    This CL adds checks to ensure restricted users cannot change or
    start/stop platform VPNs. In addition, this also adds checks to the
    ConnectivityManager#getEstablishingAppUid() to ensure that only
    VpnService based VPNs can identify connections
    
    Bug: 148040659
    Test: FrameworksNetTests run
    Change-Id: Id47ada5766036bfc84f3ba47f66f2d2683af916d

--- services/core/java/com/android/server/connectivity/Vpn.java
+++ services/core/java/com/android/server/connectivity/Vpn.java
@@ -1125,9 +1125,7 @@ public class Vpn {
         try {
             // Restricted users are not allowed to create VPNs, they are tied to Owner
             UserInfo user = mgr.getUserInfo(mUserHandle);
-            if (user.isRestricted()) {
-                throw new SecurityException("Restricted users cannot establish VPNs");
-            }
+            enforceRestrictedUser();
 
             ResolveInfo info = AppGlobals.getPackageManager().resolveService(intent,
                     null, 0, mUserHandle);
@@ -1678,6 +1676,18 @@ public class Vpn {
         return mNetworkCapabilities.appliesToUid(uid);
     }
 
+    public synchronized int getVpnType() {
+        if (VpnConfig.SETTINGS_VPN.equals(mPackage)) {
+            return VpnManager.TYPE_PLATFORM_VPN;
+        }
+
+        if (mVpnRunner != null && mVpnRunner instanceof IkeV2VpnRunner) {
+            return VpnManager.TYPE_PLATFORM_VPN;
+        } else {
+            return VpnManager.TYPE_VPN_SERVICE;
+        }
+    }
+
     /**
      * @param uid The target uid.
      *
@@ -1800,6 +1810,14 @@ public class Vpn {
         }
 
         throw new IllegalStateException("Unable to find IPv4 default gateway");
+    }
+
+    private void enforceRestrictedUser() {
+        UserManager mgr = UserManager.get(mContext);
+        UserInfo user = mgr.getUserInfo(mUserHandle);
+        if (user.isRestricted()) {
+            throw new SecurityException("Restricted users cannot establish VPNs");
+        }
     }
 
     /**
@@ -2908,6 +2926,7 @@ public class Vpn {
         checkNotNull(keyStore, "KeyStore missing");
 
         verifyCallingUidAndPackage(packageName);
+        enforceRestrictedUser();
 
         // Permissions checked during startVpnProfile()
         Binder.withCleanCallingIdentity(
@@ -2938,6 +2957,7 @@ public class Vpn {
         checkNotNull(keyStore, "KeyStore missing");
 
         verifyCallingUidAndPackage(packageName);
+        enforceRestrictedUser();
 
         Binder.withCleanCallingIdentity(
                 () -> {
@@ -2977,6 +2997,8 @@ public class Vpn {
             @NonNull String packageName, @NonNull KeyStore keyStore) {
         checkNotNull(packageName, "No package name provided");
         checkNotNull(keyStore, "KeyStore missing");
+
+        enforceRestrictedUser();
 
         // Prepare VPN for startup
         if (!prepare(packageName, null /* newPackage */, VpnManager.TYPE_PLATFORM_VPN)) {
@@ -3035,6 +3057,8 @@ public class Vpn {
      */
     public synchronized void stopVpnProfile(@NonNull String packageName) {
         checkNotNull(packageName, "No package name provided");
+
+        enforceRestrictedUser();
 
         // To stop the VPN profile, the caller must be the current prepared package. Otherwise,
         // the app is not prepared, and we can just return.
