1491f4667b37ca0cd8cf37eadc652df6246a3acb
Lorenzo Colitti <lorenzo@google.com>
2020-03-18 11:42:25 +0900

    Release the interface request when Ethernet tethering is stopped.
    When the Ethernet interface becomes unavailable (e.g., because
    the cable was unplugged or the interface was removed), or when
    setEthernetTethering(false) is called, release the Ethernet
    interface request.
    
    This ensures that:
    - The Ethernet interface immediately becomes available for use in
      client mode.
    - If an interface later becomes available, tethering is not
      automatically started. This is consistent with what happens for
      other downstream types such as wifi and USB. Evey time one of
      those downstreams goes down, tethering is stopped and will not
      be restarted.
    
    Test: manual
    Bug: 148824036
    Change-Id: Iaf85e800569f2e08c39f7ebb96f8aa34f6e53133

--- packages/Tethering/src/com/android/server/connectivity/tethering/Tethering.java
+++ packages/Tethering/src/com/android/server/connectivity/tethering/Tethering.java
@@ -611,11 +611,6 @@ public class Tethering {
                 mEthernetIfaceRequest = em.requestTetheredInterface(mExecutor, mEthernetCallback);
             } else {
                 stopEthernetTetheringLocked();
-                if (mEthernetCallback != null) {
-                    mEthernetIfaceRequest.release();
-                    mEthernetCallback = null;
-                    mEthernetIfaceRequest = null;
-                }
             }
         }
         return TETHER_ERROR_NO_ERROR;
@@ -626,6 +621,11 @@ public class Tethering {
         changeInterfaceState(mConfiguredEthernetIface, IpServer.STATE_AVAILABLE);
         stopTrackingInterfaceLocked(mConfiguredEthernetIface);
         mConfiguredEthernetIface = null;
+        if (mEthernetCallback != null) {
+            mEthernetIfaceRequest.release();
+            mEthernetCallback = null;
+            mEthernetIfaceRequest = null;
+        }
     }
 
     private class EthernetCallback implements EthernetManager.TetheredInterfaceCallback {
