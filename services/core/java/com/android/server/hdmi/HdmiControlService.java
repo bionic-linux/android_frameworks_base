21c21f51e785aa19bd0656dc5e81593f416d0e05
Tarundeep Singh <tarundeep.singh@ittiam.com>
2020-03-19 18:03:25 +0530

    Remove some hdmi ro preperties
    property_hdmi_cec_never_claim_playback_logical_address
    property_hdmi_cec_never_assign_logical_addresses
    
    Bug: (To be filed)
    Test: m services.core
    Change-Id: Id5db6b781253b874ce1170fe827717ed74e7ccce

--- services/core/java/com/android/server/hdmi/HdmiControlService.java
+++ services/core/java/com/android/server/hdmi/HdmiControlService.java
@@ -152,10 +152,6 @@ public class HdmiControlService extends SystemService {
     @GuardedBy("mLock")
     private boolean mSystemAudioActivated = false;
 
-    private static final boolean isHdmiCecNeverClaimPlaybackLogicAddr =
-            SystemProperties.getBoolean(
-                    Constants.PROPERTY_HDMI_CEC_NEVER_CLAIM_PLAYBACK_LOGICAL_ADDRESS, false);
-
     /**
      * Interface to report send result.
      */
@@ -704,8 +700,7 @@ public class HdmiControlService extends SystemService {
         // A container for [Device type, Local device info].
         ArrayList<HdmiCecLocalDevice> localDevices = new ArrayList<>();
         for (int type : mLocalDevices) {
-            if (type == HdmiDeviceInfo.DEVICE_PLAYBACK
-                    && isHdmiCecNeverClaimPlaybackLogicAddr) {
+            if (type == HdmiDeviceInfo.DEVICE_PLAYBACK) {
                 continue;
             }
             HdmiCecLocalDevice localDevice = mCecController.getLocalDevice(type);
@@ -1105,8 +1100,7 @@ public class HdmiControlService extends SystemService {
             }
             ArrayList<HdmiCecLocalDevice> localDevices = new ArrayList<>();
             for (int type : mLocalDevices) {
-                if (type == HdmiDeviceInfo.DEVICE_PLAYBACK
-                        && isHdmiCecNeverClaimPlaybackLogicAddr) {
+                if (type == HdmiDeviceInfo.DEVICE_PLAYBACK) {
                     continue;
                 }
                 HdmiCecLocalDevice localDevice = mCecController.getLocalDevice(type);
