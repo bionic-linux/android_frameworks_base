fca6b169d9087375f63305c63b5fcafea63cfcd8
calvinpan <calvinpan@google.com>
2020-05-05 11:52:25 +0800

    Update getSimCarrierId() document
    Move carrier_list.pb to latest_carrierid/ folder
    
    Bug: 155254987
    Test: Build pass
    Change-Id: Iacf1bfe68fa86194e8b75cbfce9aa37c0abfbca3

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -10314,7 +10314,7 @@ public class TelephonyManager {
      * <p>To recognize a carrier (including MVNO) as a first-class identity, Android assigns each
      * carrier with a canonical integer a.k.a. carrier id. The carrier ID is an Android
      * platform-wide identifier for a carrier. AOSP maintains carrier ID assignments in
-     * <a href="https://android.googlesource.com/platform/packages/providers/TelephonyProvider/+/master/assets/carrier_list.textpb">here</a>
+     * <a href="https://android.googlesource.com/platform/packages/providers/TelephonyProvider/+/master/assets/latest_carrier_id/carrier_list.textpb">here</a>
      *
      * <p>Apps which have carrier-specific configurations or business logic can use the carrier id
      * as an Android platform-wide identifier for carriers.
