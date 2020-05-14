a8e3f85f16d611b5ed07bff6145e0da15cd11008
Francois Gaffie <francois.gaffie@renault.com>
2020-05-14 15:25:19 +0200

    AudioService: user switch management
    Upon user switch, cache volume index table shall be cleared.
    If no volume has been saved in SettingsDB for current device,
    this CL forces a sync on this device to prevent start any player
    on previous user index.
    
    Test: adb shell am instrument -w -e class com.android.audiopolicytest.UserSwitchVolumeTest -w com.android.audiopolicytest
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: I31a590a9a9f01679684a9959c6999ef26aa12e24

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -2240,8 +2240,8 @@ public class AudioService extends IAudioService.Stub
         final VolumeGroupState vgs = sVolumeGroupStates.get(volumeGroup);
 
         sVolumeLogger.log(new VolumeEvent(VolumeEvent.VOL_SET_GROUP_VOL, attr, vgs.name(),
-                index/*val1*/, flags/*val2*/, callingPackage));
-
+                index/*val1*/, flags/*val2*/, callingPackage + ", user "
+                + ActivityManager.getCurrentUser()));
         vgs.setVolumeIndex(index, flags);
 
         // For legacy reason, propagate to all streams associated to this volume group
@@ -3658,7 +3658,7 @@ public class AudioService extends IAudioService.Stub
             }
         }
 
-        readVolumeGroupsSettings();
+        readVolumeGroupsSettings(userSwitch);
     }
 
     /** @see AudioManager#setSpeakerphoneOn(boolean) */
@@ -4571,7 +4571,7 @@ public class AudioService extends IAudioService.Stub
         }
         for (int i = 0; i < sVolumeGroupStates.size(); i++) {
             final VolumeGroupState vgs = sVolumeGroupStates.valueAt(i);
-            vgs.applyAllVolumes();
+            vgs.applyAllVolumes(false/*userSwitch*/);
         }
     }
 
@@ -4584,14 +4584,17 @@ public class AudioService extends IAudioService.Stub
         }
     }
 
-    private void readVolumeGroupsSettings() {
+    private void readVolumeGroupsSettings(boolean userSwitch) {
         if (DEBUG_VOL) {
-            Log.v(TAG, "readVolumeGroupsSettings.");
+            Log.v(TAG, "readVolumeGroupsSettings userSwitch=" + userSwitch);
+        }
+        for (int i = 0; i < sVolumeGroupStates.size(); i++) {
+            sVolumeGroupStates.valueAt(i).clearIndexCache();
         }
         for (int i = 0; i < sVolumeGroupStates.size(); i++) {
             final VolumeGroupState vgs = sVolumeGroupStates.valueAt(i);
             vgs.readSettings();
-            vgs.applyAllVolumes();
+            vgs.applyAllVolumes(userSwitch);
         }
     }
 
@@ -4602,7 +4605,7 @@ public class AudioService extends IAudioService.Stub
         }
         for (int i = 0; i < sVolumeGroupStates.size(); i++) {
             final VolumeGroupState vgs = sVolumeGroupStates.valueAt(i);
-            vgs.applyAllVolumes();
+            vgs.applyAllVolumes(false/*userSwitch*/);
         }
     }
 
@@ -4717,7 +4720,7 @@ public class AudioService extends IAudioService.Stub
             // As for VSS, mute shall apply minIndex to all devices found in IndexMap and default.
             if (changed) {
                 mIsMuted = muted;
-                applyAllVolumes();
+                applyAllVolumes(false /*userSwitch*/);
             }
             return changed;
         }
@@ -4829,7 +4832,7 @@ public class AudioService extends IAudioService.Stub
             return (stream != AudioSystem.STREAM_DEFAULT) && (stream < mStreamStates.length);
         }
 
-        public void applyAllVolumes() {
+        public void applyAllVolumes(boolean userSwitch) {
             final String caller = "from vgs";
             synchronized (VolumeStreamState.class) {
                 // apply device specific volumes first
@@ -4872,11 +4875,16 @@ public class AudioService extends IAudioService.Stub
                 // apply default volume last: by convention , default device volume will be used
                 final int index = getIndex(AudioSystem.DEVICE_OUT_DEFAULT);
                 boolean synced = false;
+                final int deviceForVolume = getDeviceForVolume();
+                boolean forceDeviceSync = userSwitch && (mIndexMap.indexOfKey(deviceForVolume) < 0);
                 for (int stream : getLegacyStreamTypes()) {
                     if (isValidStream(stream)) {
                         final boolean streamMuted = mStreamStates[stream].mIsMuted;
                         final int defaultStreamIndex = (mStreamStates[stream].getIndex(
                                         AudioSystem.DEVICE_OUT_DEFAULT) + 5) / 10;
+                        if (forceDeviceSync) {
+                            mStreamStates[stream].setIndex(index * 10, deviceForVolume, caller);
+                        }
                         if (defaultStreamIndex == index && (isMuted() == streamMuted)
                                 && isVssMuteBijective(stream)) {
                             synced = true;
@@ -4898,6 +4906,14 @@ public class AudioService extends IAudioService.Stub
                     }
                     setVolumeIndexInt(isMuted() ? mIndexMin : index, AudioSystem.DEVICE_OUT_DEFAULT, 0);
                 }
+                if (forceDeviceSync) {
+                    if (DEBUG_VOL) {
+                        Log.v(TAG, "applyAllVolumes: forceDeviceSync index " + index
+                                + ", device " + AudioSystem.getOutputDeviceName(deviceForVolume)
+                                + ", group " + mAudioVolumeGroup.name());
+                    }
+                    setVolumeIndexInt(isMuted() ? mIndexMin : index, deviceForVolume, 0);
+                }
             }
         }
 
@@ -4918,6 +4934,10 @@ public class AudioService extends IAudioService.Stub
             if (!success) {
                 Log.e(TAG, "persistVolumeGroup failed for group " +  mAudioVolumeGroup.name());
             }
+        }
+
+        public void clearIndexCache() {
+            mIndexMap.clear();
         }
 
         public void readSettings() {
