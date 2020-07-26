0fb7b2d50841e300c08c120eec04bf6cf26ee4c9
Chalard Jean <jchalard@google.com>
2020-07-26 20:02:04 +0900

    Resolve the endpoint in legacy VPN
    Test: VpnTest and in particular VpnTest#testStartRacoon
          Fixes the test meant to test this
    Bug: 158974172
    
    Change-Id: Iefd941da06ce4d6466660b3c1e10670b54cc3c3a

--- services/core/java/com/android/server/connectivity/Vpn.java
+++ services/core/java/com/android/server/connectivity/Vpn.java
@@ -48,6 +48,7 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.content.pm.UserInfo;
 import android.net.ConnectivityManager;
+import android.net.DnsResolver;
 import android.net.INetworkManagementEventObserver;
 import android.net.Ikev2VpnProfile;
 import android.net.IpPrefix;
@@ -79,6 +80,7 @@ import android.net.ipsec.ike.IkeSessionParams;
 import android.os.Binder;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
+import android.os.CancellationSignal;
 import android.os.FileUtils;
 import android.os.IBinder;
 import android.os.INetworkManagementService;
@@ -136,6 +138,8 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -329,13 +333,48 @@ public class Vpn {
             // Nothing to do here, this is used by the test to check state.
         }
 
-        // TODO : use this.
         public InetAddress resolve(final String endPoint) throws UnknownHostException {
             try {
                 return InetAddress.parseNumericAddress(endPoint);
             } catch (IllegalArgumentException e) {
-                return InetAddress.getByName(endPoint);
+                Log.e(TAG, "Endpoint is not numeric, resolving");
             }
+
+            final CancellationSignal cancellationSignal = new CancellationSignal();
+            try {
+                final DnsResolver resolver = DnsResolver.getInstance();
+                final CompletableFuture<InetAddress> result = new CompletableFuture();
+                final DnsResolver.Callback<List<InetAddress>> cb =
+                        new DnsResolver.Callback<List<InetAddress>>() {
+                            @Override
+                            public void onAnswer(@NonNull final List<InetAddress> answer,
+                                    final int rcode) {
+                                if (answer.size() > 0) {
+                                    result.complete(answer.get(0));
+                                } else {
+                                    result.completeExceptionally(new UnknownHostException());
+                                }
+                            }
+
+                            @Override
+                            public void onError(@Nullable final DnsResolver.DnsException error) {
+                                // Unfortunately UnknownHostException doesn't accept a cause, so
+                                // print a message here instead
+                                Log.e(TAG, "Async dns resolver error", error);
+                                result.completeExceptionally(new UnknownHostException());
+                            }
+                        };
+                resolver.query(null /* network, null for default */, endPoint,
+                        DnsResolver.FLAG_EMPTY, r -> r.run(), cancellationSignal, cb);
+                return result.get();
+            } catch (final ExecutionException e) {
+                Log.e(TAG, "Exception resolving endpoint : " + endPoint + ". "
+                        + "Throw route won't be installed.", e);
+            } catch (final InterruptedException e) {
+                Log.e(TAG, "Legacy VPN was interrupted while resolving the endpoint", e);
+                cancellationSignal.cancel();
+            }
+            return null;
         }
     }
 
@@ -2582,11 +2621,12 @@ public class Vpn {
     }
 
     @Nullable private static String[] getMtpdArgs(final VpnConfig config,
-            final VpnProfile profile) {
+            final VpnProfile profile, @NonNull final InetAddress serverAddress) {
+        final String server = serverAddress.getHostAddress();
         switch (profile.type) {
             case VpnProfile.TYPE_PPTP:
                 return new String[] {
-                        config.interfaze, "pptp", profile.server, "1723",
+                        config.interfaze, "pptp", server, "1723",
                         "name", profile.username, "password", profile.password,
                         "linkname", "vpn", "refuse-eap", "nodefaultroute",
                         "usepeerdns", "idle", "1800", "mtu", "1400", "mru", "1400",
@@ -2595,7 +2635,7 @@ public class Vpn {
             case VpnProfile.TYPE_L2TP_IPSEC_PSK:
             case VpnProfile.TYPE_L2TP_IPSEC_RSA:
                 return new String[] {
-                        config.interfaze, "l2tp", profile.server, "1701", profile.l2tpSecret,
+                        config.interfaze, "l2tp", server, "1701", profile.l2tpSecret,
                         "name", profile.username, "password", profile.password,
                         "linkname", "vpn", "refuse-eap", "nodefaultroute",
                         "usepeerdns", "idle", "1800", "mtu", "1400", "mru", "1400",
@@ -2605,32 +2645,34 @@ public class Vpn {
     }
 
     @Nullable private static String[] getRacoonArgs(final VpnConfig config,
-            final VpnProfile profile, final CertificateSet certificates, final String gateway) {
+            final VpnProfile profile, final CertificateSet certificates, final String gateway,
+            @NonNull final InetAddress serverAddress) {
+        final String server = serverAddress.getHostAddress();
         switch (profile.type) {
             case VpnProfile.TYPE_L2TP_IPSEC_PSK:
                 return new String[] {
-                        config.interfaze, profile.server, "udppsk", profile.ipsecIdentifier,
+                        config.interfaze, server, "udppsk", profile.ipsecIdentifier,
                         profile.ipsecSecret, "1701",
                 };
             case VpnProfile.TYPE_L2TP_IPSEC_RSA:
                 return new String[] {
-                        config.interfaze, profile.server, "udprsa", certificates.privateKey,
+                        config.interfaze, server, "udprsa", certificates.privateKey,
                         certificates.userCert, certificates.caCert, certificates.serverCert, "1701",
                 };
             case VpnProfile.TYPE_IPSEC_XAUTH_PSK:
                 return new String[] {
-                        config.interfaze, profile.server, "xauthpsk", profile.ipsecIdentifier,
+                        config.interfaze, server, "xauthpsk", profile.ipsecIdentifier,
                         profile.ipsecSecret, profile.username, profile.password, "", gateway,
                 };
             case VpnProfile.TYPE_IPSEC_XAUTH_RSA:
                 return new String[] {
-                        config.interfaze, profile.server, "xauthrsa", certificates.privateKey,
+                        config.interfaze, server, "xauthrsa", certificates.privateKey,
                         certificates.userCert, certificates.caCert, certificates.serverCert,
                         profile.username, profile.password, "", gateway,
                 };
             case VpnProfile.TYPE_IPSEC_HYBRID_RSA:
                 return new String[] {
-                        config.interfaze, profile.server, "hybridrsa", certificates.caCert,
+                        config.interfaze, server, "hybridrsa", certificates.caCert,
                         certificates.serverCert, profile.username, profile.password, "", gateway,
                 };
             // Other cases are IKEv2 platform VPN so they don't generate arguments for racoon.
@@ -2784,9 +2826,17 @@ public class Vpn {
         private void bringup() {
             // Catch all exceptions so we can clean up a few things.
             try {
+                final InetAddress endpointAddress = mDeps.resolve(mProfile.server);
+                if (null == endpointAddress) {
+                    // No end point (address can't be resolved, or the thread was interrupted)
+                    // Nothing can be done in any case, so bail.
+                    agentDisconnect();
+                    return;
+                }
+
                 mDaemons[0] = new Daemon("racoon",
-                        getRacoonArgs(mConfig, mProfile, mCertificates, mGateway));
-                mDaemons[1] = new Daemon("mtpd", getMtpdArgs(mConfig, mProfile));
+                        getRacoonArgs(mConfig, mProfile, mCertificates, mGateway, endpointAddress));
+                mDaemons[1] = new Daemon("mtpd", getMtpdArgs(mConfig, mProfile, endpointAddress));
 
                 // Initialize the timer.
                 mBringupStartTime = SystemClock.elapsedRealtime();
@@ -2883,19 +2933,16 @@ public class Vpn {
                 }
 
                 // Add a throw route for the VPN server endpoint, if one was specified.
-                String endpoint = parameters[5].isEmpty() ? mProfile.server : parameters[5];
-                if (!endpoint.isEmpty()) {
-                    try {
-                        InetAddress addr = InetAddress.parseNumericAddress(endpoint);
-                        if (addr instanceof Inet4Address) {
-                            mConfig.routes.add(new RouteInfo(new IpPrefix(addr, 32), RTN_THROW));
-                        } else if (addr instanceof Inet6Address) {
-                            mConfig.routes.add(new RouteInfo(new IpPrefix(addr, 128), RTN_THROW));
-                        } else {
-                            Log.e(TAG, "Unknown IP address family for VPN endpoint: " + endpoint);
-                        }
-                    } catch (IllegalArgumentException e) {
-                        Log.e(TAG, "Exception constructing throw route to " + endpoint + ": " + e);
+                if (endpointAddress != null) {
+                    if (endpointAddress instanceof Inet4Address) {
+                        mConfig.routes.add(new RouteInfo(
+                                new IpPrefix(endpointAddress, 32), RTN_THROW));
+                    } else if (endpointAddress instanceof Inet6Address) {
+                        mConfig.routes.add(new RouteInfo(
+                                new IpPrefix(endpointAddress, 128), RTN_THROW));
+                    } else {
+                        Log.e(TAG, "Unknown IP address family for VPN endpoint: "
+                                + endpointAddress);
                     }
                 }
 
