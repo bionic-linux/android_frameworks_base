8e719f431f96e68744e085ec0285431decc1fbd2
Lorenzo Colitti <lorenzo@google.com>
2020-11-16 10:11:12 +0900

    Test a VPN with an underlying network that does not yet exist.
    This test checks that if a VPN declares an underlying network
    that does not exist, the capabilities of that network are applied
    to the VPN as soon as the network starts to exist.
    
    Bug: 173331190
    Test: test-only change
    Change-Id: Icc0701cb4cea7d91f7738c1e426e94cd26686b74

--- tests/net/integration/util/com/android/server/TestNetIdManager.kt
+++ tests/net/integration/util/com/android/server/TestNetIdManager.kt
@@ -35,4 +35,5 @@ class TestNetIdManager : NetIdManager() {
     private val nextId = AtomicInteger(MAX_NET_ID)
     override fun reserveNetId() = nextId.decrementAndGet()
     override fun releaseNetId(id: Int) = Unit
+    fun predictNextNetId() = nextId.get() - 1
 }
