709c627c69dd001187ef21239755ba0c9569fafd
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2020-11-17 09:11:17 +0000

    gtbs: Add Generic Telephony Bearer Service support
    Tag: #feature
    Bug: 159786353
    Sponsor: jpawlowski@
    Change-Id: I8264ade4b07df17fc0207437dfdcae7028cc20ff

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -3122,6 +3122,9 @@ public final class BluetoothAdapter {
             BluetoothCsipSetCoordinator csipSetCoordinator =
                     new BluetoothCsipSetCoordinator(context, listener, this);
             return true;
+        } else if (profile == BluetoothProfile.TBS) {
+            BluetoothTbs tbs = new BluetoothTbs(context, listener);
+            return true;
         } else {
             return false;
         }
@@ -3223,6 +3226,10 @@ public final class BluetoothAdapter {
                 BluetoothCsipSetCoordinator csipSetCoordinator =
                         (BluetoothCsipSetCoordinator) proxy;
                 csipSetCoordinator.close();
+                break;
+            case BluetoothProfile.TBS:
+                BluetoothTbs tbs = (BluetoothTbs) proxy;
+                tbs.close();
                 break;
         }
     }
--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -45,6 +45,7 @@ import android.bluetooth.IBluetoothManager;
 import android.bluetooth.IBluetoothManagerCallback;
 import android.bluetooth.IBluetoothProfileServiceConnection;
 import android.bluetooth.IBluetoothStateChangeCallback;
+import android.bluetooth.IBluetoothTbs;
 import android.content.ActivityNotFoundException;
 import android.content.AttributionSource;
 import android.content.BroadcastReceiver;
@@ -1326,11 +1327,15 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                             + bluetoothProfile);
                 }
 
-                if (bluetoothProfile != BluetoothProfile.HEADSET) {
+                Intent intent;
+                if (bluetoothProfile == BluetoothProfile.HEADSET) {
+                    intent = new Intent(IBluetoothHeadset.class.getName());
+                } else if (bluetoothProfile== BluetoothProfile.TBS) {
+                    intent = new Intent(IBluetoothTbs.class.getName());
+                } else {
                     return false;
                 }
 
-                Intent intent = new Intent(IBluetoothHeadset.class.getName());
                 psc = new ProfileServiceConnections(intent);
                 if (!psc.bindService()) {
                     return false;
