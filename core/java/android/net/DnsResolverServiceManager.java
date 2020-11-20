e150dfbaab3c5dd6f262b87a965fc07213dac3a4
paulhu <paulhu@google.com>
2020-11-20 16:44:13 +0800

    Add DnsResolverServiceManager
    Add DnsResolverServiceManager to obtain android.net.IDnsResolver
    IBinder for incoming connectivity service mainline.
    
    Bug: 170598012
    Test: atest FrameworksNetTests
    Test: atest CtsNetTestCasesLatestSdk --instant on both Q and S devices.
    Change-Id: Ibd7a678ceaaa550ff3d8eff1d0c4d3a5073b5527

---
+++ core/java/android/net/DnsResolverServiceManager.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.net;
+
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.annotation.RequiresPermission;
+import android.annotation.SystemApi;
+import android.content.Context;
+import android.os.IBinder;
+import android.os.ServiceManager;
+
+import java.util.Objects;
+
+/**
+ * Provides a way to obtain the DnsResolver binder objects.
+ *
+ * @hide
+ */
+@SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
+public class DnsResolverServiceManager {
+    /**
+     * Name to retrieve a {@link android.net.IDnsResolver} IBinder.
+     */
+    private static final String DNS_RESOLVER_SERVICE = "dnsresolver";
+
+    private DnsResolverServiceManager() {}
+
+    /**
+     * Get an {@link IBinder} representing the DnsResolver stable AIDL interface or null if the
+     * service is not registered.
+     */
+    @Nullable
+    @RequiresPermission(NetworkStack.PERMISSION_MAINLINE_NETWORK_STACK)
+    public static IBinder getService(@NonNull final Context context) {
+        Objects.requireNonNull(context);
+        context.enforceCallingOrSelfPermission(NetworkStack.PERMISSION_MAINLINE_NETWORK_STACK,
+                "DnsResolverServiceManager");
+
+        return ServiceManager.getService(DNS_RESOLVER_SERVICE);
+    }
+}
+
