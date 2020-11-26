2a5a79a8613e1c3ef4746bd880e5b191ba84c4e5
Chalard Jean <jchalard@google.com>
2020-11-26 23:21:13 +0900

    Add a provider to VPN
    Test: FrameworksNetTests NetworkStackTests
    Change-Id: I982543cdee358bb62d3b56a7fd9d71dc18908b65

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -5135,7 +5135,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
                 loge("Stopped user has no VPN");
                 return;
             }
-            userVpn.onUserStopped();
+            userVpn.onUserStop();
             mVpns.delete(userId);
         }
     }
