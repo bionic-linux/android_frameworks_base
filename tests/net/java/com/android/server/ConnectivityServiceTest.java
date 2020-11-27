590546a2db3d9546bce80aa34a314303209e7639
Lorenzo Colitti <lorenzo@google.com>
2020-11-27 15:26:31 +0900

    Add a test that checks that a network's owner UID cannot change.
    Test: test-only change
    Change-Id: I345bd320eced96316d92e520f576ae06b8020d9f

--- tests/net/java/com/android/server/ConnectivityServiceTest.java
+++ tests/net/java/com/android/server/ConnectivityServiceTest.java
@@ -1958,6 +1958,35 @@ public class ConnectivityServiceTest {
     }
 
     @Test
+    public void testOwnerUidCannotChange() throws Exception {
+        // Owner UIDs are not visible without location permission.
+        setupLocationPermissions(Build.VERSION_CODES.Q, true, AppOpsManager.OPSTR_FINE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        final NetworkCapabilities ncTemplate = new NetworkCapabilities();
+        final int originalOwnerUid = Process.myUid();
+        ncTemplate.setOwnerUid(originalOwnerUid);
+
+        mWiFiNetworkAgent = new TestNetworkAgentWrapper(TRANSPORT_WIFI, new LinkProperties(),
+                ncTemplate);
+        mWiFiNetworkAgent.connect(false);
+        waitForIdle();
+
+        NetworkCapabilities nc = mCm.getNetworkCapabilities(mWiFiNetworkAgent.getNetwork());
+        assertFalse(nc.hasCapability(NET_CAPABILITY_NOT_CONGESTED));
+        assertEquals(originalOwnerUid, nc.getOwnerUid());
+
+        nc.setOwnerUid(42);
+        nc.addCapability(NET_CAPABILITY_NOT_CONGESTED);
+        mWiFiNetworkAgent.setNetworkCapabilities(nc, true);
+        waitForIdle();
+
+        nc = mCm.getNetworkCapabilities(mWiFiNetworkAgent.getNetwork());
+        assertTrue(nc.hasCapability(NET_CAPABILITY_NOT_CONGESTED));
+        assertEquals(originalOwnerUid, nc.getOwnerUid());
+    }
+
+    @Test
     public void testMultipleLingering() throws Exception {
         // This test would be flaky with the default 120ms timer: that is short enough that
         // lingered networks are torn down before assertions can be run. We don't want to mock the
