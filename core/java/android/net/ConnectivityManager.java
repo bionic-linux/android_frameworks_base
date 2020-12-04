63c19a05db4cbd4cf59a835ac073f5ed3f35b8d2
Chiachang Wang <chiachangwang@google.com>
2020-12-04 19:27:46 +0800

    [IT03] Remove network activity logic out from NMS
    In order to support CS mainline, this commit removes the idle
    timer dependency between NMS and CS. NMS gathers both the
    interface activity change and idle timer configuration change
    to report network active status. Thus, network active status
    also has to be moved to CS.
    
    This change will also impact the API in ConnectivityManager since
    the network active status and the actvitiy listener are sent to
    NMS, but those logic is removed from NMS. These APIs will need to
    be updated together.
    
    Bug: 170598012
    Test: atest FrameworksNetTests
    Change-Id: Ief9e892586aaee8e9542b9313088fa50d1d411e5

--- core/java/android/net/ConnectivityManager.java
+++ core/java/android/net/ConnectivityManager.java
@@ -41,7 +41,6 @@ import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
 import android.os.INetworkActivityListener;
 import android.os.INetworkManagementService;
 import android.os.Looper;
@@ -2155,17 +2154,6 @@ public class ConnectivityManager {
         void onNetworkActive();
     }
 
-    private INetworkManagementService getNetworkManagementService() {
-        synchronized (this) {
-            if (mNMService != null) {
-                return mNMService;
-            }
-            IBinder b = ServiceManager.getService(Context.NETWORKMANAGEMENT_SERVICE);
-            mNMService = INetworkManagementService.Stub.asInterface(b);
-            return mNMService;
-        }
-    }
-
     private final ArrayMap<OnNetworkActiveListener, INetworkActivityListener>
             mNetworkActivityListeners = new ArrayMap<>();
 
@@ -2190,7 +2178,7 @@ public class ConnectivityManager {
         };
 
         try {
-            getNetworkManagementService().registerNetworkActivityListener(rl);
+            mService.registerNetworkActivityListener(rl);
             mNetworkActivityListeners.put(l, rl);
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
@@ -2207,7 +2195,7 @@ public class ConnectivityManager {
         INetworkActivityListener rl = mNetworkActivityListeners.get(l);
         Preconditions.checkArgument(rl != null, "Listener was not registered.");
         try {
-            getNetworkManagementService().unregisterNetworkActivityListener(rl);
+            mService.registerNetworkActivityListener(rl);
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
         }
@@ -2223,7 +2211,7 @@ public class ConnectivityManager {
      */
     public boolean isDefaultNetworkActive() {
         try {
-            return getNetworkManagementService().isNetworkActive();
+            return mService.isDefaultNetworkActive();
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
         }
