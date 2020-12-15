123eee2163e86b36ae4059c9931513591790ede2
Tianjie <xunchang@google.com>
2020-12-15 15:13:42 -0800

    Load and generate key from keystore in reboot escrow
    According to the design, we need to wrap both reboot escrow key
    and reboot escrow data with an additional key from android
    keystore. The new key in keystore is created upon a new resume on
    reboot request, and cleared after decryption is attempted at boot
    time.
    
    Bug: 172780686
    Test: atest FrameworksServicesTests:RebootEscrowDataTest \
                FrameworksServicesTests:LockSettingsServiceTests \
                FrameworksServicesTests:RecoverySystemServiceTest \
                FrameworksServicesTests:RebootEscrowManagerTests
    
    Change-Id: Ie2b5f559fb5d3217337e31b0dff507b98715384d

--- services/core/java/com/android/server/locksettings/RebootEscrowData.java
+++ services/core/java/com/android/server/locksettings/RebootEscrowData.java
@@ -16,8 +16,6 @@
 
 package com.android.server.locksettings;
 
-import com.android.internal.util.Preconditions;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
@@ -26,6 +24,7 @@ import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -81,10 +80,10 @@ class RebootEscrowData {
         return mKey;
     }
 
-    static RebootEscrowData fromEncryptedData(RebootEscrowKey key, byte[] blob)
+    static RebootEscrowData fromEncryptedData(RebootEscrowKey ks, byte[] blob)
             throws IOException {
-        Preconditions.checkNotNull(key);
-        Preconditions.checkNotNull(blob);
+        Objects.requireNonNull(ks);
+        Objects.requireNonNull(blob);
 
         DataInputStream dis = new DataInputStream(new ByteArrayInputStream(blob));
         int version = dis.readInt();
@@ -112,7 +111,7 @@ class RebootEscrowData {
         final byte[] syntheticPassword;
         try {
             Cipher c = Cipher.getInstance(CIPHER_ALGO);
-            c.init(Cipher.DECRYPT_MODE, key.getKey(), new IvParameterSpec(iv));
+            c.init(Cipher.DECRYPT_MODE, ks.getKey(), new IvParameterSpec(iv));
             syntheticPassword = c.doFinal(cipherText);
         } catch (NoSuchAlgorithmException | InvalidKeyException | BadPaddingException
                 | IllegalBlockSizeException | NoSuchPaddingException
@@ -120,13 +119,13 @@ class RebootEscrowData {
             throw new IOException("Could not decrypt ciphertext", e);
         }
 
-        return new RebootEscrowData(spVersion, iv, syntheticPassword, blob, key);
+        return new RebootEscrowData(spVersion, iv, syntheticPassword, blob, ks);
     }
 
-    static RebootEscrowData fromSyntheticPassword(RebootEscrowKey key, byte spVersion,
+    static RebootEscrowData fromSyntheticPassword(RebootEscrowKey ks, byte spVersion,
             byte[] syntheticPassword)
             throws IOException {
-        Preconditions.checkNotNull(syntheticPassword);
+        Objects.requireNonNull(syntheticPassword);
 
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(bos);
@@ -135,7 +134,7 @@ class RebootEscrowData {
         final byte[] iv;
         try {
             Cipher cipher = Cipher.getInstance(CIPHER_ALGO);
-            cipher.init(Cipher.ENCRYPT_MODE, key.getKey());
+            cipher.init(Cipher.ENCRYPT_MODE, ks.getKey());
             cipherText = cipher.doFinal(syntheticPassword);
             iv = cipher.getIV();
         } catch (NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException
@@ -151,6 +150,6 @@ class RebootEscrowData {
         dos.write(cipherText);
 
         return new RebootEscrowData(spVersion, iv, syntheticPassword, bos.toByteArray(),
-                key);
+                ks);
     }
 }
