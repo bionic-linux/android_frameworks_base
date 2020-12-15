eff85fa915a893678ee10c6d4c5a9701bed11ccb
Chiachang Wang <chiachangwang@google.com>
2020-12-15 16:00:01 +0800

    [IT04] Add uid as parameter of interfaceClassDataActivityChanged
    Add uid into interfaceClassDataActivityChanged in
    INetworkManagementEventObserver. This helps the listeners to use
    BaseNetworkObserver to listen for target evnets instead of using
    whole INetdUnsolicitedEventListener with no-op in other event
    that listeners do not care about.
    
    Bug: 170598012
    Test: m ; atest FrameworksNetTests
    Change-Id: I2a42a522c2ff9b1e0be88261a8574bb7f5292fa6

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -1795,7 +1795,8 @@ public class ConnectivityService extends IConnectivityManager.Stub
 
     private INetworkManagementEventObserver mDataActivityObserver = new BaseNetworkObserver() {
         @Override
-        public void interfaceClassDataActivityChanged(String label, boolean active, long tsNanos) {
+        public void interfaceClassDataActivityChanged(String label, boolean active, long tsNanos,
+                int uid) {
             int deviceType = Integer.parseInt(label);
             sendDataActivityBroadcast(deviceType, active, tsNanos);
         }
