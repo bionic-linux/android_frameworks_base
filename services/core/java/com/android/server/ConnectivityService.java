52485f4e7e1b97838e366c5b3ce65831bb7614a4
Chiachang Wang <chiachangwang@google.com>
2020-12-23 15:02:54 +0800

    [IT4.6] Unbundle NMS out from ConnectivityManager
    ConnectivitySerivce is no longer to update idle timer to NMS but
    send to INetd directly after this change. Replace the API
    implementation in ConnectivityManager to refer into
    ConnectivityService instead of NetworkManagementService to remove
    the dependency between CM and NMS for ConnectivityService mainline.
    
    Bug: 170598012
    Test: atest FrameworksNetTests
    Change-Id: If0ac9a6427dba5a732a15b5d7ca1351b71b07b7b

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -157,6 +157,7 @@ import android.os.Parcelable;
 import android.os.PersistableBundle;
 import android.os.PowerManager;
 import android.os.Process;
+import android.os.RemoteCallbackList;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.ServiceSpecificException;
@@ -218,6 +219,7 @@ import com.android.server.net.NetworkPolicyManagerInternal;
 import com.android.server.utils.PriorityDump;
 
 import com.google.android.collect.Lists;
+import com.google.android.collect.Maps;
 
 import libcore.io.IoUtils;
 
@@ -2325,7 +2327,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
      */
     @Override
     public void registerNetworkActivityListener(@NonNull INetworkActivityListener l) {
-        // TODO: Replace network activity listener registry in ConnectivityManager from NMS to here
+        mNetworkActivityTracker.registerNetworkActivityListener(l);
     }
 
     /**
@@ -2333,7 +2335,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
      */
     @Override
     public void unregisterNetworkActivityListener(@NonNull INetworkActivityListener l) {
-        // TODO: Replace network activity listener registry in ConnectivityManager from NMS to here
+        mNetworkActivityTracker.unregisterNetworkActivityListener(l);
     }
 
     /**
@@ -2341,8 +2343,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
      */
     @Override
     public boolean isDefaultNetworkActive() {
-        // TODO: Replace isNetworkActive() in NMS.
-        return false;
+        return mNetworkActivityTracker.isDefaultNetworkActive();
     }
 
     /**
@@ -8450,6 +8451,22 @@ public class ConnectivityService extends IConnectivityManager.Stub
     private class LegacyNetworkActivityTracker {
         private static final int NO_UID = -1;
         private Context mContext;
+        private final RemoteCallbackList<INetworkActivityListener> mNetworkActivityListeners =
+                new RemoteCallbackList<>();
+        @GuardedBy("mActiveIdleTimers")
+        private boolean mNetworkActive;
+        @GuardedBy("mActiveIdleTimers")
+        private HashMap<String, IdleTimerParams> mActiveIdleTimers = Maps.newHashMap();
+
+        private class IdleTimerParams {
+            public final int timeout;
+            public final int transportType;
+
+            IdleTimerParams(int timeout, int transport) {
+                this.timeout = timeout;
+                this.transportType = transport;
+            }
+        }
 
         LegacyNetworkActivityTracker(@NonNull Context context) {
             mContext = context;
@@ -8461,8 +8478,30 @@ public class ConnectivityService extends IConnectivityManager.Stub
             public void interfaceClassDataActivityChanged(int networkType, boolean active,
                     long tsNanos, int uid) {
                 sendDataActivityBroadcast(networkType, active, tsNanos);
+                synchronized (mActiveIdleTimers) {
+                    mNetworkActive = active;
+                    // If there are no idle timers, it means that system is not monitoring activity,
+                    // so the default network is always considered active.
+                    if (mNetworkActive || mActiveIdleTimers.isEmpty()) reportNetworkActive();
+                }
             }
         };
+
+        private void reportNetworkActive() {
+            final int length = mNetworkActivityListeners.beginBroadcast();
+            if (DDBG) log("reportNetworkActive, notify " + length + " listeners");
+            try {
+                for (int i = 0; i < length; i++) {
+                    try {
+                        mNetworkActivityListeners.getBroadcastItem(i).onNetworkActive();
+                    } catch (RemoteException | RuntimeException e) {
+                        loge("Fail to send network activie to listener " + e);
+                    }
+                }
+            } finally {
+                mNetworkActivityListeners.finishBroadcast();
+            }
+        }
 
         public INetworkManagementEventObserver getDataActivityObserver() {
             return mDataActivityObserver;
@@ -8519,8 +8558,13 @@ public class ConnectivityService extends IConnectivityManager.Stub
 
             if (timeout > 0 && iface != null) {
                 try {
-                    // TODO: Access INetd directly instead of NMS
-                    mNMS.addIdleTimer(iface, timeout, type);
+                    synchronized (mActiveIdleTimers) {
+                        // Networks start up.
+                        mNetworkActive = true;
+                        mActiveIdleTimers.put(iface, new IdleTimerParams(timeout, type));
+                    }
+                    mNetd.idletimerAddInterface(iface, timeout, Integer.toString(type));
+                    reportNetworkActive();
                 } catch (Exception e) {
                     // You shall not crash!
                     loge("Exception in setupDataActivityTracking " + e);
@@ -8548,9 +8592,13 @@ public class ConnectivityService extends IConnectivityManager.Stub
 
             try {
                 updateRadioPowerState(false /* isActive */, type);
-                // The call fails silently if no idle timer setup for this interface.
-                // TODO: Access INetd directly instead of NMS
-                mNMS.removeIdleTimer(iface);
+                synchronized (mActiveIdleTimers) {
+                    final IdleTimerParams params = mActiveIdleTimers.get(iface);
+                    mActiveIdleTimers.remove(iface);
+                    // The call fails silently if no idle timer setup for this interface
+                    mNetd.idletimerRemoveInterface(iface, params.timeout,
+                            Integer.toString(params.transportType));
+                }
             } catch (Exception e) {
                 // You shall not crash!
                 loge("Exception in removeDataActivityTracking " + e);
@@ -8582,6 +8630,22 @@ public class ConnectivityService extends IConnectivityManager.Stub
                 default:
                     logw("Untracked transport type:" + transportType);
             }
+        }
+
+        public boolean isDefaultNetworkActive() {
+            synchronized (mActiveIdleTimers) {
+                // If there are no idle timers, it means that system is not monitoring activity,
+                // so the default network is always considered active.
+                return mNetworkActive || mActiveIdleTimers.isEmpty();
+            }
+        }
+
+        public void registerNetworkActivityListener(@NonNull INetworkActivityListener l) {
+            mNetworkActivityListeners.register(l);
+        }
+
+        public void unregisterNetworkActivityListener(@NonNull INetworkActivityListener l) {
+            mNetworkActivityListeners.unregister(l);
         }
     }
 }
