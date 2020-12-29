963bcf9ae1757c95e1e8cdbb0e08ec19b72c11f5
Rakesh Kumar <rakesh.kumar@ittiam.com>
2020-12-29 22:00:19 +0530

    StressTest: MultiInstance tests
    Test: atest com.android.mediastresstest.MultiInstanceTest \
    	-- --enable-module-dynamic-download=true
    
    Bug: 176704791
    
    Change-Id: I2931a252348693e15a352f30e8df46a89ac27614

--- media/tests/StressTest/src/com/android/stresstests/CodecTestBase.java
+++ media/tests/StressTest/src/com/android/stresstests/CodecTestBase.java
@@ -1077,8 +1077,7 @@ class CodecDecoderTestBase extends CodecTestBase {
         if (isAudio) {
             flatBuffer.putInt(info.size);
         }
-        flatBuffer.putInt(info.flags & ~MediaCodec.BUFFER_FLAG_END_OF_STREAM)
-                .putLong(info.presentationTimeUs);
+        flatBuffer.putLong(info.presentationTimeUs);
         flatBuffer.flip();
     }
 
