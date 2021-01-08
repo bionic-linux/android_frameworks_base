c3ec0b36e786c9c100a8772ddffce96650b17239
junyulai <junyulai@google.com>
2021-01-08 18:19:24 +0800

    [FUI13] Get subscriber Id from NetworkCapabilities
    This patch stop referencing subscriberId in NetworkState.
    
    Test: atest NetworkStatsServiceTest NetworkAgentConfigTest
    Bug: 174123988
    Change-Id: I9c46a84fb1b3bddd076585e39b8718e03c202bd1

--- services/tests/servicestests/src/com/android/server/net/NetworkPolicyManagerServiceTest.java
+++ services/tests/servicestests/src/com/android/server/net/NetworkPolicyManagerServiceTest.java
@@ -1754,7 +1754,7 @@ public class NetworkPolicyManagerServiceTest {
             expectNetworkState(true /* roaming */);
             callback.onCapabilitiesChanged(
                     new Network(TEST_NET_ID),
-                    buildNetworkCapabilities(TEST_SUB_ID, true /* roaming */));
+                    buildNetworkCapabilities(TEST_SUB_ID, TEST_IMSI,  /* roaming */true));
 
             assertEquals(0, internal.getSubscriptionOpportunisticQuota(
                     new Network(TEST_NET_ID), NetworkPolicyManagerInternal.QUOTA_TYPE_MULTIPATH));
@@ -1992,7 +1992,8 @@ public class NetworkPolicyManagerServiceTest {
         return lp;
     }
 
-    private NetworkCapabilities buildNetworkCapabilities(int subId, boolean roaming) {
+    private NetworkCapabilities buildNetworkCapabilities(int subId, String subscriberId,
+            boolean roaming) {
         final NetworkCapabilities nc = new NetworkCapabilities();
         nc.addTransportType(TRANSPORT_CELLULAR);
         if (!roaming) {
@@ -2000,6 +2001,7 @@ public class NetworkPolicyManagerServiceTest {
         }
         nc.setNetworkSpecifier(new TelephonyNetworkSpecifier.Builder()
                 .setSubscriptionId(subId).build());
+        nc.setSubscriberId(subscriberId);
         return nc;
     }
 
@@ -2067,7 +2069,7 @@ public class NetworkPolicyManagerServiceTest {
         when(mConnManager.getAllNetworkState()).thenReturn(new NetworkState[] {
                 new NetworkState(TYPE_MOBILE,
                         buildLinkProperties(TEST_IFACE),
-                        buildNetworkCapabilities(TEST_SUB_ID, roaming),
+                        buildNetworkCapabilities(TEST_SUB_ID, TEST_IMSI, roaming),
                         new Network(TEST_NET_ID), TEST_IMSI, null)
         });
     }
