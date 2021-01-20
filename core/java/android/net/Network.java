02aa8cacf02950485d570e45c9f56f66b53f7646
Chalard Jean <jchalard@google.com>
2021-01-20 20:47:32 +0900

    [NS03] Mix in other CS-managed properties
    These properties are necessary to figure out the maximum
    score of a network.
    
    Test: FrameworksNetTests
    Change-Id: I48dce20ad4a80597039393dca607e8da829b2a61

--- core/java/android/net/Network.java
+++ core/java/android/net/Network.java
@@ -61,7 +61,6 @@ import javax.net.SocketFactory;
  * {@link ConnectivityManager#bindProcessToNetwork}.
  */
 public class Network implements Parcelable {
-
     /**
      * The unique id of the network.
      * @hide
