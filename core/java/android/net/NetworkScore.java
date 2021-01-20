e018a70aab0b5f70c228cfdec8b0efb080b9df2b
Chalard Jean <jchalard@google.com>
2021-01-20 20:47:32 +0900

    [NS03] Mix in other CS-managed properties
    These properties are necessary to figure out the maximum
    score of a network.
    
    Test: FrameworksNetTests
    Change-Id: I48dce20ad4a80597039393dca607e8da829b2a61

--- core/java/android/net/NetworkScore.java
+++ core/java/android/net/NetworkScore.java
@@ -40,6 +40,20 @@ public final class NetworkScore implements Parcelable {
     /** @hide */
     public static final int POLICY_IS_VALIDATED = 63;
 
+    // This is a VPN and behaves as one for scoring purposes.
+    /** @hide */
+    public static final int POLICY_IS_VPN = 62;
+
+    // This network has been selected by the user manually from settings or a 3rd party app
+    // at least once. {@see NetworkAgentConfig#explicitlySelected}.
+    /** @hide */
+    public static final int POLICY_ONCE_CHOSEN_BY_USER = 61;
+
+    // The user has indicated in UI that this network should be used even if it doesn't
+    // validate. {@see NetworkAgentConfig#acceptUnvalidated}.
+    /** @hide */
+    public static final int POLICY_ACCEPT_UNVALIDATED = 60;
+
     private final long mPolicy;
 
     /** @hide */
@@ -55,17 +69,61 @@ public final class NetworkScore implements Parcelable {
 
     // Helper methods for connectivity service to mix in bits
     /** @hide */
-    public NetworkScore withPolicyBits(final boolean isValidated) {
-        return new NetworkScore(legacyInt, isValidated ? 1L << POLICY_IS_VALIDATED : 0L);
+    public NetworkScore withPolicyBits(
+            final boolean isValidated,
+            final boolean isVpn,
+            final boolean onceChosenByUser,
+            final boolean acceptUnvalidated) {
+        return new NetworkScore(legacyInt,
+                (isValidated ? 1L << POLICY_IS_VALIDATED : 0)
+                | (isVpn ? 1L << POLICY_IS_VPN : 0)
+                | (onceChosenByUser ? 1L << POLICY_ONCE_CHOSEN_BY_USER : 0)
+                | (acceptUnvalidated ? 1L << POLICY_ACCEPT_UNVALIDATED : 0));
     }
 
     public int getLegacyInt() {
-        return legacyInt;
+        return getLegacyInt(false);
+    }
+
+    /** @hide */
+    public int getLegacyIntAsValidated() {
+        return getLegacyInt(true);
+    }
+
+    // TODO : remove these two constants
+    // Penalty applied to scores of Networks that have not been validated.
+    private static final int UNVALIDATED_SCORE_PENALTY = 40;
+
+    // Score for a network that can be used unvalidated
+    private static final int ACCEPT_UNVALIDATED_NETWORK_SCORE = 100;
+
+    private int getLegacyInt(boolean pretendValidated) {
+        // If the user has chosen this network at least once, give it the maximum score when
+        // checking to pretend it's validated, or if it doesn't need to validate because the
+        // user said to use it even if it doesn't validate.
+        // This ensures that networks that have been selected in UI are not torn down before the
+        // user gets a chance to prefer it when a higher-scoring network (e.g., Ethernet) is
+        // available.
+        if (hasPolicy(POLICY_ONCE_CHOSEN_BY_USER)
+                && (hasPolicy(POLICY_ACCEPT_UNVALIDATED) || pretendValidated)) {
+            return ACCEPT_UNVALIDATED_NETWORK_SCORE;
+        }
+
+        int score = legacyInt;
+        if (!hasPolicy(POLICY_IS_VALIDATED) && !pretendValidated && !hasPolicy(POLICY_IS_VPN)) {
+            score -= UNVALIDATED_SCORE_PENALTY;
+        }
+        if (score < 0) score = 0;
+        return score;
+    }
+
+    private boolean hasPolicy(final int policy) {
+        return 0 != (mPolicy & (1L << policy));
     }
 
     @Override
     public String toString() {
-        return "Score(" + legacyInt + ")";
+        return "Score(" + legacyInt + " ; " + mPolicy + ")";
     }
 
     @Override
