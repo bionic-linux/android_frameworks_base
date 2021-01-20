99057f2771a9851f1484a20eb0b9dae69a0787ac
Chalard Jean <jchalard@google.com>
2021-01-20 20:47:32 +0900

    [NS03] Mix in other CS-managed properties
    These properties are necessary to figure out the maximum
    score of a network.
    
    Test: FrameworksNetTests
    Change-Id: I48dce20ad4a80597039393dca607e8da829b2a61

--- tests/net/java/com/android/server/connectivity/LingerMonitorTest.java
+++ tests/net/java/com/android/server/connectivity/LingerMonitorTest.java
@@ -359,9 +359,9 @@ public class LingerMonitorTest {
         caps.addTransportType(transport);
         NetworkAgentInfo nai = new NetworkAgentInfo(null, new Network(netId), info,
                 new LinkProperties(), caps, new NetworkScore.Builder().setLegacyInt(50).build(),
-                mCtx, null, new NetworkAgentConfig() /* config */,
-                mConnService, mNetd, mDnsResolver, mNMS, NetworkProvider.ID_NONE,
-                Binder.getCallingUid(), mQosCallbackTracker);
+                mCtx, null, new NetworkAgentConfig.Builder().build(), mConnService, mNetd,
+                mDnsResolver, mNMS, NetworkProvider.ID_NONE, Binder.getCallingUid(),
+                mQosCallbackTracker);
         nai.everValidated = true;
         return nai;
     }
