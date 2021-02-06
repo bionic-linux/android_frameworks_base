e43da16a640f636b570a23ce5d2eabeb7d3564c5
Lorenzo Colitti <lorenzo@google.com>
2021-02-07 01:32:49 +0900

    DO NOT SUBMIT: Ignore super-slow test
    Test: test now runs faster
    Change-Id: Iabff34aec8099b8f93f7cd944f997475f3f6da29

--- tests/net/java/com/android/server/ConnectivityServiceTest.java
+++ tests/net/java/com/android/server/ConnectivityServiceTest.java
@@ -2858,6 +2858,7 @@ public class ConnectivityServiceTest {
     }
 
     @Test
+    @Ignore  // Takes more than 50 seconds to run on device
     public void testNetworkFactoryUnregister() throws Exception {
         final NetworkCapabilities filter = new NetworkCapabilities();
         filter.clearAll();
