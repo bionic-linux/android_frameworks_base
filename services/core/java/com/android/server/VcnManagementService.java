59310ee2be3a874516a59b276c67f4b5785ac578
Cody Kesting <ckesting@google.com>
2021-02-07 12:40:53 -0800

    Notify status callbacks when a VCN enters Safemode.
    This CL updates VcnManagementService to notify VcnStatusCallbacks when
    the VCN for their specified subscription group enters Safemode. In order
    to be notified, the registering app must also have permissions for the
    specified permission.
    
    Bug: 163433613
    Test: atest FrameworksVcnTests
    Change-Id: I3242ad0ee1dc406aef56253f884c2544a994869e

--- services/core/java/com/android/server/VcnManagementService.java
+++ services/core/java/com/android/server/VcnManagementService.java
@@ -800,6 +800,27 @@ public class VcnManagementService extends IVcnManagementService.Stub {
                     return;
                 }
 
+                // Notify all registered StatusCallbacks for this subGroup
+                for (VcnStatusCallbackInfo cbInfo : mRegisteredStatusCallbacks.values()) {
+                    if (!mSubGroup.equals(cbInfo.mSubGroup)) {
+                        continue;
+                    }
+                    if (!mLastSnapshot.packageHasPermissionsForSubscriptionGroup(
+                            mSubGroup, cbInfo.mPkgName)) {
+                        continue;
+                    }
+
+                    if (!mLocationPermissionChecker.checkLocationPermission(
+                            cbInfo.mPkgName,
+                            "VcnStatusCallback" /* featureId */,
+                            cbInfo.mUid,
+                            null /* message */)) {
+                        continue;
+                    }
+
+                    Binder.withCleanCallingIdentity(() -> cbInfo.mCallback.onEnteredSafemode());
+                }
+
                 notifyAllPolicyListenersLocked();
             }
         }
