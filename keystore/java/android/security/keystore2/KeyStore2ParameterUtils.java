ec0263af29735e93620b89533c8cba8ee6436ab9
Inseob Kim <inseob@google.com>
2021-02-09 04:04:06 +0000

    Revert "Keystore 2.0 SPI: Add CERTIFICATE_* tags."
    Revert "Keymint: Add support for CERTIFICATE_NOT_BEFORE/AFTER"
    
    Revert "Keymint: Add CERTIFICATE_* tags"
    
    Revert submission 1566353-keymint_cert_tags
    
    Reason for revert: caused test failure
    Reverted Changes:
    Iaa4d4b9d6:Keymint: Add support for CERTIFICATE_NOT_BEFORE/AF...
    I5b1ed20a9:Keymint: Add CERTIFICATE_* tags
    I27e9dcfa6:Keymint: Add CERTIFICATE_* tags required for certi...
    I7ce664b01:Keystore 2.0 SPI: Add CERTIFICATE_* tags.
    Ifbecd4517:Keystore 2.0: Add support for the new CERTIFICATE_...
    
    Bug: 179729062
    Change-Id: Ie19e770c39fadd4e24ca5e19b33aa25c8607cd17

--- keystore/java/android/security/keystore2/KeyStore2ParameterUtils.java
+++ keystore/java/android/security/keystore2/KeyStore2ParameterUtils.java
@@ -28,7 +28,6 @@ import android.security.keystore.KeyProperties;
 import android.security.keystore.UserAuthArgs;
 import android.system.keystore2.Authorization;
 
-import java.math.BigInteger;
 import java.security.ProviderException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -155,23 +154,6 @@ public abstract class KeyStore2ParameterUtils {
     }
 
     /**
-     * This function constructs a {@link KeyParameter} expressing a Bignum.
-     * @param tag Must be KeyMint tag with the associated type BIGNUM.
-     * @param b A BitInteger to be stored in the new key parameter.
-     * @return An instance of {@link KeyParameter}.
-     * @hide
-     */
-    static @NonNull KeyParameter makeBignum(int tag, @NonNull BigInteger b) {
-        if (KeymasterDefs.getTagType(tag) != KeymasterDefs.KM_BIGNUM) {
-            throw new IllegalArgumentException("Not a bignum tag: " + tag);
-        }
-        KeyParameter p = new KeyParameter();
-        p.tag = tag;
-        p.value = KeyParameterValue.blob(b.toByteArray());
-        return p;
-    }
-
-    /**
      * This function constructs a {@link KeyParameter} expressing date.
      * @param tag Must be KeyMint tag with the associated type DATE.
      * @param date A date
@@ -185,6 +167,10 @@ public abstract class KeyStore2ParameterUtils {
         KeyParameter p = new KeyParameter();
         p.tag = tag;
         p.value = KeyParameterValue.dateTime(date.getTime());
+        if (p.value.getDateTime() < 0) {
+            throw new IllegalArgumentException("Date tag value out of range: "
+                    + p.value.getDateTime());
+        }
         return p;
     }
     /**
