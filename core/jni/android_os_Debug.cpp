9542bb901ee9abab2f699bb293a8e18e373a51f4
Hridya Valsaraju <hridya@google.com>
2021-02-10 11:02:27 -0800

    dumpsys: Print total DMA-BUFs exported from the DMA-BUF heaps framework
    This patch adds the total size of DMA-BUFs exported from the DMA-BUF
    heaps framework to dumpsys meminfo.
    
    Bug: 167709539
    Test: adb shell dumpsys meminfo
    Change-Id: I153721a0dc91ee5084a446ba1ce4f8ec9c51c4f0
    Merged-In: I153721a0dc91ee5084a446ba1ce4f8ec9c51c4f0

--- core/jni/android_os_Debug.cpp
+++ core/jni/android_os_Debug.cpp
@@ -813,6 +813,16 @@ static jlong android_os_Debug_getDmabufTotalExportedKb(JNIEnv* env, jobject claz
     return dmabufTotalSizeKb;
 }
 
+static jlong android_os_Debug_getDmabufHeapTotalExportedKb(JNIEnv* env, jobject clazz) {
+    jlong dmabufHeapTotalSizeKb = -1;
+    uint64_t size;
+
+    if (meminfo::ReadDmabufHeapTotalExportedKb(&size)) {
+        dmabufHeapTotalSizeKb = size;
+    }
+    return dmabufHeapTotalSizeKb;
+}
+
 static jlong android_os_Debug_getIonPoolsSizeKb(JNIEnv* env, jobject clazz) {
     jlong poolsSizeKb = -1;
     uint64_t size;
@@ -943,6 +953,8 @@ static const JNINativeMethod gMethods[] = {
             (void*)android_os_Debug_getIonHeapsSizeKb },
     { "getDmabufTotalExportedKb", "()J",
             (void*)android_os_Debug_getDmabufTotalExportedKb },
+    { "getDmabufHeapTotalExportedKb", "()J",
+            (void*)android_os_Debug_getDmabufHeapTotalExportedKb },
     { "getIonPoolsSizeKb", "()J",
             (void*)android_os_Debug_getIonPoolsSizeKb },
     { "getDmabufMappedSizeKb", "()J",
