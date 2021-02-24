5fc0986c4f47fb8473c902b647b0034412b92066
Yan Yan <evitayan@google.com>
2021-02-24 10:31:18 -0800

    Support converting IkeAuthConfig to/from PersistableBundle
    Bug: 163604823
    Test: FrameworksVcnTests(add new tests)
    Change-Id: I97d9a7db423711dbccea412b96f069fe1dbd2779

--- core/java/android/net/vcn/persistablebundleutils/IkeSessionParamsUtils.java
+++ core/java/android/net/vcn/persistablebundleutils/IkeSessionParamsUtils.java
@@ -21,6 +21,8 @@ import static com.android.internal.annotations.VisibleForTesting.Visibility;
 import android.annotation.NonNull;
 import android.net.ipsec.ike.IkeSaProposal;
 import android.net.ipsec.ike.IkeSessionParams;
+import android.net.ipsec.ike.IkeSessionParams.IkeAuthConfig;
+import android.net.ipsec.ike.IkeSessionParams.IkeAuthPskConfig;
 import android.os.PersistableBundle;
 
 import com.android.internal.annotations.VisibleForTesting;
@@ -74,13 +76,22 @@ public final class IkeSessionParamsUtils {
                 REMOTE_ID_KEY,
                 IkeIdentificationUtils.toPersistableBundle(params.getRemoteIdentification()));
 
+        result.putPersistableBundle(
+                LOCAL_AUTH_KEY,
+                AuthConfigUtils.toPersistableBundle(
+                        params.getLocalAuthConfig(), true /* isLocalAuth */));
+        result.putPersistableBundle(
+                REMOTE_AUTH_KEY,
+                AuthConfigUtils.toPersistableBundle(
+                        params.getRemoteAuthConfig(), false /* isLocalAuth */));
+
         result.putIntArray(RETRANS_TIMEOUTS_KEY, params.getRetransmissionTimeoutsMillis());
         result.putInt(HARD_LIFETIME_SEC_KEY, params.getHardLifetimeSeconds());
         result.putInt(SOFT_LIFETIME_SEC_KEY, params.getSoftLifetimeSeconds());
         result.putInt(DPD_DELAY_SEC_KEY, params.getDpdDelaySeconds());
         result.putInt(NATT_KEEPALIVE_DELAY_SEC_KEY, params.getNattKeepAliveDelaySeconds());
 
-        // TODO: Handle authentication configuration, configuration requests and IKE options.
+        // TODO: Handle configuration requests and IKE options.
 
         return result;
     }
@@ -110,13 +121,87 @@ public final class IkeSessionParamsUtils {
                 IkeIdentificationUtils.fromPersistableBundle(
                         in.getPersistableBundle(REMOTE_ID_KEY)));
 
+        final IkeAuthConfig localAuthBundle = in.getPersistableBundle(LOCAL_AUTH_KEY);
+        final IkeAuthConfig remoteAuthBundle = in.getPersistableBundle(REMOTE_AUTH_KEY);
+        Objects.requireNonNull(localAuthBundle, "Local auth config was null");
+        Objects.requireNonNull(remoteAuthBundle, "Remote auth config was null");
+        AuthConfigUtils.setBuilderByReadingPersistableBundle(in, localAuthBundle);
+        AuthConfigUtils.setBuilderByReadingPersistableBundle(in, remoteAuthBundle);
+
         builder.setRetransmissionTimeoutsMillis(in.getIntArray(RETRANS_TIMEOUTS_KEY));
         builder.setLifetimeSeconds(
                 in.getInt(HARD_LIFETIME_SEC_KEY), in.getInt(SOFT_LIFETIME_SEC_KEY));
         builder.setDpdDelaySeconds(in.getInt(DPD_DELAY_SEC_KEY));
 
-        // TODO: Handle authentication configuration, configuration requests and IKE options.
+        // TODO: Handle configuration requests and IKE options.
 
         return builder.build();
+    }
+
+    private static class AuthConfigUtils {
+        private static final int IKE_AUTH_METHOD_PSK = 1;
+        private static final int IKE_AUTH_METHOD_PUB_KEY_SIGNATURE = 2;
+        private static final int IKE_AUTH_METHOD_EAP = 3;
+
+        private static final String AUTH_METHOD_KEY = "AUTH_METHOD_KEY";
+        private static final String AUTH_DIRECTION_KEY = "AUTH_DIRECTION_KEY";
+
+        @NonNull
+        public static PersistableBundle toPersistableBundle(
+                @NonNull IkeAuthConfig authConfig, boolean isLocalAuth) {
+            if (authConfig instanceof IkeAuthPskConfig) {
+                IkeAuthPskConfig config = (IkeAuthPskConfig) authConfig;
+                return IkeAuthPskConfigUtils.toPersistableBundle(
+                        config, createPersistableBundle(IKE_AUTH_METHOD_PSK, isLocalAuth));
+            } else {
+                throw new IllegalStateException("Invalid IkeAuthConfig subclass");
+            }
+
+            // TODO: Handle EAP auth and digital signature based auth.
+        }
+
+        private static PersistableBundle createPersistableBundle(int type, boolean isLocalAuth) {
+            final PersistableBundle result = new PersistableBundle();
+            result.putInt(AUTH_METHOD_KEY, type);
+            result.putBoolean(AUTH_DIRECTION_KEY, isLocalAuth);
+            return result;
+        }
+
+        @NonNull
+        public static void setBuilderByReadingPersistableBundle(
+                @NonNull PersistableBundle in, @NonNull IkeSessionParams.Builder builder) {
+            Objects.requireNonNull(in, "PersistableBundle is null");
+
+            final int methodType = in.getInt(AUTH_METHOD_KEY);
+            final boolean direction = in.getBoolean(AUTH_DIRECTION_KEY);
+            switch (methodType) {
+                case IKE_AUTH_METHOD_PSK:
+                    IkeAuthPskConfigUtils.setBuilderByReadingPersistableBundle(in, builder);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid EAP method type " + methodType);
+            }
+            // TODO: Handle EAP auth and digital signature based auth.
+        }
+    }
+
+    private static class IkeAuthPskConfigUtils {
+        private static final String PSK_KEY = "PSK_KEY";
+
+        @NonNull
+        public static PersistableBundle toPersistableBundle(
+                @NonNull IkeAuthPskConfig config, @NonNull PersistableBundle result) {
+            result.putPersistableBundle(
+                    PSK_KEY, PersistableBundleUtils.fromByteArray(config.getPsk()));
+            return result;
+        }
+
+        @NonNull
+        public static void setBuilderByReadingPersistableBundle(
+                @NonNull PersistableBundle in, @NonNull IkeSessionParams.Builder builder) {
+            PersistableBundle pskBundle = in.getPersistableBundle(PSK_KEY);
+            Objects.requireNonNull(in, "PSK was null");
+            builder.setAuthPsk(PersistableBundleUtils.toByteArray(pskBundle));
+        }
     }
 }
