5a14b96f6cdbe82dacb2d9fb644b5e2485913538
SongFerngWang <songferngwang@google.com>
2021-02-25 15:05:41 +0800

    The phone didn't reload the allowed Network Type
    Reload the allowed Network after device insert SIM card.
    Change the behavior of notifyAllowedNetworkTypesChanged.
    Log print String representation of the allowed network types.
    Bug: 180903931
    Bug: 179814490
    Test: atest cts/tests/tests/telephony/current/src/android/telephony/cts/TelephonyManagerTest.java#testSetAllowedNetworkTypesForReason_moreReason
    atest cts/tests/tests/telephony/current/src/android/telephony/cts/TelephonyManagerTest.java#testSetAllowedNetworkTypesForReason
    atest android.telephony.cts.TelephonyCallbackTest
    
    Change-Id: Icfffea3c74ecf6938455d841da9d6175f6aa07de
    Merged-In: Icfffea3c74ecf6938455d841da9d6175f6aa07de

--- core/java/android/telephony/PhoneStateListener.java
+++ core/java/android/telephony/PhoneStateListener.java
@@ -21,7 +21,6 @@ import android.annotation.NonNull;
 import android.annotation.RequiresPermission;
 import android.annotation.SystemApi;
 import android.annotation.TestApi;
-import android.compat.annotation.ChangeId;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.os.Binder;
 import android.os.Build;
@@ -1576,7 +1575,7 @@ public class PhoneStateListener {
             // default implementation empty
         }
 
-        public void onAllowedNetworkTypesChanged(Map allowedNetworkTypesList) {
+        public void onAllowedNetworkTypesChanged(int reason, long allowedNetworkType) {
             // default implementation empty
         }
     }
