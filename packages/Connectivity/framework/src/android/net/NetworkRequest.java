407fdf411a08f98334869af0ad8015b2fdb7d4e8
Chiachang Wang <chiachangwang@google.com>
2021-03-11 16:13:13 +0800

    Expose setUids in NetworkRequest.Builder
    NetworkRequest is moving into the incoming connectivity mainline
    module. The hidden setUids becomes inaccessible outside the
    module. Shims for support cts in different API levels will need
    to use it to verify the behavior of NetworkRequest. Thus, expose
    it to the API surface.
    
    Test: make update-api
    Bug: 172183305
    Change-Id: I107c329d4d7130d488772166eae8b5e7aaa2ff04

--- packages/Connectivity/framework/src/android/net/NetworkRequest.java
+++ packages/Connectivity/framework/src/android/net/NetworkRequest.java
@@ -276,6 +276,8 @@ public class NetworkRequest implements Parcelable {
          * @return The builder to facilitate chaining.
          * @hide
          */
+        @SystemApi
+        @NonNull
         public Builder setUids(@Nullable Set<Range<Integer>> uids) {
             mNetworkCapabilities.setUids(uids);
             return this;
