e00d1eeb3d266b9fc25e7808026b9a6795142adb
lifr <lifr@google.com>
2021-03-18 01:11:30 +0800

    [TL02]Remove hidden API usage of NetworkAgent
    The connection service will become the mainline module.
    Remove the hidden API usage of NetworkAgent.
    
    Bug: 170598012
    Test: atest FrameworksNetTests FrameworksTelephonyTests
          atest FrameworksWifiTests
    Change-Id: I4e4040ae7f94bdf479c7df9ec2ffabafbe06331c

--- packages/Connectivity/framework/src/android/net/NetworkAgent.java
+++ packages/Connectivity/framework/src/android/net/NetworkAgent.java
@@ -362,9 +362,8 @@ public abstract class NetworkAgent {
     public static final int CMD_UNREGISTER_QOS_CALLBACK = BASE + 21;
 
     private static NetworkInfo getLegacyNetworkInfo(final NetworkAgentConfig config) {
-        // The subtype can be changed with (TODO) setLegacySubtype, but it starts
-        // with 0 (TelephonyManager.NETWORK_TYPE_UNKNOWN) and an empty description.
-        final NetworkInfo ni = new NetworkInfo(config.legacyType, 0, config.legacyTypeName, "");
+        final NetworkInfo ni = new NetworkInfo(config.legacyType, config.legacySubType,
+                config.legacyTypeName, config.legacySubTypeName);
         ni.setIsAvailable(true);
         ni.setDetailedState(NetworkInfo.DetailedState.CONNECTING, null /* reason */,
                 config.getLegacyExtraInfo());
