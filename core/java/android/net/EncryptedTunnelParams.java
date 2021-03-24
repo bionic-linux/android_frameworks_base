9729b69afaf571fbdd7fff67f6af4eaa8144275e
Yan Yan <evitayan@google.com>
2021-03-24 11:20:31 -0700

    Create EncryptedTunnelParams interface
    EncryptedTunnelParams represents configurations for setting up a
    secure encrypted tunnel with a remote endpoint. EncryptedTunnelParams
    will be used to configure a VCN and will be used for VPN configuration
    in the future.
    
    Bug: 180664474
    Test: make update-api
    Change-Id: Ic8e5c8535e84971517f16c54ce8b8645cfc7f944

---
+++ core/java/android/net/EncryptedTunnelParams.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.net;
+
+/**
+ * This interface represents configurations for a secure encrypted tunnel with a remote endpoint.
+ *
+ * <p>The specialized tunnel configuration will need to implement this interface. It will need to
+ * contain all connection, authentication and authorization parameters required to establish an
+ * encrypted tunnel. APIs that depends on encrypted tunnels (e.g. {@link
+ * VcnVcnGatewayConnectionConfig}) may require callers to provide a {@link EncryptedTunnelParams}
+ * instance.
+ *
+ * @see android.net.ipsec.ike.IkeTunnelParams
+ */
+public interface EncryptedTunnelParams {}
