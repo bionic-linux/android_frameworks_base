a59238c25def6e6977f267121b6f17aec1d060fc
Houxiang Dai <houxiang.dai@mediatek.com>
2021-04-06 16:12:23 +0800

    Add customized PQ support
    CSP and TV manufacturer requests IC vendor to support customized PQ.
    Configure different PQ parameters not only on vendor APP but also
    third party of APP as YoutTube,Amazon, Netflix, ... etc.
    
    [Test Report]
    Vendor MMP APP: pass
    YouTube: pass
    
    Bug: 183634943
    Change-Id: Ie4ca776b582adb6fe0838fa9de2e4e2edacb8b0b

--- media/java/android/media/MediaCodec.java
+++ media/java/android/media/MediaCodec.java
@@ -22,12 +22,14 @@ import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.annotation.RequiresPermission;
 import android.annotation.SystemApi;
+import android.app.ActivityThread;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
 import android.hardware.HardwareBuffer;
 import android.media.MediaCodecInfo.CodecCapabilities;
+import android.media.PqApplier;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -36,6 +38,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.PersistableBundle;
 import android.view.Surface;
+import android.util.Log;
 
 import java.io.IOException;
 import java.lang.annotation.Retention;
@@ -1582,6 +1585,7 @@ import java.util.concurrent.locks.ReentrantLock;
  </table>
  */
 final public class MediaCodec {
+    private static final String TAG = "MediaCodec";
 
     /**
      * Per buffer metadata includes an offset and size specifying
@@ -1726,6 +1730,7 @@ final public class MediaCodec {
     private MediaCodecInfo mCodecInfo;
     private final Object mCodecInfoLock = new Object();
     private MediaCrypto mCrypto;
+    private PqApplier mPqApplier;
 
     private static final int EVENT_CALLBACK = 1;
     private static final int EVENT_SET_CALLBACK = 2;
@@ -1998,6 +2003,21 @@ final public class MediaCodec {
         mNameAtCreation = nameIsType ? null : name;
 
         native_setup(name, nameIsType, encoder, pid, uid);
+
+        MediaCodecInfo codecInfo = getCodecInfo();
+        String[] types = codecInfo.getSupportedTypes();
+        for (int j = 0; j < types.length; ++j) {
+            String mediaType = types[j];
+
+            if (!mediaType.toLowerCase().startsWith("audio/") && !encoder) {
+                Log.d(TAG, "decoder type:" + mediaType + " need PqApplier");
+                String packageName = ActivityThread.currentPackageName();
+                if (packageName != null) {
+                    mPqApplier = new PqApplier(this, packageName);
+                }
+                break;
+            }
+        }
     }
 
     private String mNameAtCreation;
@@ -2006,6 +2026,10 @@ final public class MediaCodec {
     protected void finalize() {
         native_finalize();
         mCrypto = null;
+        if (mPqApplier != null && mNativeContext != 0) {
+            mPqApplier.close();
+            mPqApplier = null;
+        }
     }
 
     /**
@@ -2037,6 +2061,10 @@ final public class MediaCodec {
         freeAllTrackedBuffers(); // free buffers first
         native_release();
         mCrypto = null;
+        if (mPqApplier != null) {
+            mPqApplier.close();
+            mPqApplier = null;
+        }
     }
 
     private native final void native_release();
@@ -2213,6 +2241,9 @@ final public class MediaCodec {
         }
 
         native_configure(keys, values, surface, crypto, descramblerBinder, flags);
+        if (mPqApplier != null) {
+            mPqApplier.setPqParamsToHal();
+        }
     }
 
     /**
@@ -4625,6 +4656,12 @@ final public class MediaCodec {
                 }
                 keys[i] = "audio-hw-sync";
                 values[i] = AudioSystem.getAudioHwSyncForSession(sessionId);
+            } else if (key.equals(MediaFormat.KEY_PQ_APPLIER)) {
+                String value = params.getString(key);
+                if (mPqApplier != null) {
+                    mPqApplier.setPqParamsToPqRepoWithSession(value);
+                }
+                continue;
             } else {
                 keys[i] = key;
                 Object value = params.get(key);
@@ -4639,7 +4676,9 @@ final public class MediaCodec {
             ++i;
         }
 
-        setParameters(keys, values);
+        if (0 != i) {
+            setParameters(keys, values);
+        }
     }
 
     /**
