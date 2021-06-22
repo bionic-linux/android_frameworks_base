87d1e84dbbb534be6cc288beaf8b666eac22aa80
Francois Gaffie <francois.gaffie@renault.com>
2021-06-22 10:19:41 +0200

    AudioService: move volume callback on AudioService
    Robustness to crash of AudioServer.
    Take benefit of permission enforcement.
    
    Bug: 293236285
    Test: build & boot & change volumes in Settings apps
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: If556a668f45d608b98b6e8073426f5bb8128c7a2

---
+++ services/core/java/com/android/server/audio/AudioVolumeChangeHandler.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.audio;
+
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.media.AudioSystem;
+import android.media.INativeAudioVolumeGroupCallback;
+import android.media.audiopolicy.IAudioVolumeChangeDispatcher;
+import android.media.audio.common.AudioVolumeGroupChangeEvent;
+import android.os.RemoteCallbackList;
+import android.os.RemoteException;
+import android.util.Slog;
+
+import com.android.internal.annotations.GuardedBy;
+import com.android.internal.util.Preconditions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The AudioVolumeChangeHandler handles AudioVolume callbacks invoked by native
+ * {@link INativeAudioVolumeGroupCallback} callback.
+ */
+/* private package */ class AudioVolumeChangeHandler {
+    private static final String TAG = "AudioVolumeChangeHandler";
+
+    private final Object mLock = new Object();
+    @GuardedBy("mLock")
+    private final RemoteCallbackList<IAudioVolumeChangeDispatcher> mListeners =
+            new RemoteCallbackList<>();
+
+    private @Nullable AudioVolumeGroupCallback mAudioVolumeGroupCallback;
+
+    /**
+     * Initialization: registers the {@link INativeAudioVolumeGroupCallback} callback.
+     */
+    public void init() {
+        mAudioVolumeGroupCallback = new AudioVolumeGroupCallback();
+        AudioSystem.registerAudioVolumeGroupCallback(mAudioVolumeGroupCallback);
+    }
+
+    private void sendAudioVolumeGroupChangedToClients(int groupId, int index) {
+        RemoteCallbackList<IAudioVolumeChangeDispatcher> listeners;
+        int nbDispatchers;
+        synchronized (mLock) {
+            if (mListeners == null) {
+                return;
+            }
+            listeners = mListeners;
+            nbDispatchers = mListeners.beginBroadcast();
+        }
+        for (int i = 0; i < nbDispatchers; i++) {
+            try {
+                mListeners.getBroadcastItem(i).onAudioVolumeGroupChanged(groupId, index);
+            } catch (RemoteException e) {
+                Slog.e(TAG, "Failed to broadcast Volume Changed event");
+            }
+        }
+        mListeners.finishBroadcast();
+    }
+
+    @Override
+    protected void finalize() {
+        if (mAudioVolumeGroupCallback != null) {
+            AudioSystem.unregisterAudioVolumeGroupCallback(mAudioVolumeGroupCallback);
+        }
+    }
+
+   /**
+    * @param cb the {@link IAudioVolumeChangeDispatcher} to register
+    */
+    public void registerListener(@NonNull IAudioVolumeChangeDispatcher cb) {
+        Preconditions.checkNotNull(cb, "Volume group callback must not be null");
+        synchronized (mLock) {
+            mListeners.register(cb);
+        }
+    }
+
+   /**
+    * @param cb the {@link IAudioVolumeChangeDispatcher} to unregister
+    */
+    public void unregisterListener(@NonNull IAudioVolumeChangeDispatcher cb) {
+        Preconditions.checkNotNull(cb, "Volume group callback must not be null");
+        synchronized (mLock) {
+            mListeners.unregister(cb);
+        }
+    }
+
+    private final class AudioVolumeGroupCallback extends INativeAudioVolumeGroupCallback.Stub {
+        public void onAudioVolumeGroupChanged(AudioVolumeGroupChangeEvent volumeEvent) {
+            Slog.v(TAG, "onAudioVolumeGroupChanged volumeEvent=" + volumeEvent);
+            sendAudioVolumeGroupChangedToClients(volumeEvent.groupId, volumeEvent.flags);
+        }
+    }
+}
