dad743cf42ff0db495bc58322b98ab03d598dd74
Grace Jia <xiaotonj@google.com>
2021-07-15 11:36:49 -0700

    Add a lock for call add/remove operations to avoid ConcurrentModificationException.
    Bug: 189816293
    Test: InCallService cts tests
    Change-Id: I123b2de55b856d12ee2a5e6d2fa49c15a74592fc

--- telecomm/java/android/telecom/Phone.java
+++ telecomm/java/android/telecom/Phone.java
@@ -139,6 +139,8 @@ public final class Phone {
      */
     private final int mTargetSdkVersion;
 
+    private final Object mLock = new Object();
+
     Phone(InCallAdapter adapter, String callingPackage, int targetSdkVersion) {
         mInCallAdapter = adapter;
         mCallingPackage = callingPackage;
@@ -157,8 +159,11 @@ public final class Phone {
             call = new Call(this, parcelableCall.getId(), mInCallAdapter,
                     parcelableCall.getState(), mCallingPackage, mTargetSdkVersion);
             mCallByTelecomCallId.put(parcelableCall.getId(), call);
-            mCalls.add(call);
-            checkCallTree(parcelableCall);
+
+            synchronized (mLock) {
+                mCalls.add(call);
+                checkCallTree(parcelableCall);
+            }
             call.internalUpdate(parcelableCall, mCallByTelecomCallId);
             fireCallAdded(call);
         } else {
@@ -169,8 +174,10 @@ public final class Phone {
     }
 
     final void internalRemoveCall(Call call) {
-        mCallByTelecomCallId.remove(call.internalGetCallId());
-        mCalls.remove(call);
+        synchronized (mLock) {
+            mCallByTelecomCallId.remove(call.internalGetCallId());
+            mCalls.remove(call);
+        }
 
         InCallService.VideoCall videoCall = call.getVideoCall();
         if (videoCall != null) {
