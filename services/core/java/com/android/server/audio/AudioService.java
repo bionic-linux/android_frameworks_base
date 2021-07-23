5ea20dddb2d533d03ae001567de9cae2b5a258ca
liuxiaoyu7 <liuxiaoyu7@xiaomi.com>
2021-07-23 20:01:03 +0800

    audio:support adjust stream_assistant when this stream separately from music
    should match stream_assistant firstly
    
    Signed-off-by: liuxiaoyu7 <liuxiaoyu7@xiaomi.com>
    Change-Id: I25fab79515cff6786a112ff011c5c45bf4341872

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -5086,6 +5086,9 @@ public class AudioService extends IAudioService.Stub
             return AudioSystem.STREAM_MUSIC;
         }
 
+        final boolean useAssistantStreamAlias = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_useAssistantVolume);
+
         switch (mPlatformType) {
         case AudioSystem.PLATFORM_VOICE:
             if (isInCommunication()) {
@@ -5097,6 +5100,12 @@ public class AudioService extends IAudioService.Stub
                     return AudioSystem.STREAM_VOICE_CALL;
                 }
             } else if (suggestedStreamType == AudioManager.USE_DEFAULT_STREAM_TYPE) {
+                if (wasStreamActiveRecently(AudioSystem.STREAM_ASSISTANT, sStreamOverrideDelayMs)
+                        && useAssistantStreamAlias) {
+                    if (DEBUG_VOL)
+                        Log.v(TAG, "getActiveStreamType: Forcing STREAM_ASSISTANT active");
+                    return AudioSystem.STREAM_ASSISTANT;
+                }
                 if (wasStreamActiveRecently(AudioSystem.STREAM_RING, sStreamOverrideDelayMs)) {
                     if (DEBUG_VOL)
                         Log.v(TAG, "getActiveStreamType: Forcing STREAM_RING stream active");
@@ -5132,7 +5141,11 @@ public class AudioService extends IAudioService.Stub
                     if (DEBUG_VOL)  Log.v(TAG, "getActiveStreamType: Forcing STREAM_VOICE_CALL");
                     return AudioSystem.STREAM_VOICE_CALL;
                 }
-            } else if (AudioSystem.isStreamActive(
+            } else if (AudioSystem.isStreamActive(AudioSystem.STREAM_ASSISTANT,
+                    sStreamOverrideDelayMs) && useAssistantStreamAlias) {
+                if (DEBUG_VOL) Log.v(TAG, "getActiveStreamType: Forcing STREAM_ASSISTANT");
+                return AudioSystem.STREAM_ASSISTANT;
+            } else if (mAudioSystem.isStreamActive(
                     AudioSystem.STREAM_NOTIFICATION, sStreamOverrideDelayMs)) {
                 if (DEBUG_VOL) Log.v(TAG, "getActiveStreamType: Forcing STREAM_NOTIFICATION");
                 return AudioSystem.STREAM_NOTIFICATION;
