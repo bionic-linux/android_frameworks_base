bb13702cd5286ec7c4c16feba417b09c4402914e
Aaron Huang <huangaaron@google.com>
2021-07-26 19:49:10 +0800

    Respect metered filter in matchesMobile*
    In old design, only metered mobile network would be matched,
    which means that only metered stats could be read if callers
    query the mobile stats.
    
    Currently, the caller in NetworkPolicyManagerService was updated
    to use the NetworkTemplate constructor with metered parameter in
    aosp/1563312 which means that the template will be created by a
    given meteredness. Thus, the hardcode metered can be removed.
    
    Remove the metered check from matches method will cause a
    different behavior. Therefore, to keep the original behavior,
    if the match rule is MATCH_MOBILE or MATCH_MOBILE_WILDCARD,
    it should pass METERED_YES to the constructor to build a
    metered template.
    
    Bug: 183776809
    Test: atest FrameworksNetTests:NetworkStatsServiceTest
    Change-Id: I66dd51b020f6d895cd002acc05bef6b6315cd447

--- core/java/android/net/NetworkTemplate.java
+++ core/java/android/net/NetworkTemplate.java
@@ -305,6 +305,7 @@ public class NetworkTemplate implements Parcelable {
         }
     }
 
+    // TODO: Consider make this function no-op in next Android release.
     @UnsupportedAppUsage
     public NetworkTemplate(int matchRule, String subscriberId, String networkId) {
         this(matchRule, subscriberId, new String[] { subscriberId }, networkId);
@@ -312,9 +313,15 @@ public class NetworkTemplate implements Parcelable {
 
     public NetworkTemplate(int matchRule, String subscriberId, String[] matchSubscriberIds,
             String networkId) {
-        this(matchRule, subscriberId, matchSubscriberIds, networkId, METERED_ALL, ROAMING_ALL,
-                DEFAULT_NETWORK_ALL, NETWORK_TYPE_ALL, OEM_MANAGED_ALL,
-                SUBSCRIBER_ID_MATCH_RULE_EXACT);
+        // In matchesMobile* methods, originally it is hard-codeded to match metered network. Thus,
+        // when building a template with MATCH_MOBILE or MATCH_MOBILE_WILDCARD, meteredness will be
+        // ignored. In current design, hard code to match metered network is removed from the
+        // methods but in order to keep backward compatibility the behavior should be maintained
+        // for @UnsupportedAppUsage use cases.
+        this(matchRule, subscriberId, matchSubscriberIds, networkId,
+                (matchRule  == MATCH_MOBILE || matchRule == MATCH_MOBILE_WILDCARD) ? METERED_YES
+                : METERED_ALL , ROAMING_ALL, DEFAULT_NETWORK_ALL, NETWORK_TYPE_ALL,
+                OEM_MANAGED_ALL, SUBSCRIBER_ID_MATCH_RULE_EXACT);
     }
 
     // TODO: Remove it after updating all of the caller.
@@ -589,11 +596,7 @@ public class NetworkTemplate implements Parcelable {
             // TODO: consider matching against WiMAX subscriber identity
             return true;
         } else {
-            // Only metered mobile network would be matched regardless of metered filter.
-            // This is used to exclude non-metered APNs, e.g. IMS. See ag/908650.
-            // TODO: Respect metered filter and remove mMetered condition.
-            return (ident.mType == TYPE_MOBILE && ident.mMetered)
-                    && !ArrayUtils.isEmpty(mMatchSubscriberIds)
+            return ident.mType == TYPE_MOBILE && !ArrayUtils.isEmpty(mMatchSubscriberIds)
                     && ArrayUtils.contains(mMatchSubscriberIds, ident.mSubscriberId)
                     && matchesCollapsedRatType(ident);
         }
@@ -707,8 +710,7 @@ public class NetworkTemplate implements Parcelable {
         if (ident.mType == TYPE_WIMAX) {
             return true;
         } else {
-            return (ident.mType == TYPE_MOBILE && ident.mMetered)
-                    && matchesCollapsedRatType(ident);
+            return ident.mType == TYPE_MOBILE && matchesCollapsedRatType(ident);
         }
     }
 
