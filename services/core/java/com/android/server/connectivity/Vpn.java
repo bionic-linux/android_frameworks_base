16527d78451b156eb4beb900831b9563d7ab1d11
lucaslin <lucaslin@google.com>
2021-09-28 13:23:54 +0800

    Update underlying networks if it is changed
    Bug: 191918368
    Test: atest CtsHostsideNetworkTests:HostsideVpnTests
    Change-Id: Iabcf0538121aeb839c6acfcc9b0005df5488a436

--- services/core/java/com/android/server/connectivity/Vpn.java
+++ services/core/java/com/android/server/connectivity/Vpn.java
@@ -1398,7 +1398,11 @@ public class Vpn {
             // parameters. If that fails, disconnect.
             if (oldConfig != null
                     && updateLinkPropertiesInPlaceIfPossible(mNetworkAgent, oldConfig)) {
-                // Keep mNetworkAgent unchanged
+                // Update underlying networks if it is changed.
+                if (!Arrays.equals(oldConfig.underlyingNetworks, config.underlyingNetworks)) {
+                    mNetworkAgent.setUnderlyingNetworks((config.underlyingNetworks != null)
+                            ? Arrays.asList(config.underlyingNetworks) : null);
+                }
             } else {
                 // Initialize the state for a new agent, while keeping the old one connected
                 // in case this new connection fails.
