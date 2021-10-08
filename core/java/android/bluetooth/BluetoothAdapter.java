7a00b768601c3911ade1eea3408c23f688542c2e
Grzegorz Kołodziejczyk <grzegorz.kolodziejczyk@codecoup.pl>
2021-10-08 20:46:51 +0000

    leaudio: Add initial LeAudio implementation
    This patch adds
    1. Additional APIs for managing groups
    2. Audio routing
    3. Improves setting LeAudio active group
    
    Bug: 150670922
    Tag: #feature
    Sponsor: jpawlowski@
    Test: Manual
    
    Change-Id: Ie4276f2a21fbc83078cbbca0944102e218dac91e

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -2313,6 +2313,20 @@ public final class BluetoothAdapter {
     }
 
     /**
+     * Return true if LeAudio Profile is supported.
+     *
+     * @return true if phone supports LeAudio
+     */
+    private boolean isLeAudioProfileSupported() {
+        try {
+            return mManagerService.isLeAudioProfileSupported();
+        } catch (RemoteException e) {
+            Log.e(TAG, "remote expection when calling isLeAudioProfileSupported", e);
+            return false;
+        }
+    }
+
+    /**
      * Get the maximum number of connected audio devices.
      *
      * @return the maximum number of connected audio devices
@@ -2518,6 +2532,9 @@ public final class BluetoothAdapter {
                     // Bluetooth is disabled. Just fill in known supported Profiles
                     if (isHearingAidProfileSupported()) {
                         supportedProfiles.add(BluetoothProfile.HEARING_AID);
+                    }
+                    if (isLeAudioProfileSupported()) {
+                        supportedProfiles.add(BluetoothProfile.LE_AUDIO);
                     }
                 }
             }
