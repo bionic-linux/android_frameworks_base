153880f5b7dd604d2ac46abf53a4f1450cbe7603
Łukasz Rymanowski <lukasz.rymanowski@codecoup.pl>
2021-10-13 08:25:18 +0000

    provider/Settings: Extend with LeAudio
    Bug: 150670922
    Tag: #feature
    Sponsor: jpawlowski@
    Test: Manual
    
    Change-Id: I6301ad5c7dfc70e2516a16fa7ce3121b73a2421e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4893,6 +4893,14 @@ public final class Settings {
         public static final String HEARING_AID = "hearing_aid";
 
         /**
+         * Whether the le audio is enabled. The value is
+         * boolean (1 or 0).
+         * @hide
+         */
+        @UnsupportedAppUsage
+        public static final String LE_AUDIO = "le_audio";
+
+        /**
          * CDMA only settings
          * TTY Mode
          * 0 = OFF
@@ -5252,6 +5260,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(HIDE_ROTATION_LOCK_TOGGLE_FOR_ACCESSIBILITY);
             PRIVATE_SETTINGS.add(DTMF_TONE_TYPE_WHEN_DIALING);
             PRIVATE_SETTINGS.add(HEARING_AID);
+            PRIVATE_SETTINGS.add(LE_AUDIO);
             PRIVATE_SETTINGS.add(TTY_MODE);
             PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE);
             PRIVATE_SETTINGS.add(POINTER_LOCATION);
@@ -12790,7 +12799,10 @@ public final class Settings {
         @Readable
         public static final String
                 BLUETOOTH_HEARING_AID_PRIORITY_PREFIX = "bluetooth_hearing_aid_priority_";
-
+        @Readable
+        /** {@hide} */
+        public static final String
+                BLUETOOTH_LE_AUDIO_PRIORITY_PREFIX = "bluetooth_le_audio_priority_";
         /**
          * Enable/disable radio bug detection
          *
