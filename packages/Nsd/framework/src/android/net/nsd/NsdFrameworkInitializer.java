48190a2d6d9cd3d44fbcfb3586d94ce50c328bc1
paulhu <paulhu@google.com>
2021-11-23 16:16:13 +0800

    Register NSD service from NsdFrameworkInitializer
    INsdManager is going to move into connectivity mainline module
    and it will be not visible to SystemServiceRegistry after
    migration done. Thus, use NsdFrameworkInitializer to register
    NSD service instead.
    
    Bug: 206702844
    Test: atest FrameworksNetTests CtsNetTestCases
    Change-Id: Ibf89ab9a35e35dac4978ba70c7ab306b6155a4a3

---
+++ packages/Nsd/framework/src/android/net/nsd/NsdFrameworkInitializer.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net.nsd;
+
+import android.annotation.SystemApi;
+import android.app.SystemServiceRegistry;
+import android.content.Context;
+
+/**
+ * Class for performing registration for nsd services.
+ *
+ * @hide
+ */
+@SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
+public final class NsdFrameworkInitializer {
+    private NsdFrameworkInitializer() {}
+
+    /**
+     * Called by {@link SystemServiceRegistry}'s static initializer and registers nsd services to
+     * {@link Context}, so that {@link Context#getSystemService} can return them.
+     *
+     * @throws IllegalStateException if this is called anywhere besides
+     * {@link SystemServiceRegistry}.
+     */
+    public static void registerServiceWrappers() {
+        SystemServiceRegistry.registerContextAwareService(
+                Context.NSD_SERVICE,
+                NsdManager.class,
+                (context, serviceBinder) -> {
+                    INsdManager service = INsdManager.Stub.asInterface(serviceBinder);
+                    return new NsdManager(context, service);
+                }
+        );
+    }
+}
