a1c1cf5b369dd3c55d731a44851edd7c99a348fd
Benedict Wong <benedictwong@google.com>
2021-12-16 19:34:03 +0000

    Add minimum required bandwidth to network templates
    This change adds a minimum required bandwidth to the VCN network
    templates, ensuring that a minimum bar of throughput can be required of
    underlying networks.
    
    Bug: 211008106
    Test: atest FrameworksVcnTests
    Change-Id: I929428f75e5e543966a03061d13f300dd8b0956f

--- core/java/android/net/vcn/VcnWifiUnderlyingNetworkTemplate.java
+++ core/java/android/net/vcn/VcnWifiUnderlyingNetworkTemplate.java
@@ -46,8 +46,19 @@ public final class VcnWifiUnderlyingNetworkTemplate extends VcnUnderlyingNetwork
     @Nullable private final Set<String> mSsids;
 
     private VcnWifiUnderlyingNetworkTemplate(
-            int networkQuality, int meteredMatchCriteria, Set<String> ssids) {
-        super(NETWORK_PRIORITY_TYPE_WIFI, networkQuality, meteredMatchCriteria);
+            int meteredMatchCriteria,
+            int minEntryUpstreamBandwidthKbps,
+            int minExitUpstreamBandwidthKbps,
+            int minEntryDownstreamBandwidthKbps,
+            int minExitDownstreamBandwidthKbps,
+            Set<String> ssids) {
+        super(
+                NETWORK_PRIORITY_TYPE_WIFI,
+                meteredMatchCriteria,
+                minEntryUpstreamBandwidthKbps,
+                minExitUpstreamBandwidthKbps,
+                minEntryDownstreamBandwidthKbps,
+                minExitDownstreamBandwidthKbps);
         mSsids = new ArraySet<>(ssids);
 
         validate();
@@ -75,15 +86,29 @@ public final class VcnWifiUnderlyingNetworkTemplate extends VcnUnderlyingNetwork
             @NonNull PersistableBundle in) {
         Objects.requireNonNull(in, "PersistableBundle is null");
 
-        final int networkQuality = in.getInt(NETWORK_QUALITY_KEY);
         final int meteredMatchCriteria = in.getInt(METERED_MATCH_KEY);
+
+        final int minEntryUpstreamBandwidthKbps =
+                in.getInt(MIN_ENTRY_UPSTREAM_BANDWIDTH_KBPS_KEY, DEFAULT_MIN_BANDWIDTH_KBPS);
+        final int minExitUpstreamBandwidthKbps =
+                in.getInt(MIN_ENTRY_DOWNSTREAM_BANDWIDTH_KBPS_KEY, DEFAULT_MIN_BANDWIDTH_KBPS);
+        final int minEntryDownstreamBandwidthKbps =
+                in.getInt(MIN_EXIT_UPSTREAM_BANDWIDTH_KBPS_KEY, DEFAULT_MIN_BANDWIDTH_KBPS);
+        final int minExitDownstreamBandwidthKbps =
+                in.getInt(MIN_EXIT_DOWNSTREAM_BANDWIDTH_KBPS_KEY, DEFAULT_MIN_BANDWIDTH_KBPS);
 
         final PersistableBundle ssidsBundle = in.getPersistableBundle(SSIDS_KEY);
         Objects.requireNonNull(ssidsBundle, "ssidsBundle is null");
         final Set<String> ssids =
                 new ArraySet<String>(
                         PersistableBundleUtils.toList(ssidsBundle, STRING_DESERIALIZER));
-        return new VcnWifiUnderlyingNetworkTemplate(networkQuality, meteredMatchCriteria, ssids);
+        return new VcnWifiUnderlyingNetworkTemplate(
+                meteredMatchCriteria,
+                minEntryUpstreamBandwidthKbps,
+                minExitUpstreamBandwidthKbps,
+                minEntryDownstreamBandwidthKbps,
+                minExitDownstreamBandwidthKbps,
+                ssids);
     }
 
     /** @hide */
@@ -137,31 +162,16 @@ public final class VcnWifiUnderlyingNetworkTemplate extends VcnUnderlyingNetwork
 
     /** This class is used to incrementally build VcnWifiUnderlyingNetworkTemplate objects. */
     public static final class Builder {
-        private int mNetworkQuality = NETWORK_QUALITY_ANY;
         private int mMeteredMatchCriteria = MATCH_ANY;
         @NonNull private final Set<String> mSsids = new ArraySet<>();
 
+        private int mMinEntryUpstreamBandwidthKbps = DEFAULT_MIN_BANDWIDTH_KBPS;
+        private int mMinExitUpstreamBandwidthKbps = DEFAULT_MIN_BANDWIDTH_KBPS;
+        private int mMinEntryDownstreamBandwidthKbps = DEFAULT_MIN_BANDWIDTH_KBPS;
+        private int mMinExitDownstreamBandwidthKbps = DEFAULT_MIN_BANDWIDTH_KBPS;
+
         /** Construct a Builder object. */
         public Builder() {}
-
-        /**
-         * Set the required network quality to match this template.
-         *
-         * <p>Network quality is a aggregation of multiple signals that reflect the network link
-         * metrics. For example, the network validation bit (see {@link
-         * NetworkCapabilities#NET_CAPABILITY_VALIDATED}), estimated first hop transport bandwidth
-         * and signal strength.
-         *
-         * @param networkQuality the required network quality. Defaults to NETWORK_QUALITY_ANY
-         * @hide
-         */
-        @NonNull
-        public Builder setNetworkQuality(@NetworkQuality int networkQuality) {
-            validateNetworkQuality(networkQuality);
-
-            mNetworkQuality = networkQuality;
-            return this;
-        }
 
         /**
          * Set the matching criteria for metered networks.
@@ -196,11 +206,115 @@ public final class VcnWifiUnderlyingNetworkTemplate extends VcnUnderlyingNetwork
             return this;
         }
 
+        /**
+         * Set the minimum accepted entry and exit upstream bandwidth allowed by this template.
+         *
+         * <p>In order to allow for hysteresis, the minimum upstream bandwidth condition allows for
+         * two conditions, an entry threshold, for which a generic network must meet in order to be
+         * match this rule (and possibly become the selected underlying network), as well as a
+         * (potentially) more lenient exit threshold, where a network that is already the selected
+         * underlying network must meet, failing which it will stop matching this template.
+         *
+         * <p>As such, in order to match this template, a network must fulfill one of the following
+         * two cases:
+         *
+         * <ul>
+         *   <li>If a network IS selected as the VCN Gateway Connection's underlying network, it
+         *       MUST advertise an estimated upstream bandwidth greater than, or equal to the
+         *       minExitUpstreamBandwidthKbps provided.
+         *   <li>If a network IS NOT selected as the VCN Gateway Connection's underlying network, it
+         *       MUST advertise an estimated upstream bandwidth greater than, or equal to the
+         *       minEntryUpstreamBandwidthKbps provided.
+         * </ul>
+         *
+         * <p>To prevent a network from being selected (fulfilling the entry criteria), and yet be
+         * unable to meet the conditions for staying selected (exit criteria), the entry criteria
+         * MUST be more stringent, or equally stringent as the exit criteria.
+         *
+         * <p>Estimated bandwidth of a network is provided by the transport layer, and reported in
+         * {@link NetworkCapabilities}. The provided estimates will be used without modification.
+         *
+         * @param minEntryUpstreamBandwidthKbps the minimum accepted upstream bandwidth for a
+         *     network that IS NOT an already-selected underlying network, or {@code 0} to disable
+         *     this requirement. MUST be greater than, or equal to minExitUpstreamBandwidthKbps.
+         *     Defaults to {@code 0}
+         * @param minExitUpstreamBandwidthKbps the minimum accepted upstream bandwidth for a network
+         *     that IS the already-selected underlying network, or {@code 0} to disable this
+         *     requirement. MUST be less than minExitUpstreamBandwidthKbps. Defaults to {@code 0}
+         * @return this {@link Builder} instance, for chaining
+         * @hide
+         */
+        @NonNull
+        public Builder setMinUpstreamBandwidthKbps(
+                int minEntryUpstreamBandwidthKbps, int minExitUpstreamBandwidthKbps) {
+            validateMinBandwidthKbps(minEntryUpstreamBandwidthKbps, minExitUpstreamBandwidthKbps);
+
+            mMinEntryUpstreamBandwidthKbps = minEntryUpstreamBandwidthKbps;
+            mMinExitUpstreamBandwidthKbps = minExitUpstreamBandwidthKbps;
+
+            return this;
+        }
+
+        /**
+         * Set the minimum acceptable downstream bandwidths allowed by this template.
+         *
+         * <p>In order to allow for hysteresis, the minimum downstream bandwidth condition allows
+         * for two thresholds to be set: an entry threshold, for which a generic network must meet
+         * in order to be match this rule (and possibly become the selected underlying network), as
+         * well as a (potentially) more lenient exit threshold, where a network that is already the
+         * selected underlying network must meet, failing which it will stop matching this template.
+         *
+         * <p>As such, in order to match this template, a network must fulfill one of the following
+         * two cases:
+         *
+         * <ul>
+         *   <li>If a network IS selected as the VCN Gateway Connection's underlying network, it
+         *       MUST advertise an estimated downstream bandwidth greater than, or equal to the
+         *       minExitDownstreamBandwidthKbps provided.
+         *   <li>If a network IS NOT selected as the VCN Gateway Connection's underlying network, it
+         *       MUST advertise an estimated downstream bandwidth greater than, or equal to the
+         *       minEntryDownstreamBandwidthKbps provided.
+         * </ul>
+         *
+         * <p>To prevent a network from being selected (fulfilling the entry criteria), and yet be
+         * unable to meet the conditions for staying selected (exit criteria), the entry criteria
+         * MUST be more stringent, or equally stringent as the exit criteria.
+         *
+         * <p>Estimated bandwidth of a network is provided by the transport layer, and reported in
+         * {@link NetworkCapabilities}. The provided estimates will be used without modification.
+         *
+         * @param minEntryDownstreamBandwidthKbps the minimum accepted downstream bandwidth for a
+         *     network that IS NOT an already-selected underlying network, or {@code 0} to disable
+         *     this requirement. MUST be greater than, or equal to minExitDownstreamBandwidthKbps.
+         *     Defaults to {@code 0}
+         * @param minExitDownstreamBandwidthKbps the minimum accepted downstream bandwidth for a
+         *     network that IS the already-selected underlying network, or {@code 0} to disable this
+         *     requirement. MUST be less than minExitDownstreamBandwidthKbps. Defaults to {@code 0}
+         * @return this {@link Builder} instance, for chaining
+         * @hide
+         */
+        @NonNull
+        public Builder setMinDownstreamBandwidthKbps(
+                int minEntryDownstreamBandwidthKbps, int minExitDownstreamBandwidthKbps) {
+            validateMinBandwidthKbps(
+                    minEntryDownstreamBandwidthKbps, minExitDownstreamBandwidthKbps);
+
+            mMinEntryDownstreamBandwidthKbps = minEntryDownstreamBandwidthKbps;
+            mMinExitDownstreamBandwidthKbps = minExitDownstreamBandwidthKbps;
+
+            return this;
+        }
+
         /** Build the VcnWifiUnderlyingNetworkTemplate. */
         @NonNull
         public VcnWifiUnderlyingNetworkTemplate build() {
             return new VcnWifiUnderlyingNetworkTemplate(
-                    mNetworkQuality, mMeteredMatchCriteria, mSsids);
+                    mMeteredMatchCriteria,
+                    mMinEntryUpstreamBandwidthKbps,
+                    mMinExitUpstreamBandwidthKbps,
+                    mMinEntryDownstreamBandwidthKbps,
+                    mMinExitDownstreamBandwidthKbps,
+                    mSsids);
         }
     }
 }
