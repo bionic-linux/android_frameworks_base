6ba9a4ce79b811a60c9d28ab14beff671b768f8a
Benedict Wong <benedictwong@google.com>
2021-12-16 19:08:30 +0000

    Add VcnLinkCriteria
    This change adds a new LinkCriteria class, which will be used to specify
    all link-metric based selection criteria. A set of this class will be
    added to the UnderlyingNetworkTemplate(s), and used by the VCN to select
    underlying networks at specific priorities.
    
    Bug: 211008106
    Test: atest FrameworksVcnTests
    Change-Id: I27e668e37e80afa4b667b91740bf3b0df14fc24a

---
+++ tests/vcn/java/android/net/vcn/VcnLinkCriteriaTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.net.vcn;
+
+
+import static org.junit.Assert.assertEquals;
+
+import android.net.vcn.VcnLinkCriteria.EstimatedBandwidthCriteria;
+
+import org.junit.Test;
+
+public class VcnLinkCriteriaTest {
+    private static final int TEST_MIN_UPSTREAM_BANDWIDTH_KBPS = 100;
+    private static final int TEST_MIN_DOWNSTREAM_BANDWIDTH_KBPS = 200;
+
+    // Package private for use in Vcn(Wifi|Cell)GatewayConnectionConfigTest
+    static EstimatedBandwidthCriteria getTestEstimatedBandwidthCriteria() {
+        return new EstimatedBandwidthCriteria(
+                TEST_MIN_UPSTREAM_BANDWIDTH_KBPS, TEST_MIN_DOWNSTREAM_BANDWIDTH_KBPS);
+    }
+
+    @Test
+    public void testEstimatedBandwidthCriteria_fields() {
+        final EstimatedBandwidthCriteria criteria = getTestEstimatedBandwidthCriteria();
+        assertEquals(TEST_MIN_UPSTREAM_BANDWIDTH_KBPS, criteria.minUpstreamBandwidthKbps);
+        assertEquals(TEST_MIN_DOWNSTREAM_BANDWIDTH_KBPS, criteria.minDownstreamBandwidthKbps);
+    }
+
+    @Test
+    public void testEstimatedBandwidthCriteria_persistableBundle() {
+        final EstimatedBandwidthCriteria criteria = getTestEstimatedBandwidthCriteria();
+        assertEquals(criteria, new EstimatedBandwidthCriteria(criteria.toPersistableBundle()));
+    }
+}
