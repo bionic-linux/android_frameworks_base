70fb5a6238cfaad7a6983ee4c1ceede300b6849d
Junyu Lai <junyulai@google.com>
2022-01-05 07:35:49 +0000

    [MS39.1] Remove the looper from NetworkStatsSubscriptionMonitor
    OnSubscriptionsChangedListenerHandler(Looper looper) is
    hidden, there is no way to pass a looper to it in the test.
    This change partially revert aosp/1340040 to fix hidden
    API dependency. And will use a different approach to address
    this problem in later stages.
    
    Test: atest NetworkStatsSubscriptionMonitorTest
    Bug: 204830222
    Bug: 213280079
    Change-Id: I429be3578d599de9a5a3f78b02387c20744b37d3

--- packages/ConnectivityT/service/src/com/android/server/net/NetworkStatsService.java
+++ packages/ConnectivityT/service/src/com/android/server/net/NetworkStatsService.java
@@ -455,7 +455,7 @@ public class NetworkStatsService extends INetworkStatsService.Stub {
         handlerThread.start();
         mHandler = new NetworkStatsHandler(handlerThread.getLooper());
         mNetworkStatsSubscriptionsMonitor = deps.makeSubscriptionsMonitor(mContext,
-                mHandler.getLooper(), new HandlerExecutor(mHandler), this);
+                new HandlerExecutor(mHandler), this);
         mContentResolver = mContext.getContentResolver();
         mContentObserver = mDeps.makeContentObserver(mHandler, mSettings,
                 mNetworkStatsSubscriptionsMonitor);
@@ -481,11 +481,11 @@ public class NetworkStatsService extends INetworkStatsService.Stub {
          */
         @NonNull
         public NetworkStatsSubscriptionsMonitor makeSubscriptionsMonitor(@NonNull Context context,
-                @NonNull Looper looper, @NonNull Executor executor,
+                @NonNull Executor executor,
                 @NonNull NetworkStatsService service) {
             // TODO: Update RatType passively in NSS, instead of querying into the monitor
             //  when notifyNetworkStatus.
-            return new NetworkStatsSubscriptionsMonitor(context, looper, executor,
+            return new NetworkStatsSubscriptionsMonitor(context, executor,
                     (subscriberId, type) -> service.handleOnCollapsedRatTypeChanged());
         }
 
