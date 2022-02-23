97f15212335d3b07ea4c59985c3c5a2f02552d23
Michael Wright <michaelwr@google.com>
2022-02-23 03:48:52 +0000

    Allow for replaceable clock within Looper
    Handler, Looper, and MessageQueue are all difficult to test since their
    internal implementations implicitly rely on the system clock. By making
    their clock replaceable in tests, we can make their behavior more
    deterministic, allowing for less flaky tests overall.
    
    Also, update TestLooper to take advantage of the replaceable clock.
    
    Bug: 184781936
    Test: atest LooperTest MessageQueueTest HandlerTest
    Change-Id: I215d2eab99888a162175c37157d7be2be911b96d

--- core/java/android/os/Handler.java
+++ core/java/android/os/Handler.java
@@ -182,7 +182,7 @@ public class Handler {
      *
      * Asynchronous messages represent interrupts or events that do not require global ordering
      * with respect to synchronous messages.  Asynchronous messages are not subject to
-     * the synchronization barriers introduced by {@link MessageQueue#enqueueSyncBarrier(long)}.
+     * the synchronization barriers introduced by {@link MessageQueue#postSyncBarrier()}.
      *
      * @param async If true, the handler calls {@link Message#setAsynchronous(boolean)} for
      * each {@link Message} that is sent to it or {@link Runnable} that is posted to it.
@@ -203,7 +203,7 @@ public class Handler {
      *
      * Asynchronous messages represent interrupts or events that do not require global ordering
      * with respect to synchronous messages.  Asynchronous messages are not subject to
-     * the synchronization barriers introduced by {@link MessageQueue#enqueueSyncBarrier(long)}.
+     * the synchronization barriers introduced by {@link MessageQueue#postSyncBarrier()}.
      *
      * @param callback The callback interface in which to handle messages, or null.
      * @param async If true, the handler calls {@link Message#setAsynchronous(boolean)} for
@@ -212,24 +212,17 @@ public class Handler {
      * @hide
      */
     public Handler(@Nullable Callback callback, boolean async) {
-        if (FIND_POTENTIAL_LEAKS) {
-            final Class<? extends Handler> klass = getClass();
-            if ((klass.isAnonymousClass() || klass.isMemberClass() || klass.isLocalClass()) &&
-                    (klass.getModifiers() & Modifier.STATIC) == 0) {
-                Log.w(TAG, "The following Handler class should be static or leaks might occur: " +
-                    klass.getCanonicalName());
-            }
-        }
+        this(getThreadLooper(), callback, async);
+    }
 
-        mLooper = Looper.myLooper();
-        if (mLooper == null) {
+    private static Looper getThreadLooper() {
+        final Looper looper = Looper.myLooper();
+        if (looper == null) {
             throw new RuntimeException(
-                "Can't create handler inside thread " + Thread.currentThread()
-                        + " that has not called Looper.prepare()");
+                    "Can't create handler inside thread " + Thread.currentThread()
+                            + " that has not called Looper.prepare()");
         }
-        mQueue = mLooper.mQueue;
-        mCallback = callback;
-        mAsynchronous = async;
+        return looper;
     }
 
     /**
@@ -253,10 +246,19 @@ public class Handler {
      */
     @UnsupportedAppUsage
     public Handler(@NonNull Looper looper, @Nullable Callback callback, boolean async) {
+        if (FIND_POTENTIAL_LEAKS) {
+            final Class<? extends Handler> klass = getClass();
+            if ((klass.isAnonymousClass() || klass.isMemberClass() || klass.isLocalClass())
+                    && (klass.getModifiers() & Modifier.STATIC) == 0) {
+                Log.w(TAG, "The following Handler class should be static or leaks might occur: "
+                        + klass.getCanonicalName());
+            }
+        }
         mLooper = looper;
         mQueue = looper.mQueue;
         mCallback = callback;
         mAsynchronous = async;
+        mClock = looper.getClock();
     }
 
     /**
@@ -694,7 +696,14 @@ public class Handler {
         if (delayMillis < 0) {
             delayMillis = 0;
         }
-        return sendMessageAtTime(msg, SystemClock.uptimeMillis() + delayMillis);
+        // mClock should theoretically never be null, but some tests create a mock handler that
+        // instantiates an instance where all members are null. Ideally we'd fix these tests to
+        // not rely on this but there are quite a lot at this point, so it's easier to just keep
+        // the existing behavior.
+        if (mClock == null) {
+            return false;
+        }
+        return sendMessageAtTime(msg, mClock.uptimeMillis() + delayMillis);
     }
 
     /**
@@ -879,7 +888,7 @@ public class Handler {
     }
 
     public final void dump(@NonNull Printer pw, @NonNull String prefix) {
-        pw.println(prefix + this + " @ " + SystemClock.uptimeMillis());
+        pw.println(prefix + this + " @ " + mClock.uptimeMillis());
         if (mLooper == null) {
             pw.println(prefix + "looper uninitialized");
         } else {
@@ -891,7 +900,7 @@ public class Handler {
      * @hide
      */
     public final void dumpMine(@NonNull Printer pw, @NonNull String prefix) {
-        pw.println(prefix + this + " @ " + SystemClock.uptimeMillis());
+        pw.println(prefix + this + " @ " + mClock.uptimeMillis());
         if (mLooper == null) {
             pw.println(prefix + "looper uninitialized");
         } else {
@@ -948,6 +957,7 @@ public class Handler {
     @UnsupportedAppUsage
     final Callback mCallback;
     final boolean mAsynchronous;
+    final MessageQueue.Clock mClock;
     @UnsupportedAppUsage
     IMessenger mMessenger;
 
@@ -978,9 +988,9 @@ public class Handler {
 
             synchronized (this) {
                 if (timeout > 0) {
-                    final long expirationTime = SystemClock.uptimeMillis() + timeout;
+                    final long expirationTime = handler.mClock.uptimeMillis() + timeout;
                     while (!mDone) {
-                        long delay = expirationTime - SystemClock.uptimeMillis();
+                        long delay = expirationTime - handler.mClock.uptimeMillis();
                         if (delay <= 0) {
                             return false; // timeout
                         }
