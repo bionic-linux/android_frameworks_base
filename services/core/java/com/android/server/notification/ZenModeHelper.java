f5495181b061da990a3b32e9389c6c7b967c1ae5
liuxiaoyu7 <liuxiaoyu7@xiaomi.com>
2022-03-30 14:54:51 +0800

    zenmode:call should always be muted, only the exception package name can pass.
    Otherwise, other apps will not be muted when the repeating call is allowed.
    
    Signed-off-by: liuxiaoyu7 <liuxiaoyu7@xiaomi.com>
    Change-Id: I44f5e94115171f735e87bcd6ae7c7ffdbd957a8f

--- services/core/java/com/android/server/notification/ZenModeHelper.java
+++ services/core/java/com/android/server/notification/ZenModeHelper.java
@@ -1046,9 +1046,6 @@ public class ZenModeHelper {
         final boolean zenPriorityOnly = mZenMode == Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS;
         final boolean zenSilence = mZenMode == Global.ZEN_MODE_NO_INTERRUPTIONS;
         final boolean zenAlarmsOnly = mZenMode == Global.ZEN_MODE_ALARMS;
-        final boolean allowCalls = mConsolidatedPolicy.allowCalls()
-                && mConsolidatedPolicy.allowCallsFrom() == PRIORITY_SENDERS_ANY;
-        final boolean allowRepeatCallers = mConsolidatedPolicy.allowRepeatCallers();
         final boolean allowSystem = mConsolidatedPolicy.allowSystem();
         final boolean allowMedia = mConsolidatedPolicy.allowMedia();
         final boolean allowAlarms = mConsolidatedPolicy.allowAlarms();
@@ -1057,8 +1054,7 @@ public class ZenModeHelper {
         final boolean muteNotifications = zenOn
                 || (mSuppressedEffects & SUPPRESSED_EFFECT_NOTIFICATIONS) != 0;
         // call restrictions
-        final boolean muteCalls = zenAlarmsOnly
-                || (zenPriorityOnly && !(allowCalls || allowRepeatCallers))
+        final boolean muteCalls = zenAlarmsOnly || zenPriorityOnly
                 || (mSuppressedEffects & SUPPRESSED_EFFECT_CALLS) != 0;
         // alarm restrictions
         final boolean muteAlarms = zenPriorityOnly && !allowAlarms;
