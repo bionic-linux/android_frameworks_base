46d80bae9e704a5df9a18d75a3737f2440135bf6
Jeff Sharkey <jsharkey@android.com>
2022-04-11 10:43:46 -0600

    Script to prune inactive OWNERS.
    We can end up with outdated owners information as people migrate
    between teams over time.  This script automates the process of
    pruning inactive team members.
    
    Bug: 211800417
    Test: none
    Change-Id: I17eb18d2602650e56b8b94414af9dd5983a62cef

---
+++ tools/owners/inactive.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# Usage: python $ANDROID_BUILD_TOP/frameworks/base/tools/owners/inactive.py
+
+import os, re, collections
+
+# Discover active team members via:
+# ganpati2 --ganpati2=ganpati2  membership list --parent %android-team-auto.prod > /tmp/active_ldaps
+with open("/tmp/active_ldaps") as f:
+    ACTIVE_LDAPS = f.read().split("\n")
+
+for root, dirs, files in os.walk("."):
+    for name in files:
+        name = os.path.join(root, name)
+        if "OWNER" in name:
+            with open(name, "r+") as f:
+                raw = f.read()
+                before = raw
+                refs = re.findall(r"([a-z\-+]+@(?:android.com|google.com))", raw)
+                for ref in refs:
+                    ldap = ref[0:ref.index("@")]
+                    if ldap not in ACTIVE_LDAPS:
+                        raw = re.sub(r", ?%s" % (ref), "", raw)
+                        raw = re.sub(r"%s, ?" % (ref), "", raw)
+                        raw = re.sub(r"%s\n?" % (ref), "", raw)
+                if raw != before:
+                    f.seek(0)
+                    f.write(raw)
+                    f.truncate()
