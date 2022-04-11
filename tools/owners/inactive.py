2658376e90078e3f124fe6bca176d778da85fe79
Jeff Sharkey <jsharkey@android.com>
2022-04-11 10:43:46 -0600

    Script to prune inactive OWNERS.
    We can end up with outdated owners information as people migrate
    between teams over time.  This script automates the process of
    pruning inactive team members.
    
    Bug: 211800417
    Test: none
    Change-Id: I17eb18d2602650e56b8b94414af9dd5983a62cef

---
+++ tools/owners/inactive.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+# Usage:
+# ganpati2 --ganpati2=ganpati2  membership list --parent %android-team-auto.prod > /path/to/active/ldaps
+# ganpati2 --ganpati2=ganpati2  membership list --parent %android-access.prod >> /path/to/active/ldaps
+# inactive.py /path/to/active/ldaps
+
+import sys, os, re, collections
+
+def filter_inactive(raw, active_ldaps):
+    res = []
+    for line in raw.split("\n"):
+        if not line.startswith("#") and "@" in line:
+            refs = re.findall(r"([a-z\-+]+@(?:android.com|google.com))", line)
+            for ref in refs:
+                ldap = ref[0:ref.index("@")]
+                if ldap not in active_ldaps:
+                    line = re.sub(r"%s,? ?" % (ref), "", line)
+            if line.startswith("per-file "):
+                if line.endswith(","):
+                    line = line[0:-1]
+                elif line.endswith(", "):
+                    line = line[0:-2]
+            if "@" not in line:
+                line = None
+        if line is not None:
+            res.append(line)
+    return "\n".join(res)
+
+
+def main():
+    with open(sys.argv[1]) as f:
+        active_ldaps = f.read().split("\n")
+
+    for root, dirs, files in os.walk("."):
+        for name in files:
+            name = os.path.join(root, name)
+            if "OWNER" in name:
+                with open(name, "r+") as f:
+                    before = f.read()
+                    after = filter_inactive(before, active_ldaps)
+                    if before != after:
+                        f.seek(0)
+                        f.write(after)
+                        f.truncate()
+
+if __name__ == "__main__":
+    main()
