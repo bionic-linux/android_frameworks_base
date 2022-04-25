cbfa52b294ae5c51ccee8c5929abb050d260ab1b
Benedict Wong <benedictwong@google.com>
2022-04-25 21:40:02 +0000

    Allow Platform-settings VPNs to work on IPv6-only networks
    This change skips checks for IPv4 default routes for platform VPNs
    configured through the settings app.
    
    Bug: 190572822
    Test: Manual test
    Test: atest FrameworksNetTests
    Change-Id: I527e81abcf2f83fc576bd8c5be5433def5bd8992

--- services/core/java/com/android/server/connectivity/Vpn.java
+++ services/core/java/com/android/server/connectivity/Vpn.java
@@ -2204,9 +2204,22 @@ public class Vpn {
             throw new SecurityException("Restricted users cannot establish VPNs");
         }
 
-        final RouteInfo ipv4DefaultRoute = findIPv4DefaultRoute(egress);
-        final String gateway = ipv4DefaultRoute.getGateway().getHostAddress();
-        final String iface = ipv4DefaultRoute.getInterface();
+        final RouteInfo ipv4DefaultRoute;
+        final String gateway;
+        final String iface;
+
+        // Platform VPNs manage their own underlying networks; do this only for Legacy VPN.
+        // Specifically, Platform VPNs support IPv6 connectivity, and dynamically picks underlying
+        // networks based on NetworkCallbacks.
+        if (!isPlatformVpnProfileType(profile.type)) {
+            ipv4DefaultRoute = findIPv4DefaultRoute(egress);
+            gateway = ipv4DefaultRoute.getGateway().getHostAddress();
+            iface = ipv4DefaultRoute.getInterface();
+        } else {
+            ipv4DefaultRoute = null;
+            gateway = null;
+            iface = null;
+        }
 
         // Load certificates.
         String privateKey = "";
@@ -2371,7 +2384,7 @@ public class Vpn {
     /**
      * Checks if the currently running VPN is a Legacy VPN.
      */
-    public boolean isLegacyVpn() {
+    public synchronized boolean isLegacyVpn() {
         return mVpnRunner instanceof LegacyVpnRunner;
     }
 
@@ -3241,6 +3254,18 @@ public class Vpn {
     @VisibleForTesting
     String getProfileNameForPackage(String packageName) {
         return Credentials.PLATFORM_VPN + mUserId + "_" + packageName;
+    }
+
+    private boolean isPlatformVpnProfileType(int vpnProfileType) {
+        switch (vpnProfileType) {
+            case VpnProfile.TYPE_IKEV2_IPSEC_USER_PASS: // fallthrough
+            case VpnProfile.TYPE_IKEV2_IPSEC_PSK: // fallthrough
+            case VpnProfile.TYPE_IKEV2_IPSEC_RSA: // fallthrough
+            case VpnProfile.TYPE_IKEV2_FROM_IKE_TUN_CONN_PARAMS: // fallthrough
+                return true;
+            default:
+                return false;
+        }
     }
 
     @VisibleForTesting
