a6797d051544cb372378748843767c38de42590d
William Hester <williamhester@google.com>
2022-05-24 13:31:57 -0700

    Consolidate point where ADB is enabled for Test Harness Mode
    Test Harness Mode relies on enabling ADB upon boot. However, currently,
    there appears to be a race condition when enabling ADB. Test Harness
    Mode enables ADB in PHASE_BOOT_COMPLETED, and ADB sets itself up in
    PHASE_ACTIVITY_MANAGER_READY, both by writing a setting. It appears that
    sometimes, the setting for initializing ADB's state commits *after* the
    setting to enable ADB for Test Harness Mode, which means that ADB turns
    on for a brief moment, then off until the device is manually fixed.
    
    This changes it so that the AdbService checks the Test Harness Mode
    property when initializing ADB state, always enabling ADB on boot if
    Test Harness Mode is enabled.
    
    Bug: 232813060
    Test: tested with user and userdebug builds, both enable adb correctly
    Change-Id: I518415dda275bc3d563820ead611e5f017368c95

--- services/core/java/com/android/server/testharness/TestHarnessModeService.java
+++ services/core/java/com/android/server/testharness/TestHarnessModeService.java
@@ -70,7 +70,7 @@ import java.util.Set;
  */
 public class TestHarnessModeService extends SystemService {
     private static final String TAG = TestHarnessModeService.class.getSimpleName();
-    private static final String TEST_HARNESS_MODE_PROPERTY = "persist.sys.test_harness";
+    public static final String TEST_HARNESS_MODE_PROPERTY = "persist.sys.test_harness";
 
     private PersistentDataBlockManagerInternal mPersistentDataBlockManagerInternal;
 
@@ -168,9 +168,9 @@ public class TestHarnessModeService extends SystemService {
             Slog.d(TAG, "Restarted adbd");
         }
 
-        // Disable the TTL for ADB keys before enabling ADB
+        // Disable the TTL for ADB keys before ADB is enabled as a part of AdbService's
+        // initialization.
         Settings.Global.putLong(cr, Settings.Global.ADB_ALLOWED_CONNECTION_TIME, 0);
-        Settings.Global.putInt(cr, Settings.Global.ADB_ENABLED, 1);
         Settings.Global.putInt(cr, Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 1);
         Settings.Global.putInt(cr, Settings.Global.PACKAGE_VERIFIER_INCLUDE_ADB, 0);
         Settings.Global.putInt(
