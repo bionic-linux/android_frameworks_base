1a9840c4f153710fe3550ee3541f9c4722b92bd5
Nalen98 <nalenaskeyx@gmail.com>
2022-07-21 21:22:45 +0300

    Remove verifyApp method cause of it's completely useless
    The method has been removed and its calls in methods addAllowedApplication/addDisallowedApplication;
    dependencies that it previously required have been removed;
    addAllowedApplication/addDisallowedApplication themselves no longer throw exceptions
    
    Test: manual test app
    Change-Id: I95333dba5f25eb17741aefc6dbd265d541272116

--- core/java/android/net/VpnService.java
+++ core/java/android/net/VpnService.java
@@ -31,15 +31,12 @@ import android.compat.annotation.UnsupportedAppUsage;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.IPackageManager;
-import android.content.pm.PackageManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.Parcel;
 import android.os.ParcelFileDescriptor;
 import android.os.RemoteException;
 import android.os.ServiceManager;
-import android.os.UserHandle;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.net.NetworkUtilsInternal;
@@ -771,16 +768,6 @@ public class VpnService extends Service {
             return this;
         }
 
-        private void verifyApp(String packageName) throws PackageManager.NameNotFoundException {
-            IPackageManager pm = IPackageManager.Stub.asInterface(
-                    ServiceManager.getService("package"));
-            try {
-                pm.getApplicationInfo(packageName, 0, UserHandle.getCallingUserId());
-            } catch (RemoteException e) {
-                throw new IllegalStateException(e);
-            }
-        }
-
         /**
          * Adds an application that's allowed to access the VPN connection.
          *
@@ -794,21 +781,16 @@ public class VpnService extends Service {
          * already been called, or vice versa, will throw an {@link UnsupportedOperationException}.
          *
          * {@code packageName} must be the canonical name of a currently installed application.
-         * {@link PackageManager.NameNotFoundException} is thrown if there's no such application.
-         *
-         * @throws PackageManager.NameNotFoundException If the application isn't installed.
          *
          * @param packageName The full name (e.g.: "com.google.apps.contacts") of an application.
          *
          * @return this {@link Builder} object to facilitate chaining method calls.
          */
         @NonNull
-        public Builder addAllowedApplication(@NonNull String packageName)
-                throws PackageManager.NameNotFoundException {
+        public Builder addAllowedApplication(@NonNull String packageName) {
             if (mConfig.disallowedApplications != null) {
                 throw new UnsupportedOperationException("addDisallowedApplication already called");
             }
-            verifyApp(packageName);
             if (mConfig.allowedApplications == null) {
                 mConfig.allowedApplications = new ArrayList<String>();
             }
@@ -827,21 +809,16 @@ public class VpnService extends Service {
          * been called, or vice versa, will throw an {@link UnsupportedOperationException}.
          *
          * {@code packageName} must be the canonical name of a currently installed application.
-         * {@link PackageManager.NameNotFoundException} is thrown if there's no such application.
-         *
-         * @throws PackageManager.NameNotFoundException If the application isn't installed.
          *
          * @param packageName The full name (e.g.: "com.google.apps.contacts") of an application.
          *
          * @return this {@link Builder} object to facilitate chaining method calls.
          */
         @NonNull
-        public Builder addDisallowedApplication(@NonNull String packageName)
-                throws PackageManager.NameNotFoundException {
+        public Builder addDisallowedApplication(@NonNull String packageName) {
             if (mConfig.allowedApplications != null) {
                 throw new UnsupportedOperationException("addAllowedApplication already called");
             }
-            verifyApp(packageName);
             if (mConfig.disallowedApplications == null) {
                 mConfig.disallowedApplications = new ArrayList<String>();
             }
