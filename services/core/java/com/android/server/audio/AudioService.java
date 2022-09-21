ca6071ab404dc25850068efba3e0ff5b8641d61b
Jakub Tyszkowski <tyszkowski@google.com>
2022-09-21 22:13:15 +0000

    BleAudioVolume: Fix calculation for the activated device
    Fixes the following:
     - invalid volume level value set for the newly activated BLE device
     - audio FW not being notified about the new volume level when
       switching to phone call mode
    
    Bug: 243334827
    Bug: 241501978
    Tag: #feature
    Test: tested manually
    Change-Id: I46b2004d205ecdec470b84d1403cbd99af734626

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -3835,7 +3835,7 @@ public class AudioService extends IAudioService.Stub
         }
     }
 
-    private void setLeAudioVolumeOnModeUpdate(int mode) {
+    private void setLeAudioVolumeOnModeUpdate(int streamType, int device, int mode) {
         switch (mode) {
             case AudioSystem.MODE_IN_COMMUNICATION:
             case AudioSystem.MODE_IN_CALL:
@@ -3851,8 +3851,6 @@ public class AudioService extends IAudioService.Stub
                 return;
         }
 
-        int streamType = getBluetoothContextualVolumeStream(mode);
-
         // Currently, DEVICE_OUT_BLE_HEADSET is the only output type for LE_AUDIO profile.
         // (See AudioDeviceBroker#createBtDeviceInfo())
         int index = mStreamStates[streamType].getIndex(AudioSystem.DEVICE_OUT_BLE_HEADSET);
@@ -3863,6 +3861,7 @@ public class AudioService extends IAudioService.Stub
                     + index + " maxIndex=" + maxIndex + " streamType=" + streamType);
         }
         mDeviceBroker.postSetLeAudioVolumeIndex(index, maxIndex, streamType);
+        mDeviceBroker.postApplyVolumeOnDevice(streamType, device, "setLeAudioVolumeOnModeUpdate");
     }
 
     private void setStreamVolume(int streamType, int index, int flags, String callingPackage,
@@ -5239,7 +5238,7 @@ public class AudioService extends IAudioService.Stub
 
                 // Forcefully set LE audio volume as a workaround, since the value of 'device'
                 // is not DEVICE_OUT_BLE_* even when BLE is connected.
-                setLeAudioVolumeOnModeUpdate(mode);
+                setLeAudioVolumeOnModeUpdate(mode, streamType, device);
 
                 // when entering RINGTONE, IN_CALL or IN_COMMUNICATION mode, clear all SCO
                 // connections not started by the application changing the mode when pid changes
