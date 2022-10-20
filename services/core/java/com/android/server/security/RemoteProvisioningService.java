c94b533cd105b2bf576b371b86db80a80f5b4f1f
Seth Moore <sethmo@google.com>
2022-10-20 14:14:39 -0700

    Add remote_provisioning system service
    This service is callable by system components like keystore2. This
    service calls into a mainline module API to do all the work for remote
    provisioning.
    
    Test: Locally keystore to call getRegistration
    Change-Id: I6708d0b415798c78a4d66f279589d9def552ae78

---
+++ services/core/java/com/android/server/security/RemoteProvisioningService.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.security;
+
+import android.content.Context;
+import android.os.OutcomeReceiver;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.android.security.rkpd.service.Registration;
+import com.android.server.SystemService;
+import com.android.server.security.remoteprovisioning.IGetRegistrationCallback;
+import com.android.server.security.remoteprovisioning.IRemoteProvisioning;
+
+/**
+ * A {@link SystemService} that implements the remote provisioning system service. This service is
+ * backed by a mainline module, allowing the underlying implementation to be updated. The code here
+ * is mostly just a thin proxy for the code in com.android.rkpd.
+ *
+ * @hide
+ */
+public class RemoteProvisioningService extends IRemoteProvisioning.Stub {
+    public static final String TAG = "RemoteProvisioningSysSvc";
+
+    Context mContext;
+
+    public RemoteProvisioningService(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void getRegistration(String irpcName, IGetRegistrationCallback callback)
+            throws RemoteException {
+        Log.i(TAG, "getRegistration(" + irpcName + ")");
+        Registration.getRegistrationAsync(
+                mContext,
+                irpcName,
+                mContext.getMainExecutor(),
+                new OutcomeReceiver<>() {
+                    @Override
+                    public void onResult(Registration registration) {
+                        try {
+                            callback.onSuccess(new RegistrationBinder(registration));
+                        } catch (RemoteException e) {
+                            Log.e(TAG, "Error calling success callback", e);
+                        }
+                    }
+
+                    @Override
+                    public void onError(RemoteException error) {
+                        try {
+                            callback.onError(error.toString());
+                        } catch (RemoteException e) {
+                            Log.e(TAG, "Error calling error callback", e);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void cancelGetRegistration(IGetRegistrationCallback callback) throws RemoteException {
+        Log.i(TAG, "cancelGetRegistration()");
+        callback.onError("cancelGetRegistration not yet implemented");
+    }
+}
