be99c9efbe47225306638e561b89a4b45a358d8b
Sean McQuillan <seanmcq@google.com>
2022-12-02 14:12:40 -0800

    Add codepoint exclusions API
    To be used by unbundled glyph drawing code to filter codepoint sequences
    excluded on this platform.
    
    Bug: b/242678495
    Test: CodepointExclusionsTest
    Change-Id: I03f188d58eef3ec1776d03664a9dacc6989f9547

---
+++ core/java/android/text/EmojiConsistency.java
@@ -0,0 +1,225 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.text;
+
+import android.annotation.IntRange;
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.util.Range;
+import android.util.SparseArray;
+
+import com.android.internal.annotations.VisibleForTesting;
+
+import java.util.Collections;
+import java.util.Set;
+
+
+/**
+ * The set of emoji that should be drawn by the system with the default font for device consistency.
+ *
+ * This is intended to be used only by applications that do custom emoji rendering using tools like
+ * {@link android.text.style.ReplacementSpan} or custom emoji fonts.
+ *
+ * An example of how this should be used:
+ *
+ * <p>
+ *     <ol>
+ *         <li>
+ *             Match emoji for third party custom rendering
+ *         </li>
+ *         <li>
+ *             For each match, check against NonStandardEmoji before displaying custom glyph
+ *         </li>
+ *         <li>
+ *             If in NonStandardEmojiSet, do not display custom glyph (render with
+ *             {@link android.graphics.Typeface.DEFAULT} instead)
+ *         </li>
+ *         <li>
+ *             Otherwise, do custom rendering like normal
+ *         </li>
+ *     </ol>
+ * </p>
+ */
+public final class EmojiConsistency {
+    /* Cannot construct */
+    private EmojiConsistency() { }
+
+    @NonNull
+    public static final Range<Integer> NO_MATCH = Range.create(-1, -1);
+
+    /**
+     * Match ranges containing emoji that should be drawn by the system with the default font for
+     * device consistency.
+     *
+     * Finds the next subsequence that contains an emoji that is in the consistency set.
+     *
+     * This will always match the longest match in
+     * {@link EmojiConsistency#getEmojiConsistencySet()}.
+     */
+    public interface Matcher {
+
+        /**
+         * Find the first emoji in the consistency set after start.
+         *
+         * Searching will start and continue until the end of the string.
+         *
+         * On match, returns an [inclusive, exclusive) range containing the matched subsequence that
+         * is in the consistency set.
+         *
+         * On no-match, {@link EmojiConsistency#NO_MATCH} or Range(-1, -1) is returned.
+         *
+         * @param source CharSequence to search
+         * @param start index to start at, is typically 0 or the end of the last match
+         * @return [inclusive, exclusive) match or (-1, -1) on no match.
+         */
+        @NonNull
+        Range<Integer> nextEmojiRangeConsistencyMatch(
+                @NonNull CharSequence source,
+                @IntRange(from = 0) int start
+        );
+    }
+
+    /**
+     * The set of emoji that should be drawn by the system with the default font for device
+     * consistency.
+     *
+     * Apps SHOULD attempt to avoid overwriting system emoji rendering with custom emoji glyphs for
+     * these codepoint sequences.
+     *
+     * Apps that display custom emoji glyphs via matching code MAY filter against this list. On
+     * match, the application SHOULD use platform text rendering for the matched emoji to display
+     * the non-standard rendering.
+     *
+     * To search a CodepointSequence for matches aainst this list see
+     * {@link emojiConsistencyMatcher}.
+     *
+     * @return set of codepoint sequences representing codepoints that should be rendered by the
+     * system using the default font.
+     */
+    @NonNull
+    public static Set<int[]> getEmojiConsistencySet() {
+        return Collections.emptySet();
+    }
+
+    /**
+     * Matcher to search a CharSequence for emoji that are in {@link getEmojiConsistencySet}.
+     *
+     * @return new matcher for searching for emoji in the consistency set.
+     */
+    @NonNull
+    public static Matcher emojiConsistencyMatcher() {
+        return new ConsistencyMatcher(getEmojiConsistencySet());
+    }
+
+    /**
+     * Used for testing trie implementation.
+     *
+     * @hide
+     */
+    @VisibleForTesting
+    @NonNull
+    public static Matcher matcher(Set<int[]> emoji) {
+        return new ConsistencyMatcher(emoji);
+    }
+
+    private static class ConsistencyMatcher implements Matcher {
+
+        private Node mRoot;
+
+        ConsistencyMatcher(Set<int[]> toMatch) {
+            mRoot = new Node(toMatch.size());
+            for (int[] charSequence : toMatch) {
+                insert(charSequence);
+            }
+        }
+
+        private void insert(int[] charSequence) {
+            Node cur = mRoot;
+            for (int i = 0; i < charSequence.length; i++) {
+                cur = cur.getChild(charSequence[i]);
+            }
+            cur.setMatch();
+        }
+
+        @Override
+        public Range<Integer> nextEmojiRangeConsistencyMatch(CharSequence source, int start) {
+            int offset = start;
+            Node cur = mRoot;
+            int matchStart = offset;
+            int lastMatch = -1;
+            while (offset < source.length()) {
+                int codepoint = Character.codePointAt(source, offset);
+                offset += Character.charCount(codepoint);
+                Node walk = cur.walk(codepoint);
+                if (walk == null) {
+                    if (lastMatch == -1) {
+                        // this is juts a normal reset
+                        matchStart = offset;
+                        walk = mRoot;
+                    } else {
+                        // we've found the longest match, return it
+                        return Range.create(matchStart, lastMatch);
+                    }
+                } else if (walk.isMatch()) {
+                    // keep walking, next char could also be a match
+                    lastMatch = offset;
+                }
+                cur = walk;
+            }
+            return NO_MATCH;
+        }
+
+        private static class Node {
+            final SparseArray<Node> mChildren;
+            boolean mIsMatch;
+
+            Node(int size) {
+                mChildren = new SparseArray<>(size);
+                mIsMatch = false;
+            }
+
+            Node(boolean match) {
+                mChildren = new SparseArray<>(1);
+                mIsMatch = match;
+            }
+
+            @NonNull
+            public Node getChild(int key) {
+                Node result = mChildren.get(key);
+                if (result == null) {
+                    result = new Node(false);
+                    mChildren.put(key, result);
+                }
+                return result;
+            }
+
+            public void setMatch() {
+                mIsMatch = true;
+            }
+
+            public boolean isMatch() {
+                return mIsMatch;
+            }
+
+            @Nullable
+            public Node walk(int codepoint) {
+                return mChildren.get(codepoint);
+            }
+        }
+
+    }
+}
---
+++ core/tests/coretests/src/android/text/EmojiConsistencyTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.text;
+
+import static junit.framework.Assert.assertEquals;
+
+import android.util.Range;
+
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Collections;
+import java.util.Set;
+
+@SmallTest
+@RunWith(AndroidJUnit4.class)
+public class EmojiConsistencyTest {
+
+    @Test
+    public void exclusionList_isEmpty() {
+        assertEquals(EmojiConsistency.getEmojiConsistencySet(), Collections.emptySet());
+    }
+
+    @Test
+    public void matcherMatches() {
+        EmojiConsistency.Matcher subject = EmojiConsistency.matcher(
+                Set.of(new int[] { 1 }, new int[] { 2 })
+        );
+
+        String str = new String(new int[] { 0, 1, 0 }, 0, 3);
+
+        Range<Integer> match = subject.nextEmojiRangeConsistencyMatch(str, 0);
+
+        assertEquals(Range.create(1, 2), match);
+
+        match = subject.nextEmojiRangeConsistencyMatch(str, 2);
+
+        assertEquals(Range.create(-1, -1), match);
+    }
+
+    @Test
+    public void matcherMatchesLongest() {
+        EmojiConsistency.Matcher subject = EmojiConsistency.matcher(
+                Set.of(new int[] { 1, 2, 3 }, new int[] { 1, 2 })
+        );
+
+        String str = new String(new int[] { 0, 1, 2, 3 }, 0, 3);
+        Range<Integer> match = subject.nextEmojiRangeConsistencyMatch(str, 0);
+        assertEquals(Range.create(1, 4), match);
+
+        str = new String(new int[] { 0, 1, 2, 4 }, 0, 3);
+        match = subject.nextEmojiRangeConsistencyMatch(str, 0);
+        assertEquals(Range.create(1, 3), match);
+    }
+
+    @Test
+    public void matcherMatchesRepeat() {
+        EmojiConsistency.Matcher subject = EmojiConsistency.matcher(
+                Set.of(new int[] { 1 }, new int[] { 1, 2 })
+        );
+
+        String str = new String(new int[] { 0, 1, 1, 2, 1, 1 }, 0, 3);
+        Range<Integer> match = subject.nextEmojiRangeConsistencyMatch(str, 0); // match 1;
+        assertEquals(Range.create(1, 2), match);
+        match = subject.nextEmojiRangeConsistencyMatch(str, 2); // match 1, 2
+        assertEquals(Range.create(2, 4), match);
+        match = subject.nextEmojiRangeConsistencyMatch(str, 4); // match 1
+        assertEquals(Range.create(4, 5), match);
+        match = subject.nextEmojiRangeConsistencyMatch(str, 5); // match 1
+        assertEquals(Range.create(5, 6), match);
+    }
+}
