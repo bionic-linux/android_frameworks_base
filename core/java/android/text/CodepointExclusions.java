6364595742a91218c8363f637874caa5125fecaa
Sean McQuillan <seanmcq@google.com>
2022-12-02 14:12:40 -0800

    Add codepoint exclusions API
    To be used by unbundled glyph drawing code to filter codepoint sequences
    excluded on this platform.
    
    Bug: b/242678495
    Test: CodepointExclusionsTest
    Change-Id: I03f188d58eef3ec1776d03664a9dacc6989f9547

---
+++ core/java/android/text/CodepointExclusions.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.text;
+
+import android.annotation.NonNull;
+
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * A list of codepoints that an OEM would like applications to avoid rendering.
+ */
+public class CodepointExclusions {
+
+    /* Cannot construct */
+    private CodepointExclusions() { }
+
+    /**
+     * Codepoints that this device would like to avoid showing.
+     *
+     * Apps that display codepoints via custom fonts or glyphs may filter against this list to
+     * honor the OEM glyph removal.
+     *
+     * Apps that use system fonts should ignore this list.
+     *
+     * Codepoint sequence should match the returned string exactly to be excluded.
+     *
+     * This list does not impact platform text rendering, and is only to be used by non-platform
+     * glyph rendering code. OEMs that add codepoints to this list should also remove the codepoints
+     * from platform text renedring.
+     *
+     * @return list of strings representing codepoints to exclude on this platform
+     */
+    @NonNull
+    public static List<String> getExcludedCodepoints() {
+        return Collections.emptyList();
+    }
+}
