c7eca04f4f192bc5a1562f74b1eef4a7e48fd1e5
lucaslin <lucaslin@google.com>
2022-12-09 02:56:21 +0000

    Set VPN as the preference network for specific apps
    Set VPN as the preference network for specific apps when
    IKEv2VpnRunner is running, and reset it when IKEv2VpnRunner is
    cleaned.
    
    VPN will be disconnected when its underlying network is gone. To
    prevent packets go throgh underlying network when underlying
    network is back but VPN is not connected yet, set VPN as the only
    preferred network for specefic apps.
    
    Bug: 231749077
    Test: Create a test app to register default network and check if
          the VPN is the only default network for the test app.
    Change-Id: Ib1b65d142a5ccc935f3882f5665134a56277f19a

--- services/core/java/com/android/server/connectivity/Vpn.java
+++ services/core/java/com/android/server/connectivity/Vpn.java
@@ -2776,6 +2776,18 @@ public class Vpn {
         return hasIPV6 && !hasIPV4;
     }
 
+    @GuardedBy("this")
+    private void setVpnNetworkPreferenceLocked() {
+        mConnectivityManager.setRequireVpnForUids(true /* enabled */, false /* lockdown */,
+                createUserAndRestrictedProfilesRanges(mUserId, mConfig.allowedApplications,
+                        mConfig.disallowedApplications));
+    }
+
+    private void clearVpnNetworkPreference() {
+        mConnectivityManager.setRequireVpnForUids(false /* enabled */,
+                false /* lockdown */, new ArraySet<>());
+    }
+
     /**
      * Internal class managing IKEv2/IPsec VPN connectivity
      *
@@ -2887,6 +2899,8 @@ public class Vpn {
                     (r, exe) -> {
                         Log.d(TAG, "Runnable " + r + " rejected by the mExecutor");
                     });
+            mConfig.disallowedApplications = getAppExclusionList(mPackage);
+            setVpnNetworkPreferenceLocked();
         }
 
         @Override
@@ -3040,7 +3054,6 @@ public class Vpn {
                     mConfig.dnsServers.addAll(dnsAddrStrings);
 
                     mConfig.underlyingNetworks = new Network[] {network};
-                    mConfig.disallowedApplications = getAppExclusionList(mPackage);
 
                     networkAgent = mNetworkAgent;
 
@@ -3744,6 +3757,7 @@ public class Vpn {
             mConnectivityManager.unregisterNetworkCallback(mNetworkCallback);
             mConnectivityDiagnosticsManager.unregisterConnectivityDiagnosticsCallback(
                     mDiagnosticsCallback);
+            clearVpnNetworkPreference();
 
             mExecutor.shutdown();
         }
@@ -4456,6 +4470,7 @@ public class Vpn {
                         .setUids(createUserAndRestrictedProfilesRanges(
                                 mUserId, null /* allowedApplications */, excludedApps))
                         .build();
+                setVpnNetworkPreferenceLocked();
                 doSendNetworkCapabilities(mNetworkAgent, mNetworkCapabilities);
             }
         }
