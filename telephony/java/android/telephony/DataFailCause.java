7555315fd8c39bdb94ac3647e849b2d9d26d321f
Tairu Wang <tairuw@google.com>
2023-01-19 14:20:34 -0800

    Add DataFailCause IWLAN_IKE_NETWORK_LOST_EXCEPTION
    - Add new DatafailCause to match IKE_NETWORK_LOST_EXCEPTION and report to framework module instead of ERROR_UNSPECIFIED
    
    Bug: 266117288
    Test: atest IwlanTests
    Change-Id: I567dfff93f9cda20698f07f0a45a0d7dd4fe749d

--- telephony/java/android/telephony/DataFailCause.java
+++ telephony/java/android/telephony/DataFailCause.java
@@ -1012,6 +1012,9 @@ public final class DataFailCause {
     public static final int IWLAN_DNS_RESOLUTION_NAME_FAILURE = 0x4004;
     /** No response received from the DNS Server due to a timeout*/
     public static final int IWLAN_DNS_RESOLUTION_TIMEOUT = 0x4005;
+    /** IWLAN PDN setup failed due to Wi-Fi lost during IKE tunnel setup,
+     * match exception reported by IKE module*/
+    public static final int IWLAN_IKE_NETWORK_LOST_EXCEPTION = 0x4011;
 
     // OEM sepecific error codes. To be used by OEMs when they don't
     // want to reveal error code which would be replaced by ERROR_UNSPECIFIED
@@ -1505,6 +1508,7 @@ public final class DataFailCause {
         sFailCauseMap.put(IWLAN_IKEV2_CERT_INVALID, "IWLAN_IKEV2_CERT_INVALID");
         sFailCauseMap.put(IWLAN_DNS_RESOLUTION_NAME_FAILURE, "IWLAN_DNS_RESOLUTION_NAME_FAILURE");
         sFailCauseMap.put(IWLAN_DNS_RESOLUTION_TIMEOUT, "IWLAN_DNS_RESOLUTION_TIMEOUT");
+        sFailCauseMap.put(IWLAN_IKE_NETWORK_LOST_EXCEPTION, "IWLAN_IKE_NETWORK_LOST_EXCEPTION");
         sFailCauseMap.put(OEM_DCFAILCAUSE_1, "OEM_DCFAILCAUSE_1");
         sFailCauseMap.put(OEM_DCFAILCAUSE_2, "OEM_DCFAILCAUSE_2");
         sFailCauseMap.put(OEM_DCFAILCAUSE_3, "OEM_DCFAILCAUSE_3");
