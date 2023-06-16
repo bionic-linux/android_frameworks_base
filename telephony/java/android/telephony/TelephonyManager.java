99450eeb9c1db692a778ecbb705ef28378287f66
Jakub Rotkiewicz <rotkiewicz@google.com>
2023-06-16 12:53:35 +0000

    AptX Voice: Make constants visible to new codec
    Bug:
    Tag: #feature
    Test: manual
    Change-Id: If24cf49b22eab82542fd56350869e61de34eee51

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -2541,11 +2541,7 @@ public class TelephonyManager {
     /** Phone is via SIP. */
     public static final int PHONE_TYPE_SIP = PhoneConstants.PHONE_TYPE_SIP;
 
-    /**
-     * Phone is via IMS.
-     *
-     * @hide
-     */
+    /** Phone is via IMS. */
     public static final int PHONE_TYPE_IMS = PhoneConstants.PHONE_TYPE_IMS;
 
     /**
@@ -2554,6 +2550,9 @@ public class TelephonyManager {
      * @hide
      */
     public static final int PHONE_TYPE_THIRD_PARTY = PhoneConstants.PHONE_TYPE_THIRD_PARTY;
+
+    /** Phone is via IMS. */
+    public static final int PHONE_TYPE_CDMA_LTE = PhoneConstants.PHONE_TYPE_CDMA_LTE;
 
     /**
      * Returns the current phone type.
