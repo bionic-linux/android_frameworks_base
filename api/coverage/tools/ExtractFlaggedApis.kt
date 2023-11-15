2ba1ca87d3339e15fd5c2e85e732d11579a68361
Yiming Pan <yimingpan@google.com>
2023-11-15 00:26:25 +0000

    Extract the mapping of flags to APIs.
    This CL creates a binary that extracts flagged APIs from an API
    signature file, and writes the mapping of flags to APIs to a pb file.
    
    Bug: 311059624
    Test: mmm -j frameworks/base/api/coverage/tools/:extract-flagged-apis
          extract-flagged-apis <input api.text file> <output pb file>
    Change-Id: I9b6f45bd126cd6a5b2ed1e9f83bdaa12dd580480

---
+++ api/coverage/tools/ExtractFlaggedApis.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.platform.coverage
+
+import com.android.tools.metalava.model.text.ApiFile
+import java.io.File
+import java.io.FileOutputStream
+
+/** Usage: extract-flagged-apis <api text file> <output .pb file> */
+fun main(args: Array<String>) {
+    var cb = ApiFile.parseApi(listOf(File(args[0])))
+    val flagToApi = mutableMapOf<String, MutableList<String>>()
+    cb.getPackages()
+        .allTopLevelClasses()
+        .filter { it.methods().size > 0 }
+        .forEach {
+            for (method in it.methods()) {
+                if (method.modifiers.isAnnotatedWith("android.annotation.FlaggedApi")) {
+                    val flagValue =
+                        method.modifiers
+                            .findAnnotation("android.annotation.FlaggedApi")
+                            ?.findAttribute("value")
+                            ?.value
+                            ?.value()
+                    val methodQualifiedName = "${it.qualifiedName()}.${method.name()}"
+                    if (flagValue !is String) continue
+                    if (flagToApi.containsKey(flagValue)) {
+                        flagToApi.get(flagValue)?.add(methodQualifiedName)
+                    } else {
+                        flagToApi.put(flagValue, mutableListOf(methodQualifiedName))
+                    }
+                }
+            }
+        }
+    var builder = FlagApiMap.newBuilder()
+    for (flag in flagToApi.keys) {
+        var flaggedApis = FlaggedApis.newBuilder()
+        for (method in flagToApi.get(flag).orEmpty()) {
+            flaggedApis.addFlaggedApi(FlaggedApi.newBuilder().setQualifiedName(method))
+        }
+        builder.putFlagToApi(flag, flaggedApis.build())
+    }
+    val flagApiMap = builder.build()
+    val fs = FileOutputStream(args[1])
+    fs.use { flagApiMap.writeTo(fs) }
+}
