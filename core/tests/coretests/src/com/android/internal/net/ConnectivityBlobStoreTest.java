2413b089696216631808653e33ddf55477e98fc1
Hansen Kurli <hkurli@google.com>
2023-11-17 18:18:44 +0800

    Implement ConnectivityBlobStore get and put.
    Implement get() and put(), including tests.
    
    Bug: 307903113
    Test: atest ConnectivityBlobStoreTest
    Change-Id: Id396b0e266475fdc9476eff642b7f15d3fc581d9

--- core/tests/coretests/src/com/android/internal/net/ConnectivityBlobStoreTest.java
+++ core/tests/coretests/src/com/android/internal/net/ConnectivityBlobStoreTest.java
@@ -16,20 +16,59 @@
 
 package com.android.internal.net;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RunWith(AndroidJUnit4.class)
 @SmallTest
 public class ConnectivityBlobStoreTest {
     private ConnectivityBlobStore mConnectivityBlobStore;
+    private static final String TEST_ALIAS = "TEST_ALIAS";
+    private static final byte[] TEST_BLOB = new byte[] {(byte) 10, (byte) 90, (byte) 45, (byte) 12};
+
+    private Set<String> mAddedAlias = new HashSet<String>();
+
+    private void doPut(String alias, byte[] blob) {
+        mAddedAlias.add(alias);
+        assertTrue(mConnectivityBlobStore.put(alias, blob));
+    }
 
     @Before
     public void setUp() throws Exception {
         mConnectivityBlobStore = new ConnectivityBlobStore(InstrumentationRegistry.getContext());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        for (String alias : mAddedAlias) {
+            mConnectivityBlobStore.remove(alias);
+        }
+
+        mAddedAlias.clear();
+    }
+
+    @Test
+    public void testPutAndGet() throws Exception {
+        assertNull(mConnectivityBlobStore.get(TEST_ALIAS));
+        doPut(TEST_ALIAS, TEST_BLOB);
+        assertArrayEquals(TEST_BLOB, mConnectivityBlobStore.get(TEST_ALIAS));
+
+        // Test replacement
+        final byte[] newBlob = new byte[] {(byte) 15, (byte) 20};
+        doPut(TEST_ALIAS, newBlob);
+        assertArrayEquals(newBlob, mConnectivityBlobStore.get(TEST_ALIAS));
     }
 }
