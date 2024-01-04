15e457bba4dfd295fddd17fd7a04d4dd6d12faa2
buwenjing <buwenjing@xiaomi.com>
2024-01-04 11:43:23 +0800

    Reset top activity mWaitForEnteringPinnedMode of half-way pip anims
    Reset top activity mWaitForEnteringPinnedMode to prevent acitvity layout computed error later.
    
    Bug: 318553345
    Test: Manual test
    
    Change-Id: I9378f54957534c2fab7d877d03bd40d2573959da
    Signed-off-by: buwenjing <buwenjing@xiaomi.com>

--- services/core/java/com/android/server/wm/Task.java
+++ services/core/java/com/android/server/wm/Task.java
@@ -4711,6 +4711,7 @@ class Task extends TaskFragment {
         }
         if (top.isAttached()) {
             top.setWindowingMode(WINDOWING_MODE_UNDEFINED);
+            top.mWaitForEnteringPinnedMode = false;
         }
     }
 
