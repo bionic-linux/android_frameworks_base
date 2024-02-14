398f2e686342843772d7f00af665c73b14568a79
Mitch Phillips <mitchp@google.com>
2024-02-14 16:02:20 +0100

    Allow AppExitInfoTracker to have multiple ExitInfos per pid.
    Currently the AppExitInfoTracker only allows a single
    ApplicationExitInfo entry per pid. This caused some problems with
    Recoverable GWP-ASan, where we'd report a native crash from Recoverable
    GWP-ASan, that would end up in the AppExitInfoTracker, then the process
    would die of other causes (or natural causes) for the same pid, and the
    GWP-ASan data would get lost. This was fixed in aosp/2520518 by having a
    separate tracking database specifically for Recoverable GWP-ASan
    crashes.
    
    Unfortunately that didn't fix the whole problem. For regular GWP-ASan,
    where the process actually does crash, there's still an outstanding
    issue where an app will encounter a native crash in a test, and there's
    a race condition where the AppExitInfoTracker will receive the
    "instrumentation finished" from the test, and overwrite the regular
    GWP-ASan crash.
    
    To support recoverable GWP-ASan, and to avoid this race with regular
    GWP-ASan, I've modified the container to allow multiple AppExitInfos per
    pid.
    
    Bug: 286667521
    Test: Boot the device, atest CtsGwpAsanTestCases --iterations=10,
    atest ApplicationExitInfoTest.
    
    Change-Id: Ifd73d0b5e4bde8bf87caed0aee51291cc6be8d32

--- services/tests/mockingservicestests/src/com/android/server/am/ApplicationExitInfoTest.java
+++ services/tests/mockingservicestests/src/com/android/server/am/ApplicationExitInfoTest.java
@@ -940,6 +940,131 @@ public class ApplicationExitInfoTest {
         }
     }
 
+    private ApplicationExitInfo createExitInfo(int i) {
+        ApplicationExitInfo info = new ApplicationExitInfo();
+        info.setPid(i);
+        info.setTimestamp(1000 + i);
+        info.setPackageUid(2000);
+        return info;
+    }
+
+    private AppExitInfoTracker.AppExitInfoContainer createBasicContainer() {
+        AppExitInfoTracker.AppExitInfoContainer container =
+                mAppExitInfoTracker.new AppExitInfoContainer(3);
+        container.addExitInfoLocked(createExitInfo(10));
+        container.addExitInfoLocked(createExitInfo(30));
+        container.addExitInfoLocked(createExitInfo(20));
+        return container;
+    }
+
+    @Test
+    public void testContainerGetExitInfosIsSortedNewestFirst() throws Exception {
+        AppExitInfoTracker.AppExitInfoContainer container = createBasicContainer();
+        assertEquals(3, container.getExitInfosLocked(0, 0).size());
+        assertEquals(30, container.getExitInfosLocked(0, 0).get(0).getPid());
+        assertEquals(20, container.getExitInfosLocked(0, 0).get(1).getPid());
+        assertEquals(10, container.getExitInfosLocked(0, 0).get(2).getPid());
+    }
+
+    @Test
+    public void testContainerRemovesOldestReports() throws Exception {
+        AppExitInfoTracker.AppExitInfoContainer container = createBasicContainer();
+        container.addExitInfoLocked(createExitInfo(40));
+        assertEquals(3, container.getExitInfosLocked(0, 0).size());
+        assertEquals(40, container.getExitInfosLocked(0, 0).get(0).getPid());
+        assertEquals(30, container.getExitInfosLocked(0, 0).get(1).getPid());
+        assertEquals(20, container.getExitInfosLocked(0, 0).get(2).getPid());
+
+        container.addExitInfoLocked(createExitInfo(50));
+        assertEquals(3, container.getExitInfosLocked(0, 0).size());
+        assertEquals(50, container.getExitInfosLocked(0, 0).get(0).getPid());
+        assertEquals(40, container.getExitInfosLocked(0, 0).get(1).getPid());
+        assertEquals(30, container.getExitInfosLocked(0, 0).get(2).getPid());
+
+        container.addExitInfoLocked(createExitInfo(45));
+        assertEquals(3, container.getExitInfosLocked(0, 0).size());
+        assertEquals(50, container.getExitInfosLocked(0, 0).get(0).getPid());
+        assertEquals(45, container.getExitInfosLocked(0, 0).get(1).getPid());
+        assertEquals(40, container.getExitInfosLocked(0, 0).get(2).getPid());
+
+        // Adding an older report shouldn't remove the newer ones.
+        container.addExitInfoLocked(createExitInfo(15));
+        assertEquals(3, container.getExitInfosLocked(0, 0).size());
+        assertEquals(50, container.getExitInfosLocked(0, 0).get(0).getPid());
+        assertEquals(45, container.getExitInfosLocked(0, 0).get(1).getPid());
+        assertEquals(40, container.getExitInfosLocked(0, 0).get(2).getPid());
+    }
+
+    @Test
+    public void testContainerFilterByPid() throws Exception {
+        AppExitInfoTracker.AppExitInfoContainer container = createBasicContainer();
+        assertEquals(1, container.getExitInfosLocked(30, 0).size());
+        assertEquals(30, container.getExitInfosLocked(0, 0).get(0).getPid());
+
+        assertEquals(1, container.getExitInfosLocked(30, 0).size());
+        assertEquals(20, container.getExitInfosLocked(20, 0).get(0).getPid());
+
+        assertEquals(1, container.getExitInfosLocked(10, 0).size());
+        assertEquals(10, container.getExitInfosLocked(10, 0).get(0).getPid());
+
+        assertEquals(0, container.getExitInfosLocked(1337, 0).size());
+    }
+
+    @Test
+    public void testContainerLimitQuantityOfResults() throws Exception {
+        AppExitInfoTracker.AppExitInfoContainer container = createBasicContainer();
+        assertEquals(1, container.getExitInfosLocked(30, 1).size());
+        assertEquals(30, container.getExitInfosLocked(0, 1).get(0).getPid());
+        assertEquals(1, container.getExitInfosLocked(30, 1000).size());
+        assertEquals(30, container.getExitInfosLocked(0, 1000).get(0).getPid());
+
+        assertEquals(1, container.getExitInfosLocked(20, 1).size());
+        assertEquals(20, container.getExitInfosLocked(20, 1).get(0).getPid());
+        assertEquals(1, container.getExitInfosLocked(20, 1000).size());
+        assertEquals(20, container.getExitInfosLocked(20, 1000).get(0).getPid());
+
+        assertEquals(1, container.getExitInfosLocked(10, 1).size());
+        assertEquals(10, container.getExitInfosLocked(10, 1).get(0).getPid());
+        assertEquals(1, container.getExitInfosLocked(10, 1000).size());
+        assertEquals(10, container.getExitInfosLocked(10, 1000).get(0).getPid());
+
+        assertEquals(0, container.getExitInfosLocked(1337, 1).size());
+        assertEquals(0, container.getExitInfosLocked(1337, 1000).size());
+        assertEquals(null, container.getLastExitInfoForPid(1337));
+    }
+
+    @Test
+    public void testContainerCanHoldMultipleFromSamePid() throws Exception {
+        AppExitInfoTracker.AppExitInfoContainer container =
+                mAppExitInfoTracker.new AppExitInfoContainer(3);
+        ApplicationExitInfo info = createExitInfo(10);
+        ApplicationExitInfo info2 = createExitInfo(10);
+        ApplicationExitInfo info3 = createExitInfo(10);
+        info2.setTimestamp(1337);
+        info3.setTimestamp(31337);
+        container.addExitInfoLocked(info);
+        container.addExitInfoLocked(info2);
+        container.addExitInfoLocked(info3);
+        assertEquals(3, container.getExitInfosLocked(0, 0).size());
+        assertEquals(10, container.getExitInfosLocked(0, 0).get(0).getPid());
+        assertEquals(10, container.getExitInfosLocked(0, 0).get(1).getPid());
+        assertEquals(10, container.getExitInfosLocked(0, 0).get(2).getPid());
+        assertEquals(31337, container.getExitInfosLocked(0, 0).get(0).getTimestamp());
+        assertEquals(1337, container.getExitInfosLocked(0, 0).get(1).getTimestamp());
+        assertEquals(1010, container.getExitInfosLocked(0, 0).get(2).getTimestamp());
+
+        assertEquals(3, container.getExitInfosLocked(10, 0).size());
+        assertEquals(31337, container.getExitInfosLocked(10, 0).get(0).getTimestamp());
+        assertEquals(1337, container.getExitInfosLocked(10, 0).get(1).getTimestamp());
+        assertEquals(1010, container.getExitInfosLocked(10, 0).get(2).getTimestamp());
+
+        assertEquals(2, container.getExitInfosLocked(10, 2).size());
+        assertEquals(31337, container.getExitInfosLocked(10, 2).get(0).getTimestamp());
+        assertEquals(1337, container.getExitInfosLocked(10, 2).get(1).getTimestamp());
+
+        assertEquals(31337, container.getLastExitInfoForPid(10).getTimestamp());
+    }
+
     private static int makeExitStatus(int exitCode) {
         return (exitCode << 8) & 0xff00;
     }
