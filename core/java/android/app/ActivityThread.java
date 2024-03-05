4230690c4c7b19b3a6be43f8a5b2333bda5f6b55
Fabien Sanglard <sanglardf@google.com>
2024-03-05 21:33:12 +0000

    Appinfo: Make adb the app debug source of truth
    Design doc: go/adb_app_info
    
    Test: Tested in adb repo
    Bug: NA
    
    Change-Id: I9de5f25730c73d61ffadd82fc79121ef75b347fb

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -429,7 +429,7 @@ public final class ActivityThread extends ClientTransactionHandler
     @UnsupportedAppUsage
     Application mInitialApplication;
     @UnsupportedAppUsage
-    final ArrayList<Application> mAllApplications = new ArrayList<>();
+    private final ArrayList<Application> mAllApplications = new ArrayList<>();
     /**
      * Bookkeeping of instantiated backup agents indexed first by user id, then by package name.
      * Indexing by user id supports parallel backups across users on system packages as they run in
@@ -7062,6 +7062,8 @@ public final class ActivityThread extends ClientTransactionHandler
             }
         }
 
+        VMDebug.setUserId(UserHandle.myUserId());
+        VMDebug.addApplication(data.appInfo.packageName);
         // send up app name; do this *before* waiting for debugger
         Process.setArgV0(data.processName);
         android.ddm.DdmHandleAppName.setAppName(data.processName,
@@ -8606,6 +8608,12 @@ public final class ActivityThread extends ClientTransactionHandler
         } catch (RemoteException ignored) {
         }
         return false;
+    }
+
+    @UnsupportedAppUsage
+    void addApplication(@NonNull Application app) {
+        mAllApplications.add(app);
+        VMDebug.addApplication(app.mLoadedApk.mPackageName);
     }
 
     @Override
