05ccdc05d8e2ee387857bfab10e0c49e5ad31bda
Pawan Wagh <waghpawan@google.com>
2024-03-29 21:35:35 +0000

    Adding FileSystemUtils test
    Adding tests to check whether apps can be launched after
    loading the punched libs. One app has uncompressed shared lib which
    will be directly loaded from apk and other will extract it.
    
    Test: acloud delete --all && m && acloud create --local-instance --local-image && adb logcat -c && m FileSystemUtilsTests && atest -c FileSystemUtilsTests
    Bug: 301631861
    Change-Id: I4f7b38902f2e02e83a350cd1cc907f8edaa20c81

---
+++ core/tests/FileSystemUtilsTest/apk_extract_native_libs/src/android/test/extract/MainActivity.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.test.extract;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.VisibleForTesting;
+
+public class MainActivity extends Activity {
+
+    static {
+        System.loadLibrary("punchtest");
+    }
+
+    @VisibleForTesting
+    static final String INTENT_TYPE = "android.test.extract.EXTRACTED_LIB_LOADED";
+
+    @Override
+    public void onCreate(Bundle savedOnstanceState) {
+        super.onCreate(savedOnstanceState);
+        Intent intent = new Intent(INTENT_TYPE);
+        // Send broadcast so that test can know app has launched and lib is loaded
+        sendBroadcast(intent);
+    }
+}
