4872b56f67b649eefb9e075edc96a239840f378f
Himanshu Singh Kushwah <himanshusingh.kushwah@nxp.com>
2024-06-26 16:27:30 +0530

    Extending setControllerAlwaysOn feature with trasnparent and Card Emulation mode support
    Bug: 331563246
    Test: Manual
    Flag: NA
    nfcOemExtension.setControllerAlwaysOn(INIT_MODE_TRANSPARENT)
    nfcOemExtension.setControllerAlwaysOn(INIT_MODE_EE)
    
    Change-Id: I3fc290ebc0b75eca45512eeeb890f99ed0cb77b7

--- nfc/java/android/nfc/NfcAdapter.java
+++ nfc/java/android/nfc/NfcAdapter.java
@@ -559,6 +559,18 @@ public final class NfcAdapter {
     @Retention(RetentionPolicy.SOURCE)
     public @interface TagIntentAppPreferenceResult {}
 
+    /**
+     * Mode Type for {@link NfcOemExtension#setControllerAlwaysOn(int)}.
+     * @hide
+     */
+    public static final int CONTROLLER_ALWAYS_ON_MODE_DEFAULT = 1;
+
+    /**
+     * Mode Type for {@link NfcOemExtension#setControllerAlwaysOn(int)}.
+     * @hide
+     */
+    public static final int CONTROLLER_ALWAYS_ON_DISABLE = 0;
+
     // Guarded by sLock
     static boolean sIsInitialized = false;
     static boolean sHasNfcFeature;
@@ -2334,7 +2346,13 @@ public final class NfcAdapter {
         if (!sHasNfcFeature && !sHasCeFeature) {
             throw new UnsupportedOperationException();
         }
-        return callServiceReturn(() ->  sService.setControllerAlwaysOn(value), false);
+        int mode;
+        if (value) {
+            mode = CONTROLLER_ALWAYS_ON_MODE_DEFAULT;
+        } else {
+            mode = CONTROLLER_ALWAYS_ON_DISABLE;
+        }
+        return callServiceReturn(() ->  sService.setControllerAlwaysOn(mode), false);
 
     }
 
