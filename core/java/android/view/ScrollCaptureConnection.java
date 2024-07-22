148d77580f0ed9690d07a58f64302505c1b3e02a
xiaomei.li <xiaomei.li@unisoc.com>
2024-07-22 20:20:57 +0800

    Fix the NPE issue caused by the multi-thread operation on mSession and mLocal object.
    Change-Id: I9dc6b2c72fd4ca2c6aaf009352fd3d7227cc7914

--- core/java/android/view/ScrollCaptureConnection.java
+++ core/java/android/view/ScrollCaptureConnection.java
@@ -115,7 +115,13 @@ public class ScrollCaptureConnection extends IScrollCaptureConnection.Stub imple
         Runnable listener =
                 SafeCallback.create(mCancellation, mUiThread, this::onStartCaptureCompleted);
         // -> UiThread
-        mUiThread.execute(() -> mLocal.onScrollCaptureStart(mSession, mCancellation, listener));
+        mUiThread.execute(() -> {
+           synchronized (ScrollCaptureConnection.this) {
+              if (mLocal != null) {
+                 mLocal.onScrollCaptureStart(mSession, mCancellation, listener));
+              }
+	   }
+	}
         return cancellation;
     }
 
@@ -145,10 +151,12 @@ public class ScrollCaptureConnection extends IScrollCaptureConnection.Stub imple
                 SafeCallback.create(mCancellation, mUiThread, this::onImageRequestCompleted);
         // -> UiThread
         mUiThread.execute(() -> {
-            if (mLocal != null) {
-                mLocal.onScrollCaptureImageRequest(
-                        mSession, mCancellation, new Rect(requestRect), listener);
-            }
+            synchronized (ScrollCaptureConnection.this) {
+                if (mLocal != null) {
+                    mLocal.onScrollCaptureImageRequest(
+                            mSession, mCancellation, new Rect(requestRect), listener);
+                }
+	    }
         });
 
         return cancellation;
