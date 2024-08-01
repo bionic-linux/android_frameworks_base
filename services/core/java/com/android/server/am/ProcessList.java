c44c5119fb36382ef6f565a5b1c01b8f704c5e87
zhuning3 <zhuning3@xiaomi.com>
2024-08-01 14:47:47 +0800

    Fix the application startup failure caused by the abnormal state value of ceDataInode in version V.
    Test:UAT
    Bug:
    
    Change-Id: I7b208fab88a77d46c4c75fe1232e1d8ca439bdbd
    Signed-off-by: zhuning3 <zhuning3@xiaomi.corp-partner.google.com>

--- services/core/java/com/android/server/am/ProcessList.java
+++ services/core/java/com/android/server/am/ProcessList.java
@@ -2333,8 +2333,9 @@ public final class ProcessList {
             }
             String volumeUuid = packageState.getVolumeUuid();
             long inode = packageState.getUserStateOrDefault(userId).getCeDataInode();
-            if (inode == 0) {
-                Slog.w(TAG, packageName + " inode == 0 (b/152760674)");
+            boolean dataInstalled = packageState.getUserStateOrDefault(userId).dataExists();
+            if (!dataInstalled) {
+                Slog.w(TAG, packageName + " is not installed. ");
                 return null;
             }
             result.put(packageName, Pair.create(volumeUuid, inode));
