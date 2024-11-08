90824e88893fb61f535b89eff1bb9393fb346587
Shawn Willden <swillden@google.com>
2024-11-08 11:18:07 -0700

    Support multiple FactoryResetProtection policies.
    Test: Not yet
    Flags: Not yet
    Change-Id: Ide7adb18cf5b07b0a0b1a7639ec1dd308b671d10

---
+++ services/devicepolicy/java/com/android/server/devicepolicy/FactoryResetProtectionPolicyValue.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.devicepolicy;
+
+import android.app.admin.FactoryResetProtectionPolicy;
+import android.app.admin.PolicyValue;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+/** @hide */
+final class FactoryResetProtectionPolicyValue extends PolicyValue<FactoryResetProtectionPolicy> {
+    public static final @NonNull Parcelable.Creator<FactoryResetProtectionPolicy> CREATOR =
+            FactoryResetProtectionPolicy.CREATOR;
+
+    FactoryResetProtectionPolicyValue(@NonNull FactoryResetProtectionPolicy value) {
+        super(value);
+    }
+
+    @Override
+    public int describeContents() {
+        return getValue().describeContents();
+    }
+
+    @Override
+    public void writeToParcel(@androidx.annotation.NonNull Parcel dest, int flags) {
+        getValue().writeToParcel(dest, flags);
+    }
+}
+
