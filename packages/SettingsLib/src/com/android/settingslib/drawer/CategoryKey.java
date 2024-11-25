0f7f099c221877eb3f2f2b40966cc730f6f33b4f
Mattias Nilsson <mattias.nilsson@sony.com>
2024-11-25 14:59:16 +0100

    Add CATEGORY_MOBILE_NETWORK_SETTINGS
    It is not possible to inject an OEM setting under
    Telephony settings under Settings>Network and Internet>SIMs>[Operator Name]
    
    Add CATEGORY_MOBILE_NETWORK_SETTINGS to solve this.
    
    Flag: NONE Bugfix
    Test: Add a setting with Category com.android.settings.category.ia.mobile_network_settings
    Bug: 380840474
    Change-Id: I8717a17af54184cd78ba5414e07195b9aea1e7e9

--- packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
@@ -72,6 +72,8 @@ public final class CategoryKey {
             "com.android.settings.category.ia.communal";
     public static final String CATEGORY_MORE_SECURITY_PRIVACY_SETTINGS =
             "com.android.settings.category.ia.more_security_privacy_settings";
+    public static final String CATEGORY_MOBILE_NETWORK_SETTINGS =
+            "com.android.settings.category.ia.mobile_network_settings";
 
     public static final Map<String, String> KEY_COMPAT_MAP;
 
